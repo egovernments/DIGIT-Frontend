6c919ff8404b88d797109acec6a70eb1c84446cb
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-01-28 20:55:56 +0530

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const commonConfig = require("./webpack.common");
 const packageJson = require("./package.json");
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN || "https://design-egov.github.io";
 
 module.exports = () => {
   const prodConfig = {
