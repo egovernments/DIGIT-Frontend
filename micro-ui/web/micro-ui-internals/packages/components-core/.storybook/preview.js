3c969cb7cc254a1afc3badb71fcaccbdf5219a16
Swathi-eGov <swathi.chatrathi@egovernments.org>
2023-11-06 15:48:29 +0530

    Modification in preview

--- micro-ui/web/micro-ui-internals/packages/components-core/.storybook/preview.js
+++ micro-ui/web/micro-ui-internals/packages/components-core/.storybook/preview.js
@@ -1,5 +1,5 @@
-import "@egovernments/digit-ui-css/example/index.css";
-// import "@egovernments/digit-ui-css/dist/index.css";
+// import "@egovernments/digit-ui-css/example/index.css";
+import "@egovernments/digit-ui-css/dist/index.css";
 import { initCoreLibraries } from "@egovernments/digit-ui-libraries-core";
 
 // import '../src/index.css';
