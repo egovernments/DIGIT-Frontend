9f8123c2b8824a59c95b5301b50567080d549004
ashish-egov <ashish.tiwari@egovernments.org>
2023-11-16 16:39:40 +0530

    Added localistaion and removed inline styling

--- micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/MobileLinksSection.js
+++ micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/MobileLinksSection.js
@@ -1,51 +1,54 @@
 import React from 'react';
 import { AddIcon, ArrowBack, ArrowDirection, DashboardIcon } from './svgindex';
 import { useHistory, Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 
 export const MobileLinksSection = ({ hierarchy, selectedIcon, setSelectedIcon }) => {
     const history = useHistory();
+    const { t } = useTranslation();
+
 
     return (
         <div className="mobile-links">
             <div
                 onClick={() => setSelectedIcon(null)}
-                style={{ display: "flex", flexDirection: "row", alignSelf: "flex-start", color: "black", cursor: "pointer" }}
+                className="back-button"
             >
                 <ArrowBack />
-                <div>Back</div>
+                <div>{t("WORKBENCH_LABEL_BACK")}</div>
             </div>
-            {(hierarchy[selectedIcon].employeeModuleCardProps.kpis.length > 0) &&
-                <div className='mobile-details' style={{ marginTop: "20px", padding: "10px", paddingBottom: "5px", width: "90%", backgroundColor: "white" }}>
+            {hierarchy[selectedIcon].employeeModuleCardProps.kpis.length > 0 && (
+                <div className='mobile-details'>
                     {hierarchy[selectedIcon].employeeModuleCardProps.kpis.length !== 0 && (
-                        <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
+                        <div className="kpis-container">
                             {hierarchy[selectedIcon].employeeModuleCardProps.kpis.map(({ count, label, link }, index) => (
-                                <div key={index} style={{ marginBottom: "10px" }}>
+                                <div key={index} className="kpi-item">
                                     <span>{count || "-"}</span> {link ? <span className="link" onClick={() => history.push(`${link}`, { count })}>{label}</span> : null}
                                 </div>
                             ))}
                         </div>
                     )}
-                </div>}
+                </div>
+            )}
             <ul className='mobile-links-ul'>
                 {hierarchy[selectedIcon].employeeModuleCardProps.links.map((link, index) => (
                     <li key={index} className='mobile-links-li'>
-                        <AddIcon fill='orangered' styles={{ height: "20px", width: "20px" }} />
+                        <AddIcon fill='orangered' className="add-icon" />
                         <span
                             onClick={() => history.push(link.link)}
                             target="_blank"
                             rel="noopener noreferrer"
-                            style={{ flex: "1", marginLeft: "10px" }}
+                            className="li-link-label"
                         >
                             {link.label}
                         </span>
                         <div className="arrow-direction-container">
-                            <ArrowDirection styles={{ height: "20px", width: "20px", fill: "white" }} />
+                            <ArrowDirection className="arrow-icon" />
                         </div>
                     </li>
                 ))}
             </ul>
-
         </div>
     );
 };
