0c7fed40059db5e2ba604c85dcb165698925a8fa
ashish-egov <ashish.tiwari@egovernments.org>
2023-11-20 17:44:56 +0530

    chore(rename): update component and file name to mdms-search-v3

--- micro-ui/web/micro-ui-internals/packages/modules/workbench/src/pages/employee/index.js
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench/src/pages/employee/index.js
@@ -12,7 +12,7 @@ import MDMSSearchv2 from "./MDMSSearchv2";
 import MDMSManageMaster from "./MDMSManageMaster";
 import LocalisationAdd from "./LocalisationAdd";
 import WorkbenchHeader from "../../components/WorkbenchHeader";
-import MDMSSearchWithSchema from "./MDMSSearchWithSchema";
+import MDMSSearchv3 from "./MDMSSearchv3";
 
 const WorkbenchBreadCrumb = ({ location, defaultPath }) => {
   const { t } = useTranslation();
@@ -98,7 +98,7 @@ const App = ({ path }) => {
           <PrivateRoute path={`${path}/mdms-edit`} component={() => <MDMSEdit parentRoute={path} />} />
           <PrivateRoute path={`${path}/manage-master-data`} component={() => <MDMSManageMaster parentRoute={path} />} />
           <PrivateRoute path={`${path}/mdms-search-v2`} component={() => <MDMSSearchv2 parentRoute={path} />} />
-          <PrivateRoute path={`${path}/mdms-search-schema`} component={() => <MDMSSearchWithSchema parentRoute={path} />} />
+          <PrivateRoute path={`${path}/mdms-search-v3`} component={() => <MDMSSearchv3 parentRoute={path} />} />
           <PrivateRoute path={`${path}/localisation-add`} component={() => <LocalisationAdd parentRoute={path} />} />
 
         </AppContainer>
