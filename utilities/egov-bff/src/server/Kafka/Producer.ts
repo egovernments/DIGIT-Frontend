76035ecce5cd738d9f8263ada551d82bb59ed941
ashish-egov <ashish.tiwari@egovernments.org>
2024-01-10 12:14:36 +0530

    Fix : kafka integration fix

--- utilities/egov-bff/src/server/Kafka/Producer.ts
+++ utilities/egov-bff/src/server/Kafka/Producer.ts
@@ -2,12 +2,22 @@ import { Producer, KafkaClient } from "kafka-node";
 
 import config from '../config';
 
-// Replace with the correct Kafka broker(s) and topic name
 const kafkaConfig = {
-    kafkaHost: config.KAFKA_BROKER_HOST, // Use the correct broker address and port
+    // connect directly to kafka broker (instantiates a KafkaClient)
+    kafkaHost: config.KAFKA_BROKER_HOST,
     autoCommit: true,
     autoCommitIntervalMs: 5000,
-    fromOffset: 'earliest', // Start reading from the beginning of the topic
+    sessionTimeout: 15000,
+    fetchMaxBytes: 10 * 1024 * 1024, // 10 MB
+    // An array of partition assignment protocols ordered by preference. 'roundrobin' or 'range' string for
+    // built ins (see below to pass in custom assignment protocol)
+    protocol: ["roundrobin"],
+    // Offsets to use for new groups other options could be 'earliest' or 'none'
+    // (none will emit an error if no offsets were saved) equivalent to Java client's auto.offset.reset
+    fromOffset: "latest",
+    // how to recover from OutOfRangeOffset error (where save offset is past server retention)
+    // accepts same value as fromOffset
+    outOfRangeOffset: "earliest"
 };
 
 // Create a Kafka client
