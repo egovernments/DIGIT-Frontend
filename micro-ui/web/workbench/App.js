73100f6367df36ca55fb398bcdfcd375b2b0ca72
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-01-10 11:14:38 +0530

    Update App.js

--- micro-ui/web/workbench/App.js
+++ micro-ui/web/workbench/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
-import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
+// import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
 import { UICustomizations } from "./Customisations/UICustomizations";
 import { initWorkbenchComponents } from "@egovernments/digit-ui-module-workbench";
 import { initUtilitiesComponents } from "@egovernments/digit-ui-module-utilities";
@@ -13,7 +13,7 @@ const enabledModules = [
   "DSS",
   "NDSS",
   "Utilities",
-  "HRMS",
+  // "HRMS",
   "Engagement",
   "Workbench",
   "HCMWORKBENCH"
@@ -29,7 +29,7 @@ const initDigitUI = () => {
     PGR: {},
     commonUiConfig: UICustomizations,
   };
-  initHRMSComponents();
+  // initHRMSComponents();
   initUtilitiesComponents();
   initWorkbenchComponents();
   initWorkbenchHCMComponents();
