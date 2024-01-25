b20f8afccc0e75a39c5ccc9bca6da05657b366ef
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-01-25 06:40:28 +0530

    removed all the core modules and made only hcm workbench as active

--- micro-ui/web/workbench/App.js
+++ micro-ui/web/workbench/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { initLibraries } from "@egovernments/digit-ui-libraries";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
-import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
 import { UICustomizations } from "./Customisations/UICustomizations";
 import { initWorkbenchComponents } from "@egovernments/digit-ui-module-workbench";
+import { initUtilitiesComponents } from  "@egovernments/digit-ui-module-utilities";
 
 import { initWorkbenchHCMComponents } from "@egovernments/digit-ui-module-hcmworkbench";
 
@@ -30,7 +30,7 @@ const initDigitUI = () => {
     PGR: {},
     commonUiConfig: UICustomizations,
   };
-  initHRMSComponents();
+  initUtilitiesComponents();
   initWorkbenchComponents();
   initWorkbenchHCMComponents();
 };
