5b255e8966ef256b308d6118ef328c1b8275ba5e
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-02-06 17:21:00 +0530

    Update webpack.prod.js

--- micro-ui/web/packages/core/webpack.prod.js
+++ micro-ui/web/packages/core/webpack.prod.js
@@ -21,7 +21,7 @@ module.exports = () => {
           dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
           hrms: `hrms@${domain}/hrms/remoteEntry.js`,
           workbench : `workbench@${domain}/workbench/remoteEntry.js`,
-          dss : `dss@${doamin}/dss/remoteEntry.js`,
+          dss : `dss@${domain}/dss/remoteEntry.js`,
           measurement : `measurement@${domain}/measurement/remoteEntry.js`
         },
         shared: packageJson.dependencies,
