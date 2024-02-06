fa780bcc3f700c3eff69be669f937e195cb958fb
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-02-06 17:26:09 +0530

    Update webpack.common.js

--- micro-ui/web/packages/core/webpack.common.js
+++ micro-ui/web/packages/core/webpack.common.js
@@ -9,13 +9,18 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js?$/,
+      //   loader: "babel-loader",
+      //   exclude: /node_modules/,
+      //   options: {
+      //     presets: ["@babel/preset-react"],
+      //   },
+      // },
       {
-        test: /\.js?$/,
-        loader: "babel-loader",
+        test: /\.(js)$/,
         exclude: /node_modules/,
-        options: {
-          presets: ["@babel/preset-react"],
-        },
+        use: ["babel-loader"],
       },
     ],
   },
