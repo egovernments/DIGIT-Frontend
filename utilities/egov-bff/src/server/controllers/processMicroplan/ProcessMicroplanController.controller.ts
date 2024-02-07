69f11550b9d61aca00fc9c051ec53c0012864788
admin1 <nitish@egovernments.org>
2024-02-07 12:13:42 +0530

    Feat: added pagination and modified response HLM-5000

--- utilities/egov-bff/src/server/controllers/processMicroplan/ProcessMicroplanController.controller.ts
+++ utilities/egov-bff/src/server/controllers/processMicroplan/ProcessMicroplanController.controller.ts
@@ -5,6 +5,8 @@ import { logger } from "../../utils/logger";
 import { produceModifiedMessages } from '../../Kafka/Listener';
 import { getCampaignDetails } from '../../utils/index'
 import { validateProcessMicroplan } from '../../utils/validator'
+import { generateSortingAndPaginationClauses } from '../../utils/index'
+
 import {
   searchMDMS
 } from "../../api/index";
@@ -51,128 +53,112 @@ class BulkUploadController {
         password: '1234',
         port: 5432,
       });
-      let criteria = request?.body?.HCMConfig;
-      const userInfo = request?.body?.RequestInfo.userInfo;
-      criteria.createdBy=userInfo.uuid;
-      console.log(criteria.createdBy,"ppppppp")
-      const { campaignId, campaignName, campaignType, campaignNumber, ingestionType , createdBy} = criteria;
+      let criteria = request?.body?.CampaignDetails;
+      const { campaignIds, campaignName, campaignType, campaignNumber, createdBy, projectTypeId, pagination } = criteria;
       const client = await pool.connect();
       let queryString = 'SELECT * FROM eg_campaign_details WHERE 1=1';
-
-      if (campaignId && campaignId.length > 0) {
-        const idList = campaignId.map((id: any) => `'${id}'`).join(', ');
+      if (campaignIds && campaignIds.length > 0) {
+        const idList = campaignIds.map((id: any) => `'${id}'`).join(', ');
         queryString += ` AND id IN (${idList})`;
       }
-
       if (campaignName) {
         queryString += ` AND campaignName = '${campaignName}'`;
       }
-
       if (campaignNumber) {
         queryString += ` AND campaignNumber = '${campaignNumber}'`;
       }
-
       if (campaignType) {
         queryString += ` AND campaignType = '${campaignType}'`;
       }
-      if (userInfo && userInfo.uuid) {
-        queryString += ` AND createdBy = '${createdBy}'`; // Assuming createdby field is used for uuid
+      if (createdBy) {
+        queryString += ` AND createdBy = '${createdBy}'`;
       }
-      console.log(createdBy,"cccccrrrrrrreateeeddddddd")
-
-      console.log(queryString, "wwwwwwwwwwwww");
-
+      if (projectTypeId) {
+        queryString += ` AND projectTypeId = '${projectTypeId}'`;
+      }
+      const sortingAndPaginationClauses = generateSortingAndPaginationClauses(pagination);
+      queryString += sortingAndPaginationClauses;
       const campaignResult: QueryResult = await client.query(queryString);
-      // console.log(campaignResult, "rrrrrrrrrrrrr");
-
       const campaignDetails: any = campaignResult.rows;
-      console.log(campaignDetails, "ccccccccccccccccc");
+
 
       // Query 'eg_campaign_ingestionDetails' based on the campaignDetails
       if (campaignDetails.length > 0) {
         const idListForIngestion = campaignDetails.map((campaign: { id: any; }) => `'${campaign.id}'`).join(', ');
         let ingestionQueryString = `SELECT * FROM eg_campaign_ingestionDetails WHERE campaignId IN (${idListForIngestion})`;
-
-        if (ingestionType) {
-          ingestionQueryString += ` AND ingestionType = '${ingestionType}'`;
-        }
-        console.log(ingestionQueryString,"iiiiiiiiiiiiiiiiiiii")
-
         const ingestionResult: QueryResult = await client.query(ingestionQueryString);
         const ingestionDetails: any = ingestionResult.rows;
-        console.log(ingestionDetails, "ingestionnnnnnnnnnnnnnn");
         const responseData = campaignDetails.map((campaign: any) => {
-          console.log(campaign.id, "ccccccccaaaaaammppp");
           const filteredIngestionDetails = ingestionDetails.filter((ingestion: any) => {
-              console.log("Ingestion Campaign ID:", ingestion.campaignid);
-              return ingestion.campaignid === campaign.id;
+            return ingestion.campaignid === campaign.id;
           });
           return {
-              ...campaign,
-              ingestionDetails: filteredIngestionDetails
+            ...campaign,
+            ingestionDetails: filteredIngestionDetails
           };
-      });
-      return response.json(responseData);
-  }
-else{
-      return response.json({ error: 'No matching campaigns found.' })
-} }catch(error) {
-    console.error('Error searching campaigns:', error);
-    return response.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-
-
-processMicroplan = async (
-  request: express.Request,
-  response: express.Response
-) => {
-  try {
-    await validateProcessMicroplan(request, response);
-    const campaignDetails = await getCampaignDetails(request?.body);
-    if (campaignDetails == "INVALID_CAMPAIGN_NUMBER") {
-      throw new Error("Error during Campaign Number generation");
+        });
+        return sendResponse(response, { CampaignDetails: responseData }, request);
+      }
+      else {
+        return sendResponse(response, { CampaignDetails: [] }, request);
+      }
+    } catch (error) {
+      console.error('Error searching campaigns:', error);
+      return sendResponse(response, { CampaignDetails: [] }, request);
     }
-    var result: any, Job: any = { ingestionDetails: { userInfo: {}, projectType: request?.body?.HCMConfig?.projectType, projectTypeId: request?.body?.HCMConfig?.projectTypeId, projectName: request?.body?.HCMConfig?.campaignName, history: [], campaignDetails: campaignDetails } };
-    const saveHistory: any = Job.ingestionDetails;
-    logger.info("Saving campaign details : " + JSON.stringify(campaignDetails));
-    produceModifiedMessages(saveHistory, saveCampaignTopic);
+  };
+
+
+
+  processMicroplan = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
     try {
-      const { campaignType } = request?.body?.HCMConfig;
-      const campaign: any = await searchMDMS([campaignType], config.values.campaignType, request.body.RequestInfo, response);
-      if (!campaign.mdms || Object.keys(campaign.mdms).length === 0) {
-        throw new Error("Invalid Campaign Type");
+      await validateProcessMicroplan(request, response);
+      const campaignDetails = await getCampaignDetails(request?.body);
+      if (campaignDetails == "INVALID_CAMPAIGN_NUMBER") {
+        throw new Error("Error during Campaign Number generation");
       }
-      request.body.HCMConfig['transformTemplate'] = campaign?.mdms?.[0]?.data?.transformTemplate;
-      const parsingTemplates = campaign?.mdms?.[0]?.data?.parsingTemplates;
-      logger.info("ParsingTemplates : " + JSON.stringify(parsingTemplates))
-      const hostHcmBff = config.host.hcmBff.endsWith('/') ? config.host.hcmBff.slice(0, -1) : config.host.hcmBff;
-      result = await httpRequest(`${hostHcmBff}${config.app.contextPath}${'/bulk'}/_transform`, request.body, undefined, undefined, undefined, undefined);
-      if (result.updatedDatas.error) {
-        throw new Error(result.updatedDatas.error);
+      var result: any, Job: any = { ingestionDetails: { userInfo: {}, projectType: request?.body?.HCMConfig?.projectType, projectTypeId: request?.body?.HCMConfig?.projectTypeId, projectName: request?.body?.HCMConfig?.campaignName, history: [], campaignDetails: campaignDetails } };
+      const saveHistory: any = Job.ingestionDetails;
+      logger.info("Saving campaign details : " + JSON.stringify(campaignDetails));
+      produceModifiedMessages(saveHistory, saveCampaignTopic);
+      try {
+        const { campaignType } = request?.body?.HCMConfig;
+        const campaign: any = await searchMDMS([campaignType], config.values.campaignType, request.body.RequestInfo, response);
+        if (!campaign.mdms || Object.keys(campaign.mdms).length === 0) {
+          throw new Error("Invalid Campaign Type");
+        }
+        request.body.HCMConfig['transformTemplate'] = campaign?.mdms?.[0]?.data?.transformTemplate;
+        const parsingTemplates = campaign?.mdms?.[0]?.data?.parsingTemplates;
+        logger.info("ParsingTemplates : " + JSON.stringify(parsingTemplates))
+        const hostHcmBff = config.host.hcmBff.endsWith('/') ? config.host.hcmBff.slice(0, -1) : config.host.hcmBff;
+        result = await httpRequest(`${hostHcmBff}${config.app.contextPath}${'/bulk'}/_transform`, request.body, undefined, undefined, undefined, undefined);
+        if (result.updatedDatas.error) {
+          throw new Error(result.updatedDatas.error);
+        }
+        Job.ingestionDetails.campaignDetails.status = "Started"
+        Job.ingestionDetails.campaignDetails.auditDetails.lastModifiedTime = new Date().getTime();
+        produceModifiedMessages(Job.ingestionDetails, updateCampaignTopic);
+        await processFile(request, parsingTemplates, result, hostHcmBff, Job);
+      } catch (e: any) {
+        logger.error(String(e))
+        return errorResponder({ message: String(e) + "    Check Logs" }, request, response);
       }
-      Job.ingestionDetails.campaignDetails.status = "Started"
-      Job.ingestionDetails.campaignDetails.auditDetails.lastModifiedTime = new Date().getTime();
-      produceModifiedMessages(Job.ingestionDetails, updateCampaignTopic);
-      await processFile(request, parsingTemplates, result, hostHcmBff, Job);
+      Job.ingestionDetails.userInfo = request?.body?.RequestInfo?.userInfo;
+      Job.ingestionDetails.campaignDetails = campaignDetails;
+      const updatedJob: any = await produceIngestion({ Job });
+      return sendResponse(
+        response,
+        { Job: updatedJob },
+        request
+      );
     } catch (e: any) {
       logger.error(String(e))
-      return errorResponder({ message: String(e) + "    Check Logs" }, request, response);
+      return errorResponder({ message: String(e) }, request, response);
     }
-    Job.ingestionDetails.userInfo = request?.body?.RequestInfo?.userInfo;
-    Job.ingestionDetails.campaignDetails = campaignDetails;
-    const updatedJob: any = await produceIngestion({ Job });
-    return sendResponse(
-      response,
-      { Job: updatedJob },
-      request
-    );
-  } catch (e: any) {
-    logger.error(String(e))
-    return errorResponder({ message: String(e) }, request, response);
-  }
-};
+  };
 
 }
 // Export the MeasurementController class
