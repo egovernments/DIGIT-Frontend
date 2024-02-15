f020eb8422ee76aab0a36fd53c76152295e0cfcf
Navya <navya.bhatt@egovernments.org>
2024-02-15 22:45:59 +0530

    changes

--- micro-ui/web/packages/core/webpack.prod.js
+++ micro-ui/web/packages/core/webpack.prod.js
@@ -19,12 +19,12 @@ module.exports = () => {
           // auth: `auth@${domain}/auth/remoteEntry.js`,
           // header: `header@${domain}/header/remoteEntry.js`,
           // dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
-          // hrms: `hrms@${domain}/hrms/remoteEntry.js`,
+          hrms: `hrms@${domain}/hrms/remoteEntry.js`,
           workbench : `workbench@${domain}/workbench/remoteEntry.js`,
           // dss : `dss@${domain}/dss/remoteEntry.js`,
           // measurement : `measurement@${domain}/measurement/remoteEntry.js`
         },
-        shared: packageJson.dependencies,
+        // shared: packageJson.dependencies,
       }),
     ],
   };
