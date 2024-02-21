1a5f5afc9c044dba62f755eae2b3ac9cdb08cf29
Nipun Arora <aroranipun1@gmail.com>
2024-02-21 10:11:48 +0530

    Update webpack.prod.js

--- micro-ui/web/packages/common/webpack.prod.js
+++ micro-ui/web/packages/common/webpack.prod.js
@@ -25,7 +25,7 @@ module.exports = () => {
           'react-query': '3.6.1',
           'react-router-dom': '5.2.0',
           'single-spa-react': '^4.6.1',
-          webpack: '^5.68.0',
+          'webpack': '^5.68.0',
           'webpack-cli': '^4.9.2',
           'webpack-dev-server': '^4.8.1',
           'webpack-merge': '5.7.3',
