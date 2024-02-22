8471b9618069907bdcffc55abd61b361375962c7
hridya <hridya.s@egovernments.org>
2024-02-22 13:40:50 +0530

    create and search screen for sample module

---
+++ micro-ui/web/micro-ui-internals/packages/modules/sample/src/components/SampleCard.js
@@ -0,0 +1,50 @@
+import { HRIcon, EmployeeModuleCard, AttendanceIcon, PropertyHouse } from "@egovernments/digit-ui-react-components";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const SampleCard = () => {
+ 
+  const { t } = useTranslation();
+
+  const propsForModuleCard = {
+    Icon: <PropertyHouse />,
+    moduleName: t("Sample"),
+    kpis: [
+
+    ],
+    links: [
+   
+      {
+        label: t("Create"),
+        link: `/${window?.contextPath}/employee/sample/create`,
+      },
+      {
+        label: t("Inbox"),
+        link: `/${window?.contextPath}/employee/sample/inbox`,
+      },
+      {
+        label: t("Search"),
+        link: `/${window?.contextPath}/employee/sample/search`,
+      },
+      {
+        label: t("Advanced Form"),
+        link: `/${window?.contextPath}/employee/sample/advanced`,
+      },
+      {
+        label: t("Search individual"),
+        link: `/${window?.contextPath}/employee/sample/search-individual`,
+
+      },
+      {
+        label: t("Create Individual"),
+        link: `/${window?.contextPath}/employee/sample/create-individual`,
+
+      },
+      
+    ],
+  };
+
+  return <EmployeeModuleCard {...propsForModuleCard} />;
+};
+
+export default SampleCard;
