73e51f28ca7990985f507b62894469f90dd48d4c
Nipun Arora <aroranipun1@gmail.com>
2024-03-04 12:07:25 +0530

    Update webpack.prod.js

--- micro-ui/web/packages/core/webpack.prod.js
+++ micro-ui/web/packages/core/webpack.prod.js
@@ -20,7 +20,7 @@ module.exports = () => {
           // header: `header@${domain}/header/remoteEntry.js`,
           // dashboard: `dashboard@${domain}/dashboard/remoteEntry.js`,
           // hrms: `hrms@${domain}/hrms/remoteEntry.js`,
-          common: `common-ui@${domain}/common-ui/remoteEntry.js`,
+          common: `common@${domain}/common-ui/remoteEntry.js`,
           // workbench : `workbench_web@${domain}/workbench/remoteEntry.js`,
           // dss : `dss@${domain}/dss/remoteEntry.js`,
           // measurement : `measurement@${domain}/measurement/remoteEntry.js`
