a013e398994f9ae1a44f9943c6ae945692e4a099
Nipun Arora <aroranipun1@gmail.com>
2024-03-06 14:38:32 +0530

    updated hrms configs

--- micro-ui/web/packages/hrms/webpack.prod.js
+++ micro-ui/web/packages/hrms/webpack.prod.js
@@ -1,27 +1,40 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("./package.json");
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('./package.json');
 
 module.exports = () => {
   const prodConfig = {
-    mode: "production",
+    mode: 'production',
     output: {
-      publicPath: "/hrms-ui/",
-      filename: "[name].[contenthash].js",
+      publicPath: '/hrms-ui/',
+      filename: '[name].[contenthash].js',
     },
     plugins: [
       new ModuleFederationPlugin({
-        name: "hrms",
-        filename: "remoteEntry.js",
+        name: 'hrms',
+        filename: 'remoteEntry.js',
         exposes: {
-          "./HRMSModule": "./src/SingleSpaEntry",
+          './HRMSModule': './src/SingleSpaEntry',
         },
-        // shared: packageJson.dependencies, //removed the shared logic for now will be enabled later for optimization
+        shared: {
+          'single-spa-react': '^4.6.1',
+          'react': '17.0.1',
+          'react-dom': '17.0.1',
+          'react-router-dom': '5.2.0',
+          'rxjs': '6.6.3',
+          'react-hook-form': '6.15.8',
+          'react-i18next': '11.16.2',
+          'react-query': '3.6.1',
+          'rooks': '^4.4.0',
+          'webpack': '^5.68.0',
+          'webpack-cli': '^4.9.2',
+          'webpack-dev-server': '^4.8.1',
+          'webpack-merge': '5.7.3',
+        },
       }),
     ],
   };
 
   return merge(commonConfig, prodConfig);
-
-}
+};
