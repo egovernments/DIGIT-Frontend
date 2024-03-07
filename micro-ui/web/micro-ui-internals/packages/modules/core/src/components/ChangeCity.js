6b4a2e238929d6a8522fb88a25e4785f8c34c748
hridya <hridya.s@egovernments.org>
2024-03-07 09:56:49 +0530

    Searchable Dropdown for city

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/components/ChangeCity.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/components/ChangeCity.js
@@ -2,7 +2,8 @@ import { Dropdown } from "@egovernments/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-const stringReplaceAll = (str = "", searcher = "", replaceWith = "") => {
+const stringReplaceAll = (str = "", searcher = "", replaceWith = "") =>
+ {
   if (searcher == "") return str;
   while (str?.includes(searcher)) {
     str = str?.replace(searcher, replaceWith);
@@ -11,12 +12,18 @@ const stringReplaceAll = (str = "", searcher = "", replaceWith = "") => {
 };
 
 const ChangeCity = (prop) => {
-  const [dropDownData, setDropDownData] = useState(null);
+  const [dropDownData, setDropDownData] = useState({
+    label: `TENANT_TENANTS_${stringReplaceAll(Digit.SessionStorage.get("Employee.tenantId"), ".", "_")?.toUpperCase()}`,
+    value: Digit.SessionStorage.get("Employee.tenantId"),
+  });
   const [selectCityData, setSelectCityData] = useState([]);
-  const [selectedCity, setSelectedCity] = useState([]); //selectedCities?.[0]?.value
+  const [searchQuery, setSearchQuery] = useState(""); // State to store search query
   const history = useHistory();
   const isDropdown = prop.dropdown || false;
   let selectedCities = [];
+
+  const { data: data = {}, isLoading } =
+    Digit.Hooks.hrms.useHrmsMDMS(Digit.ULBService.getCurrentTenantId(), "egov-hrms", "HRMSRolesandDesignation") || {};
 
   const handleChangeCity = (city) => {
     const loggedInData = Digit.SessionStorage.get("citizen.userRequestObject");
@@ -41,52 +48,51 @@ const ChangeCity = (prop) => {
       filteredArray = [];
     userloggedValues?.info?.roles?.forEach((role) => teantsArray.push(role.tenantId));
     let unique = teantsArray.filter((item, i, ar) => ar.indexOf(item) === i);
+
     unique?.forEach((uniCode) => {
-      filteredArray.push({
-        label: `TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`,
-        value: uniCode,
+      data?.MdmsRes?.["tenant"]["tenants"]?.map((items) => {
+        if (items?.code !== "pb" && items?.code === uniCode) {
+          filteredArray.push({
+            label: `${prop?.t(Digit.Utils.locale.convertToLocale(items?.divisionCode, "EGOV_LOCATION_DIVISION"))} - ${prop?.t(
+              `TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`
+            )}`,
+            value: uniCode,
+          });
+        } else if (items?.code === "pb" && items?.code === uniCode) {
+          filteredArray.push({
+            label: `TENANT_TENANTS_${stringReplaceAll(uniCode, ".", "_")?.toUpperCase()}`,
+            value: uniCode,
+          });
+        }
       });
     });
     selectedCities = filteredArray?.filter((select) => select.value == Digit.SessionStorage.get("Employee.tenantId"));
     setSelectCityData(filteredArray);
-  }, [dropDownData]);
+  }, [dropDownData, data?.MdmsRes]);
 
-  // if (isDropdown) {
+  // Filter options based on search query
+  const filteredOptions = selectCityData.filter(option =>
+    option.label.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div style={prop?.mobileView ? { color: "#767676" } : {}}>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <Dropdown
         t={prop?.t}
-        option={selectCityData}
-        selected={selectCityData.find((cityValue) => cityValue.value === dropDownData?.value)}
+        style={{ width: "150px" }}
+        option={filteredOptions}
+        selected={dropDownData}
         optionKey={"label"}
         select={handleChangeCity}
-        freeze={true}
-        customSelector={
-          <label className="cp">
-            {prop?.t(`TENANT_TENANTS_${stringReplaceAll(Digit.SessionStorage.get("Employee.tenantId"), ".", "_")?.toUpperCase()}`)}
-          </label>
-        }
+        optionCardStyles={{ overflow: "revert", display: "table" }}
       />
     </div>
   );
-  // } else {
-  //   return (
-  //     <React.Fragment>
-  //       <div style={{ marginBottom: "5px" }}>City</div>
-  //       <div className="language-selector" style={{display: "flex", flexWrap: "wrap"}}>
-  //         {selectCityData?.map((city, index) => (
-  //           <div className="language-button-container" key={index}>
-  //             <CustomButton
-  //               selected={city.value === Digit.SessionStorage.get("Employee.tenantId")}
-  //               text={city.label}
-  //               onClick={() => handleChangeCity(city)}
-  //             ></CustomButton>
-  //           </div>
-  //         ))}
-  //       </div>
-  //     </React.Fragment>
-  //   );
-  // }
 };
 
 export default ChangeCity;
