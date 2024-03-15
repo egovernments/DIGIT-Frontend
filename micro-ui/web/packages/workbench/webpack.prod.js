f6f53c6e41b5a14f0b33cce07e660567c56ac2a4
Nipun Arora <aroranipun1@gmail.com>
2024-03-15 15:11:45 +0530

    updated webpack conf

--- micro-ui/web/packages/workbench/webpack.prod.js
+++ micro-ui/web/packages/workbench/webpack.prod.js
@@ -17,7 +17,26 @@ module.exports = () => {
         exposes: {
           "./WorkbenchModule": "./src/SingleSpaEntry",
         },
-        shared: packageJson.dependencies
+        shared: {
+          "single-spa-react": "^4.6.1",
+          "xlsx": "0.17.5",
+          "@egovernments/digit-ui-react-components":"1.4.108",
+          "@rjsf/core": "5.10.0",
+          "@rjsf/utils": "5.10.0",
+          "@rjsf/validator-ajv8": "5.10.0",
+          "react": "17.0.2",
+          "react-date-range": "1.4.0",
+          "react-dom": "17.0.2",
+          "react-hook-form": "6.15.8",
+          "react-i18next": "11.16.2",
+          "react-query": "3.6.1",
+          "react-router-dom": "5.3.0",
+          "react-select": "5.7.4",
+          "webpack": "^5.68.0",
+          "webpack-cli": "^4.9.2",
+          "webpack-dev-server": "^4.8.1",
+          "webpack-merge": "5.7.3",
+        }
       }),
     ],
   };
