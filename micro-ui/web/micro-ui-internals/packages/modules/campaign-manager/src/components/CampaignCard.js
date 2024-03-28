2b6f24836e3f2f691308c0c304d8579acb895af5
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-03-28 19:45:59 +0530

    merged

---
+++ micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/CampaignCard.js
@@ -0,0 +1,58 @@
+import { EmployeeModuleCard, ArrowRightInbox, WorksMgmtIcon } from "@egovernments/digit-ui-react-components";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const ROLES = {
+  LOCALISATION: ["EMPLOYEE", "SUPERUSER", "EMPLOYEE_COMMON", "LOC_ADMIN"],
+  MDMS: ["MDMS_ADMIN", "EMPLOYEE", "SUPERUSER"],
+  DSS: ["STADMIN"],
+};
+
+const CampaignCard = () => {
+  // if (!Digit.Utils.didEmployeeHasAtleastOneRole(Object.values(ROLES).flatMap((e) => e))) {
+  // return null;
+  // }
+
+  const { t } = useTranslation();
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+
+  let links = [
+    {
+      label: t("ACTION_TEST_CAMPAIGN_HOME"),
+      link: `/${window?.contextPath}/employee/campaign/sample`,
+      roles: [], // @nabeel roles to be added later
+    },
+    {
+      label: t("ACTION_TEST_CAMPAIGN_CYCLE_CONFIGURE"),
+      link: `/${window?.contextPath}/employee/campaign/create-campaign/cycle-configure`,
+      roles: [], // @nabeel roles to be added later
+    },
+    {
+      label: t("ACTION_TEST_CAMPAIGN_CYCLE"),
+      link: `/${window?.contextPath}/employee/campaign/create-campaign/delivery-details`,
+      roles: [], // @nabeel roles to be added later
+    },
+    {
+      label: t("ACTION_TEST_SETUP_CAMPAIGN"),
+      link: `/${window?.contextPath}/employee/campaign/setup-campaign`,
+      roles: [], // @nabeel roles to be added later
+    },
+    // {
+      // label: t("ACTION_TEST_MY_CAMPAIGN"),
+      // link: `/${window?.contextPath}/employee/campaign/my-campaign`,
+      // roles: [], // @nabeel roles to be added later
+    // },
+  ];
+
+  links = links.filter((link) => (link?.roles && link?.roles?.length > 0 ? Digit.Utils.didEmployeeHasAtleastOneRole(link?.roles) : true));
+
+  const propsForModuleCard = {
+    Icon: <WorksMgmtIcon />,
+    moduleName: t("ACTION_TEST_CAMPAIGN"),
+    kpis: [],
+    links: links,
+  };
+  return <EmployeeModuleCard {...propsForModuleCard} />;
+};
+
+export default CampaignCard;
---
+++ micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/configs/CampaignConfig.js
@@ -0,0 +1,204 @@
+export const CampaignConfig = (totalFormData) => {
+  return [
+    {
+      form: [
+        {
+          stepCount: "1",
+          key: "1",
+          name: "HCM_CAMPAIGN_DATE",
+          body: [
+            {
+              isMandatory: false,
+              key: "campaignDates",
+              type: "component",
+              component: "CampaignDates",
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+              },
+              populators: {
+                name: "campaignDates",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "1",
+          key: "2",
+          name: "HCM_CAMPAIGN_TYPE",
+          body: [
+            {
+              isMandatory: false,
+              key: "projectType",
+              type: "component",
+              component: "CampaignType",
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+                sessionData: totalFormData,
+              },
+              populators: {
+                name: "projectType",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "1",
+          key: "3",
+          name: "HCM_CAMPAIGN_NAME",
+          body: [
+            {
+              isMandatory: false,
+              key: "campaignName",
+              type: "component",
+              component: "CampaignName",
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+                sessionData: totalFormData,
+              },
+              populators: {
+                name: "campaignName",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "2",
+          key: "4",
+          name: "HCM_CAMPAIGN_CYCLE_CONFIGURE",
+          body: [
+            {
+              isMandatory: false,
+              key: "cycleConfigure",
+              type: "component",
+              component: "CycleConfiguration",
+              withoutLabelFieldPair: true,
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+              },
+              populators: {
+                name: "cycleConfiguration",
+                sessionData: totalFormData,
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "2",
+          key: "5",
+          name: "HCM_CAMPAIGN_DELIVERY_DATA",
+          body: [
+            {
+              isMandatory: false,
+              key: "deliveryRule",
+              type: "component",
+              component: "DeliveryRule",
+              withoutLabelFieldPair: true,
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+                sessionData: totalFormData,
+              },
+              populators: {
+                name: "deliveryRule",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "3",
+          key: "6",
+          body: [
+            {
+              isMandatory: false,
+              key: "campaignName3",
+              type: "text",
+              withoutLabel: false,
+              label: "boundary",
+              disable: false,
+              customProps: {
+                module: "HCM",
+              },
+              populators: {
+                name: "campaignName3",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "4",
+          key: "7",
+          body: [
+            {
+              isMandatory: false,
+              key: "uploadBoundary",
+              type: "component",
+              component: "UploadBoundaryData",
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+              },
+              populators: {
+                name: "uploadBoundary",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+        {
+          stepCount: "5",
+          key: "8",
+          isLast: true,
+          body: [
+            {
+              isMandatory: false,
+              key: "campaignType",
+              type: "component",
+              component: "CampaignType",
+              withoutLabel: true,
+              disable: false,
+              customProps: {
+                module: "HCM",
+              },
+              populators: {
+                name: "campaignType",
+                // optionsKey: "code",
+                error: "ES__REQUIRED",
+                required: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ];
+};
