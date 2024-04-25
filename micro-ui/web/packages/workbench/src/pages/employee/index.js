615451211bf537feca59a3c8ec3d789930a06fd2
Nipun Arora <aroranipun1@gmail.com>
2024-04-25 14:35:55 +0530

    Update index.js

--- micro-ui/web/packages/workbench/src/pages/employee/index.js
+++ micro-ui/web/packages/workbench/src/pages/employee/index.js
@@ -70,7 +70,7 @@ const App = ({ path }) => {
   const { isLoading } = Digit.Hooks.core.useLocalization({
     params:{
       tenantId: Digit.ULBService.getCurrentTenantId(),
-      module: 'rainmaker-workbench,rainmaker-mdms',
+      module: 'rainmaker-workbench,rainmaker-mdms,rainmaker-schema',
       locale:i18n.language,
     },
     i18n,
