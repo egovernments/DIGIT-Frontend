10b99a15db24c88cc133633d088e431a64c1a9e9
nabeelmd-eGov <nabeel.md@egovernments.com>
2024-05-03 02:20:00 +0530

    add product page redirection

--- micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/AddProduct.js
+++ micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/AddProduct.js
@@ -65,7 +65,7 @@ function AddProduct() {
               message: "ES_PRODUCT_CREATE_SUCCESS_RESPONSE",
               text: "ES_PRODUCT_CREATE_SUCCESS_RESPONSE_TEXT",
               actionLabel: "ES_PRODUCT_RESPONSE_ACTION",
-              actionLink: `/${window.contextPath}/employee/campaign/setup-campaign?id=${state?.campaignId}&draft=true`,
+              actionLink: `/${window.contextPath}/employee/campaign/setup-campaign${state?.urlParams}`,
             });
           },
         });
