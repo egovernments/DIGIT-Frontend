5c68568582d652d6c7d0a434d0ff64fe0b6e963b
Nipun Arora <aroranipun1@gmail.com>
2024-05-08 01:37:44 +0530

    Update constants.js

--- micro-ui/web/micro-ui-internals/packages/modules/hcm-microplanning/src/configs/constants.js
+++ micro-ui/web/micro-ui-internals/packages/modules/hcm-microplanning/src/configs/constants.js
@@ -1,15 +1,15 @@
-export const LOCALITY = "Locality"
+export const LOCALITY = "Locality";
 
-export const EXCEL = "Excel"
+export const EXCEL = "Excel";
 
-export const GEOJSON = "GeoJSON"
+export const GEOJSON = "GeoJSON";
 
-export const SHAPEFILE = "Shapefile"
+export const SHAPEFILE = "Shapefile";
 
 export const commonColumn = "boundaryCode";
 
 export const ACCEPT_HEADERS = {
-  GeoJSON:"application/geo+json",
-  Shapefile:"application/shapefile",
-  Excel:"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-}
+  GeoJSON: "application/geo+json",
+  Shapefile: "application/shapefile",
+  Excel: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+};
