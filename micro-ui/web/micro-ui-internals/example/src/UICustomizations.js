615b5d86c618abae5a998213fd9d25b3ccab3298
Nipun Arora <aroranipun1@gmail.com>
2024-05-09 18:33:58 +0530

    updated status for campaign search

--- micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
+++ micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
@@ -216,7 +216,7 @@ export const UICustomizations = {
       data.body.CampaignDetails.tenantId = Digit.ULBService.getCurrentTenantId();
       // data.body.CampaignDetails.boundaryCode = boundaryCode;
       data.body.CampaignDetails.campaignName = campaignName;
-      data.body.CampaignDetails.status = "started"
+      data.body.CampaignDetails.status = ["started","In Progress"]
       if (startDate) {
         data.body.CampaignDetails.startDate = Digit.Utils.date.convertDateToEpoch(startDate);
       }
