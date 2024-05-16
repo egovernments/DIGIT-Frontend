a416f796afa449ac5c83e1071df8741f77712dcf
Nipun Arora <aroranipun1@gmail.com>
2024-05-16 13:42:16 +0530

    updated packages and webpack conf

--- micro-ui/web/packages/core/webpack.dev.js
+++ micro-ui/web/packages/core/webpack.dev.js
@@ -53,6 +53,7 @@ module.exports = () => {
           ...packageJson.dependencies,
           react: { singleton: true }, // React will be shared as a singleton
           'react-dom': { singleton: true }, // ReactDOM will be shared as a singleton
+          'react-query': { singleton: true },
         },
       }),
     ],
