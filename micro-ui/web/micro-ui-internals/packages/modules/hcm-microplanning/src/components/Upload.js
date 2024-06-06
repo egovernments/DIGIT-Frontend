3ba114704a942227dacbba91438479ff82a310ad
siddhant-nawale-egov <162107530+siddhant-nawale-egov@users.noreply.github.com>
2024-06-06 11:56:28 +0530

    Update Upload.js

--- micro-ui/web/micro-ui-internals/packages/modules/hcm-microplanning/src/components/Upload.js
+++ micro-ui/web/micro-ui-internals/packages/modules/hcm-microplanning/src/components/Upload.js
@@ -301,38 +301,38 @@ const Upload = ({
     if (schemaData && !schemaData.doHierarchyCheckInUploadedData) {
       try {
         let boundaryDataAgainstBoundaryCode = {};
-        const rootBoundary = campaignData?.boundaries?.filter((boundary) => boundary.isRoot); // Retrieve session storage data once and store it in a variable
-        const sessionData = Digit.SessionStorage.get("microplanHelperData") || {};
-        let boundaryData = sessionData.filteredBoundaries;
-        let filteredBoundaries;
-        if (!boundaryData) {
-          // Only fetch boundary data if not present in session storage
-          boundaryData = await fetchBoundaryData(Digit.ULBService.getCurrentTenantId(), campaignData?.hierarchyType, rootBoundary?.[0]?.code);
-          filteredBoundaries = filterBoundaries(boundaryData, campaignData?.boundaries);
+        // const rootBoundary = campaignData?.boundaries?.filter((boundary) => boundary.isRoot); // Retrieve session storage data once and store it in a variable
+        // const sessionData = Digit.SessionStorage.get("microplanHelperData") || {};
+        // let boundaryData = sessionData.filteredBoundaries;
+        // let filteredBoundaries;
+        // if (!boundaryData) {
+        //   // Only fetch boundary data if not present in session storage
+        //   boundaryData = await fetchBoundaryData(Digit.ULBService.getCurrentTenantId(), campaignData?.hierarchyType, rootBoundary?.[0]?.code);
+        //   filteredBoundaries = filterBoundaries(boundaryData, campaignData?.boundaries);
 
-          // Update the session storage with the new filtered boundaries
-          Digit.SessionStorage.set("microplanHelperData", {
-            ...sessionData,
-            filteredBoundaries: filteredBoundaries,
-          });
-        } else {
-          filteredBoundaries = boundaryData;
-        }
-        const xlsxData = addBoundaryData([], filteredBoundaries)?.[0]?.data;
-        xlsxData.forEach((item, i) => {
-          if (i === 0) return;
-          let boundaryCodeIndex = xlsxData?.[0]?.indexOf(commonColumn);
-          if (boundaryCodeIndex >= item.length) {
-            // If boundaryCodeIndex is out of bounds, return the item as is
-            boundaryDataAgainstBoundaryCode[item[boundaryCodeIndex]] = item.slice().map(t);
-          } else {
-            // Otherwise, remove the element at boundaryCodeIndex
-            boundaryDataAgainstBoundaryCode[item[boundaryCodeIndex]] = item
-              .slice(0, boundaryCodeIndex)
-              .concat(item.slice(boundaryCodeIndex + 1))
-              .map(t);
-          }
-        });
+        //   // Update the session storage with the new filtered boundaries
+        //   Digit.SessionStorage.set("microplanHelperData", {
+        //     ...sessionData,
+        //     filteredBoundaries: filteredBoundaries,
+        //   });
+        // } else {
+        //   filteredBoundaries = boundaryData;
+        // }
+        // const xlsxData = addBoundaryData([], filteredBoundaries)?.[0]?.data;
+        // xlsxData.forEach((item, i) => {
+        //   if (i === 0) return;
+        //   let boundaryCodeIndex = xlsxData?.[0]?.indexOf(commonColumn);
+        //   if (boundaryCodeIndex >= item.length) {
+        //     // If boundaryCodeIndex is out of bounds, return the item as is
+        //     boundaryDataAgainstBoundaryCode[item[boundaryCodeIndex]] = item.slice().map(t);
+        //   } else {
+        //     // Otherwise, remove the element at boundaryCodeIndex
+        //     boundaryDataAgainstBoundaryCode[item[boundaryCodeIndex]] = item
+        //       .slice(0, boundaryCodeIndex)
+        //       .concat(item.slice(boundaryCodeIndex + 1))
+        //       .map(t);
+        //   }
+        // });
         return boundaryDataAgainstBoundaryCode;
       } catch (error) {
         console.error(error?.message);
