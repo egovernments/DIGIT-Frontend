008158984b95e37010f7d7eecfbdf86664a6a460
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-06-13 17:03:53 +0530

    workbench-mdms-v2

--- micro-ui/web/workbench/webpack.config.js
+++ micro-ui/web/workbench/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/workbench-ui/",
+    publicPath: "/workbench-mdms-v2/",
   },
   optimization: {
     splitChunks: {
