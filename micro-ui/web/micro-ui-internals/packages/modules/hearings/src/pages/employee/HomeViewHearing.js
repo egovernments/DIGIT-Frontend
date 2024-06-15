af1f2807e98d48a54665046f2f8c47c4c452cdec
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2024-06-15 11:47:31 +0530

    Added UpComing Hearing Panel Card

--- micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/HomeViewHearing.js
+++ micro-ui/web/micro-ui-internals/packages/modules/hearings/src/pages/employee/HomeViewHearing.js
@@ -1,17 +1,18 @@
 import { useTranslation } from "react-i18next";
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Header, InboxSearchComposer, FormComposerV2 } from "@egovernments/digit-ui-react-components";
-import { TabSearchconfig } from "../../configs/HearingsHomeConfig";
+import { Header, InboxSearchComposer ,FormComposerV2} from "@egovernments/digit-ui-react-components";
+import { TabSearchconfig } from "../../configs/HearingsHomeConfig"; 
+import UpcomingHearings from "../../components/UpComingHearing";
 
-const fieldStyle = { marginRight: 0 };
+const fieldStyle={ marginRight: 0 };
 
 
 const defaultSearchValues = {
-  individualName: "",
-  mobileNumber: "",
-  IndividualID: "",
-};
+    individualName: "",
+    mobileNumber: "",
+    IndividualID: "",
+  };
 
 const HomeViewHearing = () => {
 
@@ -26,12 +27,12 @@ const HomeViewHearing = () => {
     // Set default values when component mounts
     setDefaultValues(defaultSearchValues);
   }, []);
-
+  
   const onTabChange = (n) => {
     setTabData((prev) => prev.map((i, c) => ({ ...i, active: c === n ? true : false }))); //setting tab enable which is being clicked
     setConfig(TabSearchconfig?.TabSearchconfig?.[n]);// as per tab number filtering the config
   };
-
+  
 
   const handleNavigate = () => {
     const contextPath = window?.contextPath || ''; // Adjust as per your context path logic
@@ -40,33 +41,21 @@ const HomeViewHearing = () => {
 
   return (
     <div>
-      <button className="redirect-button" onClick={handleNavigate}
-        style={{
-          backgroundColor: 'blue',
-          color: 'white',
-          padding: '10px 20px',
-          borderRadius: '5px',
-          border: 'none',
-          cursor: 'pointer',
-          margin: '2px'
-
-        }}>
-
-        View Hearing
-      </button>
-      <React.Fragment>
-        <Header styles={{ fontSize: "32px" }}>{t(config?.label)}</Header>
-        <div className="inbox-search-wrapper">
-          {/* Pass defaultValues as props to InboxSearchComposer */}
-          <InboxSearchComposer
-            configs={config}
-            defaultValues={defaultValues}
-            showTab={true}
-            tabData={tabData}
-            onTabChange={onTabChange}
-          ></InboxSearchComposer>
-        </div>
-      </React.Fragment>
+  
+    <UpcomingHearings handleNavigate={handleNavigate}></UpcomingHearings>
+    <React.Fragment>
+    <Header styles={{ fontSize: "32px" }}>{t(config?.label)}</Header>
+    <div className="inbox-search-wrapper">
+      {/* Pass defaultValues as props to InboxSearchComposer */}
+      <InboxSearchComposer
+        configs={config}
+        defaultValues={defaultValues}
+        showTab={true}
+        tabData={tabData}
+        onTabChange={onTabChange}
+      ></InboxSearchComposer>
+    </div>
+  </React.Fragment>
 
 
     </div>
