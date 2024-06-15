4e0b93a67e56579bffa0be63b5acd7db8b2229d7
Bhavya-egov <137176879+Bhavya-egov@users.noreply.github.com>
2024-06-15 12:40:16 +0530

    changed document upload prop (#889)
    * changed document upload prop
    
    * added in the formComposer

--- micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/UploadFileComposer.js
+++ micro-ui/web/micro-ui-internals/packages/react-components/src/hoc/UploadFileComposer.js
@@ -9,14 +9,14 @@ import { Loader } from '../atoms/Loader'
 import MultiUploadWrapper from '../molecules/MultiUploadWrapper'
 import TextInput from '../atoms/TextInput'
 
-const UploadFileComposer = ({module, config, Controller, control, register, formData, errors, localePrefix, customClass, customErrorMsg}) => {
+const UploadFileComposer = ({module, config, Controller, control, register, formData, errors, localePrefix, customClass, customErrorMsg,mdmsModuleName='works'}) => {
   const { t } = useTranslation()
   
   //fetch mdms config based on module name
   const tenant = Digit.ULBService.getStateId();
   const { isLoading, data } = Digit.Hooks.useCustomMDMS(
       tenant,
-      "works",
+      mdmsModuleName,
       [
           {
               "name": "DocumentConfig",
