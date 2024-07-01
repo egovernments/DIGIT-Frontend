cf144a669a051aed92749e1213d82cc4319b894d
Bhavya-egov <137176879+Bhavya-egov@users.noreply.github.com>
2024-07-01 12:11:04 +0530

    Update SelectingBoundaries.js

--- micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/SelectingBoundaries.js
+++ micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/SelectingBoundaries.js
@@ -52,8 +52,6 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
   const isDraft = searchParams.get("draft");
   const draftBoundary = searchParams.get("draftBoundary");
 
-  console.log("lowest," , lowestHierarchy)
-
   function updateUrlParams(params) {
     const url = new URL(window.location.href);
     Object.entries(params).forEach(([key, value]) => {
