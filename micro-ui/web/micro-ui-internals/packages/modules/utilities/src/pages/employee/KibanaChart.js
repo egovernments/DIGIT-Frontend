814b22854b905605adacc15b2457cd802426b17b
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-07-10 12:49:51 +0530

    Update micro-ui/web/micro-ui-internals/packages/modules/utilities/src/pages/employee/KibanaChart.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- micro-ui/web/micro-ui-internals/packages/modules/utilities/src/pages/employee/KibanaChart.js
+++ micro-ui/web/micro-ui-internals/packages/modules/utilities/src/pages/employee/KibanaChart.js
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import IFrameInterface from "./IFrameInterface";
 
 const KibanaChart = ({ stateCode }) => {
-  const { stateCode } = props;
   const { moduleName, pageName } = useParams();
 
   return <IFrameInterface moduleName={moduleName} pageName={pageName} stateCode={stateCode} />;
