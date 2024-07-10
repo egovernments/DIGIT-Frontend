2416eff80f6c59fe58aa2ad63d1c465b766c4880
nabeelmd-eGov <nabeel.md@egovernments.com>
2024-07-10 12:46:08 +0530

    Added Kibana chart and version upgraded

---
+++ micro-ui/web/micro-ui-internals/packages/modules/utilities/src/pages/employee/KibanaChart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import IFrameInterface from "./IFrameInterface";
+
+const KibanaChart = ({ stateCode }) => {
+  const { stateCode } = props;
+  const { moduleName, pageName } = useParams();
+
+  return <IFrameInterface moduleName={moduleName} pageName={pageName} stateCode={stateCode} />;
+};
+
+export default KibanaChart;
