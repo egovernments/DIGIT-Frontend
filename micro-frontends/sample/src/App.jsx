497cb8604ef3fe815d8ee8e0953d8e70f9496cce
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-07-12 10:12:20 +0530

    other cleanups

--- micro-frontends/sample/src/App.jsx
+++ micro-frontends/sample/src/App.jsx
@@ -1,20 +1,8 @@
 import "./App.css";
-import Users from "./examples/Users";
 import Theme from "./examples/Theme";
-import Action from "./examples/Action";
-import Optimistic from "./examples/Optimistic";
-import FormStatus from "./examples/FormStatus";
-import FormState from "./examples/FormState";
-// import Test from "./pages/test";
 import StatusBar from "./components/statusBar";
-import { isOnline, onNetworkChange } from "./idb/networkStatus";
-// import TestIdb from "./pages/testIdb";
-// import TabForm from "./pages/TabForm";
-// import StepperFormScreen from "./pages/StepperForm";
 import LanguageSwitcher from "./components/LanguageSelector";
 import MyComponent from "./components/SampleComponent";
-// import TestIdb from "./pages/TestIdb.jsx";
-// import { isOnline, onNetworkChange } from './networkStatus';
 import { useQuery } from "@tanstack/react-query";
 import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
@@ -92,7 +80,9 @@ function App() {
                   <ul>
                     {routes?.map((route) => (
                       <li>
-                        <Link to={`${basePath}${route?.url}`}>{route?.url}</Link>
+                        <Link to={`${basePath}${route?.url}`}>
+                          {route?.url}
+                        </Link>
                       </li>
                     ))}
                   </ul>
@@ -105,28 +95,19 @@ function App() {
                         ? withAuth(route?.component)
                         : route?.component;
                       return (
-                        <Route path={`${basePath}${route?.url}`} element={<Component />} />
+                        <Route
+                          path={`${basePath}${route?.url}`}
+                          element={<Component />}
+                        />
                       );
                     })}
                   </Routes>
                 </Suspense>
               </div>
             </Router>
-
-            {/* <Sample></Sample>
-          <Test></Test>
-       <TestIdb></TestIdb>
           </Theme>
-          <Theme >
-<TabForm></TabForm>
-</Theme>
-<Theme >
-<StepperFormScreen></StepperFormScreen> */}
-          </Theme>
-
           <LanguageSwitcher />
           <MyComponent />
-
           <StatusBar></StatusBar>
         </Suspense>
       </div>
--- micro-frontends/sample/src/configs/i18.js
+++ micro-frontends/sample/src/configs/i18.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 // import LanguageDetector from 'i18next-browser-languagedetector'; "i18next-browser-languagedetector": "^8.0.0", we dont need as of now
 import HttpBackend from 'i18next-http-backend';
+const basePath = import.meta.env.BASE_URL;
 
 i18n
   .use(HttpBackend) // we need to have backend else we get cant load resources error
@@ -14,7 +15,7 @@ i18n
       escapeValue: false, // React already safes from xss
     },
     backend: {
-      loadPath: '/sample-ui/locales/{{lng}}/{{ns}}.json',
+      loadPath: `${basePath}locales/{{lng}}/{{ns}}.json`,
     },
   });
 
