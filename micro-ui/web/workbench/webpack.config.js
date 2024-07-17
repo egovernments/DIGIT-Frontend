511fd7a4e78d035fd068f6adc59eac48d5a1dfa3
Tulika-eGov <tulika.kundu@egovernments.org>
2024-07-17 10:56:23 +0530

    Updated the namespace from mukta-workbench to workbench

--- micro-ui/web/workbench/webpack.config.js
+++ micro-ui/web/workbench/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: "/mukta-workbench-ui/",
+    publicPath: "/workbench-ui/",
   },
   optimization: {
     splitChunks: {
