fda6e9fd8bf8090834b211cda16d3a3e4187cd55
Swathi-eGov <swathi.chatrathi@egovernments.org>
2024-07-18 15:04:57 +0530

    added restricion for ViewAll

--- micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/MultiSelect.js
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/MultiSelect.js
@@ -155,7 +155,7 @@ const CustomSelectWidget = (props) => {
 
   const handleViewMoreClick = (detail) => {
     const schemaCode = detail?.schemaCode;
-    const [moduleName, masterName] = schemaCode.split(".");
+    const [moduleName, masterName] = schemaCode?.split(".");
     const uniqueIdentifier = detail?.uniqueIdentifier;
     history.push(
       `/${window.contextPath}/employee/workbench/mdms-view?moduleName=${moduleName}&masterName=${masterName}&uniqueIdentifier=${uniqueIdentifier}`
@@ -209,7 +209,7 @@ const CustomSelectWidget = (props) => {
         isMulti={multiple}
         styles={customStyles}
         // components={isSeeAll ? { MenuList: SelectMenuButton, Option: OptionWithInfo } : { Option: OptionWithInfo }}
-        components={{ MenuList: SelectMenuButton, Option: OptionWithInfo }}
+        components={schema?.schemaCode ? { MenuList: SelectMenuButton, Option: OptionWithInfo } :  { Option: OptionWithInfo }}
       />
 
       <div className="info-icon-container">
@@ -295,7 +295,7 @@ const CustomSelectWidget = (props) => {
           <div className="modal-wrapper">
             <div className="modal-content">
               <div className="modal-inner">
-                <MDMSSearchv2Popup masterNameInherited={schema.schemaCode.split(".")[1]} moduleNameInherited={schema.schemaCode.split(".")[0]} onClickSelect={onClickSelect} />
+                <MDMSSearchv2Popup masterNameInherited={schema?.schemaCode?.split(".")[1]} moduleNameInherited={schema?.schemaCode?.split(".")[0]} onClickSelect={onClickSelect} />
               </div>
               <Button label={"Close"} onButtonClick={handleCloseModal}></Button>
             </div>
