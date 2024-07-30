1f55c4ecc4c6d682dcaa61eaf226fa840c3a8f2c
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-07-30 16:40:01 +0530

    deleted the app

--- micro-frontends/react-18/components/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Button } from './atoms/Button';
-export { default as Text } from './atoms/Text';
-export { default as Checkbox } from './atoms/Checkbox';
--- micro-frontends/react-18/react-app-one/src/root.component.js
+++
@@ -1,3 +0,0 @@
-export default function Root(props) {
-  return <section>{props.name} hey react-app-1</section>;
-}
