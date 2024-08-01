09df6e9ff241502b2f5cf47036de6b8200e6b95f
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-08-01 11:18:09 +0530

    deleted components

--- micro-frontends/react-18/components/src/atoms/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = () => {
-  return <button>Click </button>;
-};
-
-export default Button;
--- micro-frontends/react-18/components/src/atoms/Text.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Text = () => {
-  return <p>Testing test Component.</p>;
-};
-
-export default Text;
