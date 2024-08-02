0cdc66a2998055725ec5dfb3bb60039faea02394
Bhavya-egov <bhavya.mangal@egovernments.org>
2024-08-02 18:03:17 +0530

    changes privacy schema

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/components/PrivacyComponent.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/components/PrivacyComponent.js
@@ -8,16 +8,12 @@ const PrivacyComponent = ({ onSelect, formData, control, formState, ...props }) 
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const [isChecked, setIsChecked] = useState(false);
   const [showPopUp, setShowPopUp] = useState(false);
-  const { data: privacy } = Digit.Hooks.useCustomMDMS(
-    tenantId,
-    "commonUiConfig",
-    [{ name: "PrivacyPolicy" }],
-    {
-      select: (data) => {
-        return data?.commonUiConfig?.PrivacyPolicy?.[0]?.texts?.[0];
-      },
-    }
-  );
+  const { data: privacy } = Digit.Hooks.useCustomMDMS(tenantId, "commonUiConfig", [{ name: "PrivacyPolicy" }], {
+    select: (data) => {
+      const filteredPrivacyPolicy = data?.commonUiConfig?.PrivacyPolicy.find(policy => policy.module === props?.props?.module);
+      return filteredPrivacyPolicy;
+    },
+  });
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
   };
@@ -45,7 +41,7 @@ const PrivacyComponent = ({ onSelect, formData, control, formState, ...props }) 
           size={"medium"}
           onClick={onButtonClick}
           // isSuffix={true}
-          style={{ marginBottom: "1.18rem", paddingLeft: "unset" }}
+          style={{ marginBottom: "1.18rem", paddingLeft: "0.2rem" }}
         ></Button>
       </div>
       {showPopUp && (
@@ -86,41 +82,68 @@ const PrivacyComponent = ({ onSelect, formData, control, formState, ...props }) 
           <div>
             <div className="privacy-table">{t("DIGIT_TABLE_OF_CONTENTS")}</div>
             <ul>
-              {privacy?.descriptions
-                .filter((desc) => desc?.isHeader)
-                .map((desc, index) => (
-                  <li key={index}>
-                    <Button
-                      label={t(desc.text)}
-                      variation={"link"}
-                      size={"medium"}
-                      onClick={(e) => {
-                        e.preventDefault();
-                        handleScrollToElement(desc.text);
-                      }}
-                    ></Button>
-                  </li>
-                ))}
+              {privacy?.contents.map((content, index) => (
+                <li key={index} style={{ display: "flex", alignItems: "center" }}>
+                  <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
+                  <Button
+                    label={t(content.header)}
+                    variation={"link"}
+                    size={"medium"}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleScrollToElement(content?.header);
+                    }}
+                  ></Button>
+                </li>
+              ))}
             </ul>
           </div>
-          {privacy?.descriptions.map((desc, index) => (
-            <div
-              key={index}
-              id={desc.isHeader ? desc.text : undefined}
-              style={{
-                fontWeight: desc.isBold ? 700 : 400,
-                paddingLeft: desc.isSpaceRequired ? "1rem" : "0",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              {desc.isDotRequired && (
-                <span className="desc-dot"
-                ></span>
+          {privacy?.contents.map((content, index) => (
+        <div key={index} id={content?.header}>
+          <div
+            style={{
+              fontWeight: 'bold',
+              paddingLeft: content?.isSpaceRequired ? "1rem" : "0",
+            }}
+          >
+            {t(content.header)}
+          </div>
+          {content.descriptions.map((description, subIndex) => (
+            <div key={subIndex} style={{ paddingLeft: description.isSpaceRequired ? "1rem" : "0"  , marginBottom: '0.5rem'}}>
+              <div
+                style={{
+                  fontWeight: description?.isBold ? 700 : 400,
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                {description.type === 'points' && (
+                  <span style={{ marginRight: '0.5rem', listStyleType: 'disc' }}>&#8226;</span>
+                )}
+                {description.type === 'step' && (
+                  <span style={{ marginRight: '0.5rem', listStyleType: 'decimal' }}>{subIndex + 1}. </span>
+                )}
+                {t(description.text)}
+              </div>
+              {description?.subDescriptions && description?.subDescriptions.length > 0 && (
+                <div className="policy-subdescription">
+                  {description.subDescriptions.map((subDesc, subSubIndex) => (
+                    <div key={subSubIndex} className="policy-subdescription-points">
+                      {subDesc.type === 'points' && (
+                        <span style={{ marginRight: '0.5rem', listStyleType: 'disc' }}>&#8226;</span>
+                      )}
+                      {subDesc.type === 'step' && (
+                        <span style={{ marginRight: '0.5rem', listStyleType: 'decimal' }}>{subSubIndex + 1}. </span>
+                      )}
+                      {t(subDesc.text)}
+                    </div>
+                  ))}
+                </div>
               )}
-              {t(desc.text)}
             </div>
           ))}
+        </div>
+      ))}
         </PopUp>
       )}
     </React.Fragment>
