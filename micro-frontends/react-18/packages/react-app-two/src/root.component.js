89c6163b5d81088b647e5143c310e7bff4e609eb
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-08-02 16:13:29 +0530

    fixed the component import issues and css required

--- micro-frontends/react-18/packages/react-app-two/src/root.component.js
+++ micro-frontends/react-18/packages/react-app-two/src/root.component.js
@@ -1,7 +1,23 @@
 import {TestComponent} from "@egovernments/digit-ui-components";
+import {
+  Tag,
+  SelectionCard,
+  ErrorMessage,
+} from "@egovernments/digit-ui-components";
+
 export default function Root(props) {
   return<div>
     <h1>hi</h1>
+    <Tag />
+      <SelectionCard
+        options={[
+          {
+            code: "sasa",
+            name: "name",
+          },
+        ]}
+      />
+      <ErrorMessage message="Error" />
     <TestComponent></TestComponent>
     <section>{props.name} hey react-app-2</section></div> ;
 }
