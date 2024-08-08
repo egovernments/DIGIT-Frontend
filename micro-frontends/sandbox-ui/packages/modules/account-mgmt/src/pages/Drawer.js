7aefe1ab6ad07c76b36396f2b918d80f2069fea4
mukund-egov <mukund.tripathi@egovernments.org>
2024-08-08 10:40:00 +0530

    SN-54 :: Added Drawer Component

---
+++ micro-frontends/sandbox-ui/packages/modules/account-mgmt/src/pages/Drawer.js
@@ -0,0 +1,87 @@
+import React, { Fragment, useState } from "react";
+
+import { DigitUIComponents } from "components";
+import "./Drawer.css";
+
+const { Card, Button, TextBlock } = DigitUIComponents;
+
+function Drawer({ drawerDirection = "right", mdmsData = [] }) {
+  // Drawer is being show on the screen or not
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    console.log("ACTIVE", isOpen);
+    setIsOpen(!isOpen);
+  };
+
+  // Secondary Card
+  //Secondary Button will be used
+
+  console.log(drawerDirection);
+
+  return (
+    <Card className="container">
+      <Card className={`drawer-container ${drawerDirection}`}>
+        <Card className={`drawer ${isOpen ? "open" : "closed"}`}>
+          <TextBlock header="SetUp your SandBox!" headerClasName="header" />
+          {mdmsData.map((item) => (
+            <Card key={item.actionId} className="slider-card">
+              <Card className="icon">{item.iconName}</Card>
+              <span className="card-divider"></span>
+              <Card className="content-button">
+                <TextBlock
+                  caption="Explore your Applications"
+                  captionClassName="caption"
+                  body="Explore default DIGIT applications, envision use cases and make your applications live after configuration."
+                />
+
+                <Button variation="secondary">{item.buttonName}</Button>
+              </Card>
+            </Card>
+          ))}
+        </Card>
+
+        <button
+          onClick={toggleDrawer}
+          className={`toggle-button ${isOpen ? "open" : "closed"}`}
+        >
+          {!isOpen ? (
+            <>
+              <svg
+                width="12"
+                height="20"
+                viewBox="0 0 12 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.67 18.1284L9.9 19.8984L0 9.99844L9.9 0.0984391L11.67 1.86844L3.54 9.99844L11.67 18.1284Z"
+                  fill="#0B0C0C"
+                />
+              </svg>
+              <span className="vertical-bar"></span>
+            </>
+          ) : (
+            <>
+              <span className="vertical-bar"></span>
+              <svg
+                width="12"
+                height="20"
+                viewBox="0 0 12 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.33 18.1284L2.1 19.8984L12 9.99844L2.1 0.0984391L0.329998 1.86844L8.46 9.99844L0.33 18.1284Z"
+                  fill="#0B0C0C"
+                />
+              </svg>
+            </>
+          )}
+        </button>
+      </Card>
+    </Card>
+  );
+}
+
+export default Drawer;
--- micro-frontends/sandbox-ui/packages/modules/account-mgmt/src/pages/index.js
+++ micro-frontends/sandbox-ui/packages/modules/account-mgmt/src/pages/index.js
@@ -8,12 +8,12 @@ import { DigitRouter } from "components"; // Import the DigitRouter component fo
 const routes = [
   {
     url: "page-two",
-    withAuth: true,  // Indicates that authentication is required for this route
+    withAuth: true, // Indicates that authentication is required for this route
     component: lazy(() => import("./PageTwo")), // Lazy-load the PageTwo component
   },
   {
     url: "page-one",
-    withAuth: true,  // Indicates that authentication is required for this route
+    withAuth: true, // Indicates that authentication is required for this route
     component: lazy(() => import("./PageOne")), // Lazy-load the PageOne component
   },
   {
@@ -21,21 +21,24 @@ const routes = [
     withAuth: true, // Indicates that no authentication is required for this route
     component: lazy(() => import("./PageThree")), // Lazy-load the PageThree component
   },
+  {
+    url: "test-drawer",
+    withAuth: true, // Indicates that no authentication is required for this route
+    component: lazy(() => import("./Drawer")), // Lazy-load the PageThree component
+  },
 ];
 
 /**
  * AccountMgmtApp Component
  * This component initializes the DigitRouter with the specified routes and baseAppURL.
- * 
+ *
  * @param {Object} props - The properties passed to the component.
  * @param {string} props.rootTenant - The root tenant for routing (if needed).
  * @param {string} props.baseAppURL - The base URL for the application.
  * @returns {JSX.Element} The rendered DigitRouter component with the routes.
  */
 const AccountMgmtApp = ({ rootTenant, baseAppURL }) => {
-  return (
-    <DigitRouter baseAppURL={baseAppURL} routes={routes} />
-  );
+  return <DigitRouter baseAppURL={baseAppURL} routes={routes} />;
 };
 
 export default AccountMgmtApp;
