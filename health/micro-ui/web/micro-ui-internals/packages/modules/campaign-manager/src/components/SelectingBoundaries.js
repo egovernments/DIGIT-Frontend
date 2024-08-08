e6a0f1a1a6a1ca49634881001bbbb28ff1a3269a
nabeelmd-eGov <nabeel.md@egovernments.com>
2024-08-08 14:54:30 +0530

    Campaign V0.2 merge to master

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/SelectingBoundaries.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/SelectingBoundaries.js
@@ -43,7 +43,10 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
   const { isLoading, data: hierarchyConfig } = Digit.Hooks.useCustomMDMS(tenantId, "HCM-ADMIN-CONSOLE", [{ name: "hierarchyConfig" }]);
 
   // const lowestHierarchy = hierarchyConfig?.["HCM-ADMIN-CONSOLE"]?.hierarchyConfig?.[0]?.lowestHierarchy;
-  const lowestHierarchy = useMemo(() => hierarchyConfig?.["HCM-ADMIN-CONSOLE"]?.hierarchyConfig?.[0]?.lowestHierarchy, [hierarchyConfig]);
+  // const lowestHierarchy = useMemo(() => hierarchyConfig?.["HCM-ADMIN-CONSOLE"]?.hierarchyConfig?.[0]?.lowestHierarchy, [hierarchyConfig]);
+  const lowestHierarchy = useMemo(() => {
+    return hierarchyConfig?.["HCM-ADMIN-CONSOLE"]?.hierarchyConfig?.find(item => item.isActive)?.lowestHierarchy;
+  }, [hierarchyConfig]);
   const lowestChild = hierarchyTypeDataresult?.boundaryHierarchy.filter((item) => item.parentBoundaryType === lowestHierarchy)?.[0]?.boundaryType;
   const searchParams = new URLSearchParams(location.search);
   const isDraft = searchParams.get("draft");
@@ -56,6 +59,7 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
     });
     window.history.replaceState({}, "", url);
   }
+
 
   const fetchOptions = async ()=>{
     setLoaderEnabled(true);
@@ -366,15 +370,6 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
       data?.map((ob) => {
         res.push(ob?.[1]);
       });
-
-    // const transformedRes = res?.map((item) => ({
-    //   code: item.code,
-    //   type: item.type || item.boundaryType,
-    //   isRoot: item.boundaryType === parentBoundaryTypeRoot,
-    //   includeAllChildren:  item.type === lowestHierarchy || item.boundaryType === lowestHierarchy,
-    //   parent: item?.parent,
-    // }));
-
     let transformedRes = [];
     if (!isDraft) {
       transformedRes = res?.map((item) => ({
@@ -399,6 +394,7 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
       } else {
         transformedRes = filteredData;
       }
+
     }
 
     const newBoundaryType = transformedRes?.[0]?.type;
@@ -468,7 +464,7 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
             })
             .map((boundary, index) =>
               boundary?.parentBoundaryType == null ? (
-                <LabelFieldPair key={index}>
+                <LabelFieldPair key={index} style={{ alignItems: 'flex-start' }}>
                   <CardLabel>
                     {/* {t(`${hierarchy}_${boundary?.boundaryType}`?.toUpperCase())} */}
                     {t((hierarchy + "_" + boundary?.boundaryType).toUpperCase())}
@@ -488,11 +484,14 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
                       onSelect={(value) => {
                         handleBoundaryChange(value, boundary);
                       }}
+                      config={{
+                        isDropdownWithChip: true, 
+                      }}
                     />
                   </div>
                 </LabelFieldPair>
               ) : (
-                <LabelFieldPair key={index}>
+                <LabelFieldPair key={index} style={{ alignItems: 'flex-start' }}>
                   <CardLabel>
                     {t((hierarchy + "_" + boundary?.boundaryType).toUpperCase())}
                     <span className="mandatory-span">*</span>
@@ -516,6 +515,9 @@ function SelectingBoundaries({ onSelect, formData, ...props }) {
                       optionsKey={"code"}
                       onSelect={(value) => {
                         handleBoundaryChange(value, boundary);
+                      }}
+                      config={{
+                        isDropdownWithChip: true, 
                       }}
                       selected={selectedData?.filter((item) => item?.type === boundary?.boundaryType) || []}
                       addCategorySelectAllCheck={true}
