16f8c287d8231cf8af07d34af5ad675ba94afa51
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-08-12 16:33:24 +0530

    added custom icons

--- micro-frontends/sandbox-ui/packages/components/src/components/homecomponents/CustomCard.js
+++ micro-frontends/sandbox-ui/packages/components/src/components/homecomponents/CustomCard.js
@@ -1,26 +1,45 @@
 import React from "react";
 import * as DigitUIComponents from '@egovernments/digit-ui-components';
-
 const { InfoCard, Stepper, Button, Timeline, InfoButton, Card ,DigitIcons} = DigitUIComponents
 import './HomeScreen.css';
+import { useTranslation } from "react-i18next";
+
+import {
+  TenantManagementIcon,
+  AccountManagementIcon,
+  DataConfigurationsIcon,
+  // Add other icons
+} from './CustomIcons';
+
+const iconMapping = {
+  "Tenant Management": <TenantManagementIcon />,
+  "Account Management": <AccountManagementIcon />,
+  "Data and Configurations": <DataConfigurationsIcon />,
+  // Map other icons
+};
 
 const CustomCard = ({ card }) => {
+  const { t } = useTranslation();
+  const IconComponent = iconMapping[card.name] || null;
 
   return (
     <Card className="custom-card">
-      <p className="custom-card-description">{card.description}</p>
+      <div className="custom-card-header">
+        {IconComponent && <div className="custom-card-icon">{IconComponent}</div>}
+        <h3 className="custom-card-name">{t(card.name)}</h3>
+      </div>
+      <p className="custom-card-description">{t(card.description)}</p>
       <button
         className="custom-card-button"
         onClick={() => window.location.href = card.navigationURL}
       >
-        {card.buttonName}
+        {t(card.buttonName)}
       </button>
     </Card>
   );
 };
 
 export default CustomCard;
-
 
 
 
---
+++ micro-frontends/sandbox-ui/packages/components/src/components/homecomponents/CustomIcons.js
@@ -0,0 +1,29 @@
+import React from "react";
+export const TenantManagementIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3 12h18v9H3v-9zm18-2V7a1 1 0 00-1-1h-4V3H8v3H4a1 1 0 00-1 1v3h18zm-2 1H5v7h14v-7z"
+      fill="#C84C0E"
+    />
+  </svg>
+);
+
+export const AccountManagementIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"
+      fill="#C84C0E"
+    />
+  </svg>
+);
+
+export const DataConfigurationsIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M19.14 12.94l.69-1.73-1.45-1.45 1.73-.69a6.99 6.99 0 000-3.86l-1.73-.69 1.45-1.45-.69-1.73-1.45 1.45-.69-1.73H8.86l-.69 1.73L6.72 3.44 5.27 4.88l.69 1.73-1.45 1.45-1.73-.69a6.99 6.99 0 000 3.86l1.73.69-1.45 1.45.69 1.73 1.45-1.45.69 1.73h6.27l.69-1.73 1.45 1.45.69-1.73-1.45-1.45zm-7.14 4.06c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"
+      fill="#C84C0E"
+    />
+  </svg>
+);
+
+// Add more icons as needed
