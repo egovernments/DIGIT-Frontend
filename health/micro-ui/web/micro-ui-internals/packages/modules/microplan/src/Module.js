ac6829ebc0e171c395aeeaae78fd8f77ba04ccde
Nipun Arora <aroranipun1@gmail.com>
2024-08-14 11:47:13 +0530

    microplan new module (#1283)

---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/Module.js
@@ -0,0 +1,41 @@
+import { Loader } from "@egovernments/digit-ui-react-components";
+import React from "react";
+import { useRouteMatch } from "react-router-dom";
+import { default as EmployeeApp } from "./pages/employee";
+import MicroplanCard from "./components/MicroplanCard";
+import { overrideHooks, updateCustomConfigs } from "./utils";
+
+export const MicroplanModule = ({ stateCode, userType, tenants }) => {
+  const { path, url } = useRouteMatch();
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+  const { data: BOUNDARY_HIERARCHY_TYPE } = Digit.Hooks.useCustomMDMS(tenantId, "HCM-ADMIN-CONSOLE", [{ name: "hierarchyConfig" }], {
+    select: (data) => {
+      return data?.["HCM-ADMIN-CONSOLE"]?.hierarchyConfig?.[0]?.hierarchy;
+    },
+  });
+  const moduleCode = ["Microplanning", `boundary-${BOUNDARY_HIERARCHY_TYPE}`, "hcm-admin-schemas"];
+  const language = Digit.StoreData.getCurrentLanguage();
+  const { isLoading, data: store } = Digit.Services.useStore({
+    stateCode,
+    moduleCode,
+    language,
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+  return <EmployeeApp path={path} stateCode={stateCode} userType={userType} tenants={tenants} />;
+};
+
+const componentsToRegister = {
+  MicroplanModule,
+  MicroplanCard,
+};
+
+export const initMicroplanComponents = () => {
+  overrideHooks();
+  updateCustomConfigs();
+  Object.entries(componentsToRegister).forEach(([key, value]) => {
+    Digit.ComponentRegistryService.setComponent(key, value);
+  });
+};
