31a8307988685b80d2e26807f8d365c8ddb749f8
aaradhya-egov <137176709+aaradhya-egov@users.noreply.github.com>
2024-08-21 14:15:41 +0530

    otp custom component and static sign up page (#1277)
    * build fixes
    
    * otp custom component and static sign up page
    
    * view url
    
    * otp component
    
    * otp component
    
    * fix
    
    ---------
    
    Co-authored-by: NabeelAyubee <nayubi7@gmail.com>

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/index.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/index.js
@@ -8,6 +8,9 @@ import ChangePassword from "./ChangePassword";
 import ForgotPassword from "./ForgotPassword";
 import LanguageSelection from "./LanguageSelection";
 import EmployeeLogin from "./Login";
+import SignUp from "./SignUp";
+import Otp from "./Otp";
+import ViewUrl from "./ViewUrl";
 import UserProfile from "../citizen/Home/UserProfile";
 import ErrorComponent from "../../components/ErrorComponent";
 import { PrivateRoute } from "@egovernments/digit-ui-components";
@@ -71,6 +74,15 @@ const EmployeeApp = ({
             <Switch>
               <Route path={`${path}/user/login`}>
                 <EmployeeLogin stateCode={stateCode} />
+              </Route>
+              <Route path={`${path}/user/sign-up`}>
+                <SignUp stateCode={stateCode} />
+              </Route>
+              <Route path={`${path}/user/otp`}>
+                <Otp  />
+              </Route>
+              <Route path={`${path}/user/url`}>
+                <ViewUrl  />
               </Route>
               <Route path={`${path}/user/forgot-password`}>
                 <ForgotPassword />
