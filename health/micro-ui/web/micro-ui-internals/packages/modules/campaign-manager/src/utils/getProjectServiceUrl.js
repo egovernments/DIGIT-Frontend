a9546b5242b2e272a2b2b5238827b5e01380c54f
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-08-23 11:05:21 +0530

    Fixed the project service dynamic path issue  (#1321)
    * url fetching corrected
    
    * url fetching corrected1
    
    * url fetching corrected2
    
    * fixed url issue
    
    ---------
    
    Co-authored-by: suryansh-egov <suryansh.singh.egovernments.org>

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/getProjectServiceUrl.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/getProjectServiceUrl.js
@@ -1,10 +1,8 @@
 const getProjectServiceUrl = () => {
   // Access the globalConfigs object and retrieve the PROJECT_SEERVICE_PATH configuration.
   // If not defined, fallback to default URL `/health-project`.
-  const url = window.globalConfigs?.getConfig("PROJECT_SEERVICE_PATH") || `/health-project`;
-  
-  // Return the constructed URL.
-  return url;
+  let url = window.globalConfigs?.getConfig("PROJECT_SERVICE_PATH") || `health-project`;
+  return `/${url}`;
 };
 
 // Export the function to be used in other parts of the application.
