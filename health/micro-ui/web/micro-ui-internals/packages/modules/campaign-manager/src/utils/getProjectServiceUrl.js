509f00491b59f5fe56502aa55c6226ca46a3cd45
suryansh-egov <suryansh.singh.egovernments.org>
2024-08-23 10:33:12 +0530

    url fetching corrected1

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/getProjectServiceUrl.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/getProjectServiceUrl.js
@@ -2,12 +2,9 @@ const getProjectServiceUrl = () => {
   // Access the globalConfigs object and retrieve the PROJECT_SEERVICE_PATH configuration.
   // If not defined, fallback to default URL `/health-project`.
   let url = window.globalConfigs?.getConfig("PROJECT_SERVICE_PATH") || `/health-project`;
-  console.log("url is: ", url);
   if (!url.startsWith('/')) {
     url = `/${url}`;
-  }
-  console.log("new url is:", url);
-  // Return the constructed URL.
+  }  // Return the constructed URL.
   return url;
 };
 
