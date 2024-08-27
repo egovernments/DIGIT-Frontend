65f88834794c1ed4914a286b04ed5549bce57889
ashish-egov <137176738+ashish-egov@users.noreply.github.com>
2024-08-27 11:30:02 +0530

    Update constants.ts

--- utilities/project-factory/src/server/config/constants.ts
+++ utilities/project-factory/src/server/config/constants.ts
@@ -36,6 +36,7 @@ export const CONSTANTS: any = {
             CAMPAIGN_NAME_ERROR: "Campaign name already exists",
             CAMPAIGN_ALREADY_MAPPED: "Campaign is already mapped",
             PARENT_CAMPAIGN_ERROR: "Parent campaign not found",
+            INVALID_RESOURCE_DISTRIBUTION_STRATEGY: "Invalid resource distribution strategy",
         },
         BOUNDARY: {
             BOUNDARY_DATA_NOT_FOUND: "No boundary data found in the system.",
@@ -145,6 +146,10 @@ export const processTrackStatuses = {
     toBeCompleted: "toBeCompleted",
     failed: "failed",
 }
+
+export const resourceDistributionStrategyTypes = [
+    "FIXED", "MIXED", "HOUSE_TO_HOUSE"
+]
 
 
 // Retrieves the error object containing the error code, message, and notFound flag.
