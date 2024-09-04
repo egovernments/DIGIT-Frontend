e7b78b9b4ff2bb88ba356b394f473f5d5d2d7497
Nipun Arora <aroranipun1@gmail.com>
2024-09-04 10:37:51 +0530

    added hooks and service

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/hooks/index.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/hooks/index.js
@@ -1,23 +1,37 @@
 import utils from "../utils";
+import useCreatePlanConfig from "./useCreatePlanConfig";
+import useSearchPlanConfig from "./useSearchPlanConfig";
+import useUpdatePlanConfig from "./useUpdatePlanConfig";
+import useSavedMicroplans from "./useSavedMicroplans";
+import useSearchCampaign from "./useSearchCampaign";
+import { useGenerateIdCampaign } from "./useGenerateIdCampaign";
+import useCreateCampaign from "./useCreateCampaign";
+import useUpdateCampaign from "./useUpdateCampaign";
+
 const microplanv1 = {
-  hook:() => {
-    
-  }
+  useCreatePlanConfig,
+  useSearchPlanConfig,
+  useUpdatePlanConfig,
+  useSavedMicroplans,
+  useSearchCampaign,
+  useGenerateIdCampaign,
+  useCreateCampaign,
+  useUpdateCampaign
 };
 
 const Hooks = {
-  microplanv1
+  microplanv1,
 };
 
 const Utils = {
   browser: {
-    microplan: () => { },
+    sample: () => {},
   },
   microplanv1: {
-    ...utils,
-    he:()=>{
-      console.log("he");
-    }
+    sampleUtil:()=>{
+
+    },
+    ...utils
   },
 };
 
