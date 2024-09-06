379b68f86ab677fe30b5c6b1b31fee59ec8c7a36
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-09-06 18:27:54 +0530

    hrms changes

--- micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/createEmployee.js
+++ micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/createEmployee.js
@@ -137,7 +137,6 @@ const CreateEmployee = () => {
 
   
 
-
   const onSubmit = (data) => {
     if (data.Jurisdictions.filter(juris => juris.tenantId == tenantId).length == 0) {
       setShowToast({ key: true, label: "ERR_BASE_TENANT_MANDATORY" });
