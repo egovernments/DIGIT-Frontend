e6f17451ec7c6234ea6baee4fcbac2b6c5e2ad03
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-09-17 11:28:33 +0530

    state removed

--- micro-ui/web/micro-ui-internals/packages/modules/sandbox/src/pages/employee/tenantMgmt/TenantInfoCard.js
+++ micro-ui/web/micro-ui-internals/packages/modules/sandbox/src/pages/employee/tenantMgmt/TenantInfoCard.js
@@ -14,58 +14,55 @@ const TenantInfocard = () => {
   const searchParams = new URLSearchParams(location.search);
   const module = searchParams.get("module");
   const config = setupMasterConfig?.SetupMaster?.filter((item) => item.module === module)?.[0];
-  const [showMaster, setShowMaster] = useState(false);
 
   return (
     <Card className={"sandboxSetupMasterInfo"}>
-      {!showMaster && (
-        <>
-          <Header className="headerFlex" styles={{ fontSize: "32px" }}>
-            <SVG.Announcement height={40} width={40} />
-            {t(config?.header || "N/A")}
-          </Header>
-          <CardText>{t(config?.description)}</CardText>
-          {config?.add_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_ADD")}</CardHeader>}
-          {config?.add_organization?.length > 0 &&
-            config?.add_organization?.map((item, index) => (
-              <li key={index} style={{ display: "flex", alignItems: "center" }}>
-                <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
-                <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
-              </li>
-            ))}
+      <>
+        <Header className="headerFlex" styles={{ fontSize: "32px" }}>
+          <SVG.Announcement height={40} width={40} />
+          {t(config?.header || "N/A")}
+        </Header>
+        <CardText>{t(config?.description)}</CardText>
+        {config?.add_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_ADD")}</CardHeader>}
+        {config?.add_organization?.length > 0 &&
+          config?.add_organization?.map((item, index) => (
+            <li key={index} style={{ display: "flex", alignItems: "center" }}>
+              <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
+              <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
+            </li>
+          ))}
 
-          {config?.view_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_VIEW")}</CardHeader>}
-          {config?.view_organization?.length > 0 &&
-            config?.view_organization?.map((item, index) => (
-              <li key={index} style={{ display: "flex", alignItems: "center" }}>
-                <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
-                <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
-              </li>
-            ))}
+        {config?.view_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_VIEW")}</CardHeader>}
+        {config?.view_organization?.length > 0 &&
+          config?.view_organization?.map((item, index) => (
+            <li key={index} style={{ display: "flex", alignItems: "center" }}>
+              <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
+              <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
+            </li>
+          ))}
 
-          {config?.edit_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_EDIT")}</CardHeader>}
-          {config?.edit_organization?.length > 0 &&
-            config?.edit_organization?.map((item, index) => (
-              <li key={index} style={{ display: "flex", alignItems: "center" }}>
-                <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
-                <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
-              </li>
-            ))}
-          <div className="setupMasterSetupActionBar">
-            <Button
-              className="actionButton"
-              label={t(config.actionText)}
-              variation={"secondary"}
-              icon="ArrowForward"
-              isSuffix={true}
-              onClick={(e) => {
-                e.preventDefault();
-                history.push(`/${window?.contextPath}/employee/sandbox/tenant-management/create`);
-              }}
-            ></Button>
-          </div>
-        </>
-      )}
+        {config?.edit_organization?.length > 0 && <CardHeader className="subHeader"> {t("SUB_ORGANIZATION_EDIT")}</CardHeader>}
+        {config?.edit_organization?.length > 0 &&
+          config?.edit_organization?.map((item, index) => (
+            <li key={index} style={{ display: "flex", alignItems: "center" }}>
+              <span style={{ marginRight: "0.5rem" }}>{index + 1}. </span>
+              <span style={{ marginRight: "0.5rem" }}>{t(item.name)}</span>
+            </li>
+          ))}
+        <div className="setupMasterSetupActionBar">
+          <Button
+            className="actionButton"
+            label={t(config.actionText)}
+            variation={"secondary"}
+            icon="ArrowForward"
+            isSuffix={true}
+            onClick={(e) => {
+              e.preventDefault();
+              history.push(`/${window?.contextPath}/employee/sandbox/tenant-management/create`);
+            }}
+          ></Button>
+        </div>
+      </>
     </Card>
   );
 };
