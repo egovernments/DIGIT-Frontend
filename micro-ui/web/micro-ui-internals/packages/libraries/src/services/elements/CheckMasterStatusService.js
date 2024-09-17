67999e89da17cd9ab1011c0c66d1af9c6dc3c27e
nabeelmd-eGov <nabeel.md@egovernments.org>
2024-09-17 11:16:57 +0530

    Update CheckMasterStatusService.js

--- micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/CheckMasterStatusService.js
+++ micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/CheckMasterStatusService.js
@@ -1,26 +1,21 @@
 import { MdmsService } from "./MDMS";
-// import { CustomService } from "./CustomService";
 
 const CheckMasterStatusService = {
+  //function to check each master has atleast one data inside 
   checkDataCompleteness: function (data1, data2) {
-    // Convert data1 to a set of codes for quick lookup
     const requiredCodes = new Set(data1.map((item) => item.code));
 
-    // Iterate over each required code
     for (const item of data1) {
       const [prefix, suffix] = item.code.split(".");
 
-      // Check if the prefix exists in data2
       if (!data2[prefix]) {
         return false;
       }
 
-      // Check if the suffix exists in data2 under the prefix
       if (!data2[prefix][suffix]) {
         return false;
       }
 
-      // Check if there's at least one active object in the list
       const hasActiveObject = data2[prefix][suffix].some((obj) => obj.active);
       if (!hasActiveObject) {
         return false;
