c4b7328fc253f534d5f9bee36b9ff94b94e2b2c9
mithunhegde-egov <152362871+mithunhegde-egov@users.noreply.github.com>
2024-09-20 10:52:17 +0530

    Sandbox landing page (#1406)
    * landing page
    
    * removed console

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/index.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/index.js
@@ -8,6 +8,7 @@ import ChangePassword from "./ChangePassword";
 import ForgotPassword from "./ForgotPassword";
 import LanguageSelection from "./LanguageSelection";
 import EmployeeLogin from "./Login";
+import Landing from "./Landing";
 import SignUp from "./SignUp";
 import Otp from "./Otp";
 import ViewUrl from "./ViewUrl";
@@ -15,7 +16,7 @@ import UserProfile from "../citizen/Home/UserProfile";
 import ErrorComponent from "../../components/ErrorComponent";
 import { PrivateRoute } from "@egovernments/digit-ui-components";
 
-const userScreensExempted = ["user/profile", "user/error"];
+const userScreensExempted = ["user/landing", "user/profile", "user/error"];
 
 const EmployeeApp = ({
   stateInfo,
@@ -86,6 +87,9 @@ const EmployeeApp = ({
               </Route>
               <PrivateRoute path={`${path}/user/profile`}>
                 <UserProfile stateCode={stateCode} userType={"employee"} cityDetails={cityDetails} />
+              </PrivateRoute>
+              <PrivateRoute path={`${path}/user/landing`}>
+                <Landing />
               </PrivateRoute>
               <Route path={`${path}/user/error`}>
                 <ErrorComponent
