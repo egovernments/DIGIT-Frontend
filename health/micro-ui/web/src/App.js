d19d85d2532c1c6200b49943eaa5686d1776e050
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-09-24 15:27:11 +0530

    Update App.js

--- health/micro-ui/web/src/App.js
+++ health/micro-ui/web/src/App.js
@@ -58,10 +58,13 @@ initLibraries().then(() => {
 });
 
 function App() {
+
   window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
   const stateCode =
     window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
     process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
+  console.log(process.env,"process.env.");
+  
   if (!stateCode) {
     return <h1>stateCode is not defined</h1>;
   }
