c948b76e67ce3bc52d0cbb7ab06b5de2cea7b56c
Nipun Arora <aroranipun1@gmail.com>
2024-09-28 15:21:48 +0530

    Microplan Create/Update, Boundaries Update, Freezing Data  (#1440)
    * initialized generic mutation hook and validations logic
    
    * updates to generic hook, autogeneration of name
    
    * updated code for boundary update, freezing campaign and microplan details
    
    * Updated CSS package
    
    * Update index.html

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/configs/SetupMicroplanConfig.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/configs/SetupMicroplanConfig.js
@@ -2,6 +2,7 @@ export const MicroplanConfig = (totalFormData, dataParams, isSubmitting, summary
   return [
     {
       form:[
+        // TODO: Do no update this object key:1 
         {
           stepCount: "1",
           key: "1",
@@ -16,6 +17,10 @@ export const MicroplanConfig = (totalFormData, dataParams, isSubmitting, summary
               component: "CampaignDetails",
               withoutLabel: true,
               disable: false,
+              showPopupOnSubmission:{
+                alertMessage:"ALERT_MESSAGE_CAMPAIGN",
+                alertHeader:"ALERT_HEADER_CAMPAIGN"
+              },
               customProps: {
                 module: "HCM",
                 sessionData: totalFormData,
@@ -27,6 +32,7 @@ export const MicroplanConfig = (totalFormData, dataParams, isSubmitting, summary
             },
           ],
         },
+        // TODO: Do no update this object key:2
         {
           stepCount: "2",
           key: "2",
@@ -42,17 +48,22 @@ export const MicroplanConfig = (totalFormData, dataParams, isSubmitting, summary
               withoutLabelFieldPair: true,
               withoutLabel: true,
               disable: false,
+              showPopupOnSubmission:{
+                alertMessage:"ALERT_MESSAGE_MICROPLAN",
+                alertHeader:"ALERT_HEADER_MICROPLAN"
+              },
               customProps: {
                 module: "HCM",
                 sessionData: totalFormData,
                 isSubmitting: isSubmitting,
               },
               populators: {
-                name: "projectType",
+                name: "microplanDetails",
               },
             },
           ],
         },
+        // TODO: Do no update this object key:3
         {
           stepCount: "3",
           key: "3",
