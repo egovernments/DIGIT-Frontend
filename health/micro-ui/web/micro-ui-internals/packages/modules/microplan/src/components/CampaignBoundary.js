d536811dd29cf85917639a15a49915774665fd49
Abishek <abishek.t@egovernments.org>
2024-10-03 12:55:50 +0530

    comment changes

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/CampaignBoundary.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/CampaignBoundary.js
@@ -26,7 +26,225 @@ const CampaignBoundary = ({customProps}) => {
         // Set showAllItems to true to show all items
     };
 
-    const selectedData = customProps?.sessionData?.BOUNDARY?.boundarySelection?.selectedData;
+    const selectedData1 = customProps?.sessionData?.BOUNDARY?.boundarySelection?.selectedData;
+
+    const selectedData=[
+        {
+            "code": "MICROPLAN_MO",
+            "name": "MICROPLAN_MO",
+            "type": "Country",
+            "isRoot": true,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_SINOE",
+            "name": "MICROPLAN_MO_05_SINOE",
+            "type": "Province",
+            "parent": "MICROPLAN_MO",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_JEDEPO",
+            "name": "MICROPLAN_MO_05_07_JEDEPO",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "name": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "type": "Locality",
+            "parent": "MICROPLAN_MO_05_07_JEDEPO",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_15_POKPAKEN_TOWN_K1889",
+            "name": "MICROPLAN_MO_05_07_03_15_POKPAKEN_TOWN_K1889",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_14_POKPAKEN_TOWN_P1888",
+            "name": "MICROPLAN_MO_05_07_03_14_POKPAKEN_TOWN_P1888",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_13_BEATUOKEN_DWEHN1887",
+            "name": "MICROPLAN_MO_05_07_03_13_BEATUOKEN_DWEHN1887",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_12_BEATUOKEN_CHATT1886",
+            "name": "MICROPLAN_MO_05_07_03_12_BEATUOKEN_CHATT1886",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_11_BEATUOKEN_BEATU1885",
+            "name": "MICROPLAN_MO_05_07_03_11_BEATUOKEN_BEATU1885",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_10_BEATUOKEN",
+            "name": "MICROPLAN_MO_05_07_03_10_BEATUOKEN",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_09_POKPAKEN_TOWN_K1883",
+            "name": "MICROPLAN_MO_05_07_03_09_POKPAKEN_TOWN_K1883",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_08_POKPAKEN_TOWN_P1882",
+            "name": "MICROPLAN_MO_05_07_03_08_POKPAKEN_TOWN_P1882",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_07_POKPAKEN_TOWN_S1881",
+            "name": "MICROPLAN_MO_05_07_03_07_POKPAKEN_TOWN_S1881",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_06_POKPAKEN_TOWN",
+            "name": "MICROPLAN_MO_05_07_03_06_POKPAKEN_TOWN",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_05_JOKOKEN_ROBERT_1879",
+            "name": "MICROPLAN_MO_05_07_03_05_JOKOKEN_ROBERT_1879",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_04_JOKOKEN_SLAH_VI1878",
+            "name": "MICROPLAN_MO_05_07_03_04_JOKOKEN_SLAH_VI1878",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_03_JOKOKEN_JOKOKEN",
+            "name": "MICROPLAN_MO_05_07_03_03_JOKOKEN_JOKOKEN",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_02_JOKOKEN",
+            "name": "MICROPLAN_MO_05_07_03_02_JOKOKEN",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_03_01__124",
+            "name": "MICROPLAN_MO_05_07_03_01__124",
+            "type": "Village",
+            "parent": "MICROPLAN_MO_05_07_03_JOKOKEN_CLINIC",
+            "isRoot": false,
+            "includeAllChildren": true
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_02_DUCORFREE_CLINIC",
+            "name": "MICROPLAN_MO_05_07_02_DUCORFREE_CLINIC",
+            "type": "Locality",
+            "parent": "MICROPLAN_MO_05_07_JEDEPO",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_07_01_DOODWICKEN_CLINIC",
+            "name": "MICROPLAN_MO_05_07_01_DOODWICKEN_CLINIC",
+            "type": "Locality",
+            "parent": "MICROPLAN_MO_05_07_JEDEPO",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_06_JEADE",
+            "name": "MICROPLAN_MO_05_06_JEADE",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_05_GREENVILLE",
+            "name": "MICROPLAN_MO_05_05_GREENVILLE",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_04_GBLONEE",
+            "name": "MICROPLAN_MO_05_04_GBLONEE",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_03_DUGBE_RIVER",
+            "name": "MICROPLAN_MO_05_03_DUGBE_RIVER",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_02_BUTAW",
+            "name": "MICROPLAN_MO_05_02_BUTAW",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        },
+        {
+            "code": "MICROPLAN_MO_05_01__101",
+            "name": "MICROPLAN_MO_05_01__101",
+            "type": "District",
+            "parent": "MICROPLAN_MO_05_SINOE",
+            "isRoot": false,
+            "includeAllChildren": false
+        }
+    ] 
     
     // Memoizing parents and parent_group to only compute once when selectedData changes
     
