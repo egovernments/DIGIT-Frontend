a8e9694a5e596d590369dc16ee6dcd59bac6d347
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-10-10 15:15:54 +0530

    removed redundant home in sidebar

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/config/sidebar-menu.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/config/sidebar-menu.js
@@ -1,24 +1,6 @@
 import React from "react";
 import { LanguageIcon, LogoutIcon, AddressBookIcon, LocationIcon } from "@egovernments/digit-ui-react-components";
-import ChangeLanguage from "../components/ChangeLanguage";
-
 const SideBarMenu = (t, closeSidebar, redirectToLoginPage, isEmployee) => [
-  {
-    type: "link",
-    element: "HOME",
-    text: t("COMMON_BOTTOM_NAVIGATION_HOME"),
-    link: isEmployee ? `/${window?.contextPath}/employee` : `/${window?.contextPath}/citizen`,
-    icon: "Home",
-    populators: {
-      onClick: closeSidebar,
-    },
-  },
-  {
-    type: "component",
-    element: "LANGUAGE",
-    action: <ChangeLanguage />,
-    icon: "LanguageIcon",
-  },
   {
     id: "login-btn",
     element: "LOGIN",
