2a4d14993b7335cba691b9dfebff2bedf53ef3de
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-10-10 14:58:05 +0530

    side bar mobileview cleanup

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/config/sidebar-menu.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/config/sidebar-menu.js
@@ -4,22 +4,6 @@ import ChangeLanguage from "../components/ChangeLanguage";
 
 const SideBarMenu = (t, closeSidebar, redirectToLoginPage, isEmployee) => [
   {
-    type: "link",
-    element: "HOME",
-    text: t("COMMON_BOTTOM_NAVIGATION_HOME"),
-    link: isEmployee ? `/${window?.contextPath}/employee` : `/${window?.contextPath}/citizen`,
-    icon: "Home",
-    populators: {
-      onClick: closeSidebar,
-    },
-  },
-  {
-    type: "component",
-    element: "LANGUAGE",
-    action: <ChangeLanguage />,
-    icon: "LanguageIcon",
-  },
-  {
     id: "login-btn",
     element: "LOGIN",
     text: t("CORE_COMMON_LOGIN"),
