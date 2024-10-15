76dddd12032317396113795cefeac9de6d87fb90
Bhavya-egov <bhavya.mangal@egovernments.org>
2024-10-15 10:32:35 +0530

    fixes

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/CampaignSummary.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/CampaignSummary.js
@@ -413,8 +413,8 @@ const CampaignSummary = (props) => {
                     {
                       key: "CAMPAIGN_NO_OF_DELIVERIES",
                       value:
-                      data?.[0]?.deliveryRules && data?.[0]?.deliveryRules?.flatMap((rule) => rule?.deliveries.map((delivery) => delivery?.deliveryIndex))?.length > 0
-                      ? Math.max(...data?.[0]?.deliveryRules?.flatMap((rule) => rule?.deliveries.map((delivery) => delivery?.deliveryIndex)))
+                      data?.[0]?.deliveryRules && data?.[0]?.deliveryRules?.flatMap((rule) => rule?.deliveries?.map((delivery) => delivery?.deliveryIndex))?.length > 0
+                      ? Math.max(...data?.[0]?.deliveryRules?.flatMap((rule) => rule?.deliveries?.map((delivery) => delivery?.deliveryIndex)))
                       : t("CAMPAIGN_SUMMARY_NA"),
                     },
                   ],
