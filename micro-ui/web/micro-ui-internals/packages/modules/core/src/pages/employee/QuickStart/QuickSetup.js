9abc46407b2b57b4cfd6ded377942419715b8c4f
aaradhya-egov <aaradhya.sahu@egovernments.org>
2024-10-16 16:44:23 +0530

    faq-component

--- micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/QuickStart/QuickSetup.js
+++ micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/QuickStart/QuickSetup.js
@@ -1,9 +1,41 @@
 import React from "react";
+import{ useState } from "react";
 import { useTranslation } from "react-i18next";
 import { CardText, CardHeader, LinkLabel, Card } from "@egovernments/digit-ui-components";
+import { ArrowForward } from "@egovernments/digit-ui-svg-components";
 import { CardSubHeader, CardSectionHeader, BreakLine, CardSectionSubText } from "@egovernments/digit-ui-react-components";
-import { Link } from "react-router-dom";
-import FaqComponent from "../../citizen/FAQs/FaqComponent";
+
+const FaqComponent = (props) => {
+  const { question, answer,type,actions,content, lastIndex } = props;
+  const [isOpen, toggleOpen] = useState(false);
+  const { t } = useTranslation();
+  const ListTag = type === "number" ? "ol" : "ul";
+  return (
+    <div className="faqs border-none" onClick={() => toggleOpen(!isOpen)}>
+      <div className="faq-question" style={{ justifyContent: "space-between", display: "flex" }}>
+        <span>{t(question)}</span>
+        <span className={isOpen ? "faqicon rotate" : "faqicon"} style={{ float: "right" }}>
+          <ArrowForward />
+        </span>
+      </div>
+      <div className="faq-answer" style={isOpen ? { display: "block" } : { display: "none" }}>
+        <CardSectionSubText style={{ marginTop: "1rem" }}>{t(content)}</CardSectionSubText>
+        <div style={{ marginTop: "1rem" }}>
+        {actions && (
+          <ListTag>
+            {actions.map((action, index) => (
+              <li key={index} style={{ listStyleType: ListTag === "ul" ? "disc" : "auto", margin: "8px 0" }}>
+                {action?.label ? <strong>{t(action?.label)}:</strong> : null} {t(action?.description)}
+              </li>
+            ))}
+          </ListTag>
+        )}
+      </div>
+      </div>
+      {!lastIndex ? <div className="cs-box-border" /> : null}
+    </div>
+  );
+};
 
 const CardC = ({ type, title, content, actions, style }) => {
   const { t } = useTranslation();
@@ -28,15 +60,13 @@ const CardC = ({ type, title, content, actions, style }) => {
   );
 };
 
-const FAQ = ({ key, title, content, faqs }) => {
+const FAQ = ({ key, title, content, faqs ,type}) => {
   const { t } = useTranslation();
   return (
     <div style={{ width: "100%" }}>
-      <CardSectionHeader>{t(title)}</CardSectionHeader>
-      <CardSectionSubText style={{ marginTop: "1rem" }}>{t(content)}</CardSectionSubText>
       <div style={{ width: "100%" }}>
         {faqs.map((faq, i) => (
-          <FaqComponent key={"faq_" + i} question={faq.question} answer={faq.answer} lastIndex={i === faqs?.length - 1} />
+          <FaqComponent key={"faq_" + i} question={faq.question} answer={faq.answer} type ={type} actions={faq.answer} content ={faq.content}lastIndex={i === faqs?.length - 1} />
         ))}
       </div>
     </div>
@@ -70,7 +100,7 @@ const QuickSetup = ({ cardConfig }) => {
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {cardConfig.map((config, index) => {
           return config?.type === "faqs" ? (
-            <FAQ key={config.id} title={config.title} content={config.content} faqs={config.actions} />
+            <FAQ key={config.id} title={config.title} type = {config?.type} content={config.content} faqs={config.actions} />
           ) : (
             <React.Fragment>
               <CardC
