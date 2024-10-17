f60f6911949f7f44179507108a66ba5f6560457a
Nipun Arora <aroranipun1@gmail.com>
2024-10-17 09:59:15 +0530

    Inbox Search component (#1537)
    * API integration for assumptions
    
    * Update Module.js
    
    * Update package.json
    
    * updated depn
    
    * Update createUpdatePlanProject.js
    
    * updates to pop inbox search componet
    
    * Updated Jurisdiction search component
    
    * updates to inbox comps
    
    * Update TablePage.js

---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/SearchJurisdiction.js
@@ -0,0 +1,103 @@
+import { LabelFieldPair, CardLabel, SubmitBar, LinkLabel } from "@egovernments/digit-ui-components";
+import React, { useEffect, Fragment, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useMyContext } from "../utils/context";
+import { Dropdown, MultiSelectDropdown } from "@egovernments/digit-ui-components";
+
+const SearchJurisdiction = ({ boundaries, jurisdiction,onSubmit=()=>{} }) => {
+  const { t } = useTranslation();
+  const {
+    state: { hierarchyType, boundaryHierarchy },
+    dispatch,
+  } = useMyContext();
+
+  //hierarchy dropdowns for this jurisdiction
+  const [hierarchy, setHierarchy] = useState(
+    Digit.Utils.microplanv1.getFilteredHierarchy(boundaryHierarchy, jurisdiction.boundaryType, hierarchyType)
+  );
+  const [boundaryOptions, setBoundaryOptions] = useState([]);
+
+  const [selectedBoundaries, setSelectedBoundaries] = useState([]);
+  const [selectedHierarchy, setSelectedHierarchy] = useState(null);
+
+  useEffect(() => {
+    if (!selectedHierarchy) {
+      setBoundaryOptions([]);
+      return;
+    }
+    //fetching user's boundaries (under their jurisdiction)
+    const userBoundaries = Digit.Utils.microplanv1.filterBoundariesByJurisdiction(boundaries, jurisdiction.boundaryCodes);
+    const filteredBoundaries = userBoundaries.filter((row) => row.type === selectedHierarchy.boundaryType);
+    const filteredBoundariesGroupedByParent = Digit.Utils.microplanv1.groupByParent(filteredBoundaries);
+    setBoundaryOptions(filteredBoundariesGroupedByParent);
+    //reset selected
+    setSelectedBoundaries([])
+    //based on the select hierarchy filter from userBoundaries and form options object
+  }, [selectedHierarchy]);
+
+  const handleBoundarySelect = (selectBoundariesEvent) => {
+    if(!selectBoundariesEvent)
+      return
+    if(selectBoundariesEvent.length===0){
+      setSelectedBoundaries([])
+      return
+    }
+
+    //otherwise your event object would look like this [[a,b],[a,b]] bs' are the boundaries that we need
+    const boundariesInEvent =  selectBoundariesEvent?.map(event => {
+      return event?.[1]
+    })
+    setSelectedBoundaries(boundariesInEvent)
+  }
+
+  return (
+    <div className={"search-wrapper"}>
+      <div className={`search-field-wrapper search custom-both-clear-search`}>
+        <LabelFieldPair style={{ flexDirection: "column", gap: "0rem" }}>
+          <CardLabel>{t("SELECT_HIERARCHY_LEVEL")}</CardLabel>
+          <Dropdown
+            t={t}
+            option={hierarchy}
+            optionKey={"i18nKey"}
+            selected={selectedHierarchy}
+            select={(value) => {
+              setBoundaryOptions([]);
+              setSelectedHierarchy(value);
+            }}
+          />
+        </LabelFieldPair>
+        <LabelFieldPair style={{ flexDirection: "column", gap: "0rem" }}>
+          <CardLabel>{t("SELECT_BOUNDARIES")}</CardLabel>
+          <MultiSelectDropdown
+            variant={"nestedmultiselect"}
+            selected={selectedBoundaries}
+            onSelect={(e) => {
+              handleBoundarySelect(e)
+            }}
+            isSearchable={true}
+            t={t}
+            addCategorySelectAllCheck={true}
+            addSelectAllCheck={true}
+            options={boundaryOptions}
+            optionsKey={"name"}
+            name={"nestedmultiselectoptions"}
+          />
+        </LabelFieldPair>
+        <div className={`search-button-wrapper`} style={{}}>
+          <LinkLabel
+            style={{ marginBottom: 0, whiteSpace: "nowrap" }}
+            onClick={() => {
+              setSelectedBoundaries([]);
+              setSelectedHierarchy(null);
+            }}
+          >
+            {t("CLEAR")}
+          </LinkLabel>
+          <SubmitBar label={t("SEARCH")} onSubmit={()=>onSubmit(selectedBoundaries)} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchJurisdiction;
