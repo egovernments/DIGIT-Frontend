bd330ec92bbad9ef2a3149b2e512a429fabee350
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2024-10-17 17:35:44 +0530

    Added Choose Activity Home Page (#1545)
    * Added Choose Activity Home Page
    
    * Removed unnecessary comments
    
    * Resolved Code rabbit comments
    
    * Resolved code rabbit comments

---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/ActivityCard.js
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
+import { Card, CardHeader } from "@egovernments/digit-ui-react-components";
+
+const ActivityHomeCard = (props) => {
+  const { t } = useTranslation();
+  const history = useHistory();
+
+  return (
+    <React.Fragment>
+  <Card className="fsm" style={{ backgroundColor: "transparent", boxShadow: "none", paddingTop: "0" }}>
+    <CardHeader> {t(props.title)} </CardHeader>
+    <div style={{ display: "grid", gridTemplateColumns: "33.3% 33.3% 33.3%", textAlign: "-webkit-center", gap: "10px" }}>
+      {props.module.map((i) => {
+        return (
+          <Card
+            key={i.name} // Always use key when rendering list items
+            style={{ 
+                minWidth: "100px", 
+                cursor: i.disable ? "not-allowed" : "pointer", 
+                opacity: i.disable ? 0.6 : 1,  // To visually indicate the disabled state
+                margin: "10px" 
+              }} // Adding margin to each card
+            onClick={() => {
+                if (!i.disable) { // Check if disabled
+                  i.link ? history.push(i.link) : location.assign(i.locate);
+                }
+              }}
+            children={
+              <>
+                {i.icon} 
+                <p>{t(i.name)}</p>
+              </>
+            }
+          ></Card>
+        );
+      })}
+    </div>
+  </Card>
+</React.Fragment>
+  );
+};
+
+export default ActivityHomeCard;
---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/ChooseActivity.js
@@ -0,0 +1,59 @@
+import React, { useMemo, useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { SVG } from "@egovernments/digit-ui-react-components";
+import ActivityHomeCard from "../../components/ActivityCard";
+
+
+const ChooseActivity = () => {
+    const { t } = useTranslation();
+
+    const disableCardBasedOnRole = (validRoles = []) => {
+        const userInfo = Digit.UserService.getUser();
+        const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
+      
+        const enabled = userRoles?.filter((role) => validRoles?.includes(role));
+      
+        return !(validRoles?.length > 0 && enabled?.length > 0);
+      };      
+
+    const activityCardData = [
+        { 
+            name: t("VALIDATE_N_APPROVE_POPULATION_DATA"), 
+            link: "", 
+            icon: <SVG.Population/>,
+            disable: disableCardBasedOnRole(["POPULATION_DATA_APPROVER", "ROOT_POPULATION_DATA_APPROVER"])
+        },
+        { 
+            name: t("ASSIGN_FACILITIES_TO_VILLAGE"), 
+            link: "", 
+            icon: <SVG.AssignmentTurnedIn/>,
+            disable: disableCardBasedOnRole(["FACILITY_CATCHMENT_MAPPER", "ROOT_FACILITY_CATCHMENT_MAPPER"])
+        },
+        { 
+            name: t("VALIDATE_N_APPROVE_MICROPLAN_ESTIMATIONS"), 
+            link: "", 
+            icon: <SVG.FactCheck/>,
+            disable: disableCardBasedOnRole(["PLAN_ESTIMATION_APPROVER", "ROOT_PLAN_ESTIMATION_APPROVER"]),
+        },
+        { 
+            name: t("GEOSPATIAL_MAP_VIEW"), 
+            link: null, 
+            icon: <SVG.LocationOn/>,
+            disable: disableCardBasedOnRole()
+        },
+        { 
+            name: t("VIEW_MICROPLAN_ESTIMATIONS"), 
+            link: null, 
+            icon: <SVG.Visibility/>,
+            disable: disableCardBasedOnRole()
+        }
+      ];
+
+    return (
+        <React.Fragment>
+            <ActivityHomeCard title={t("SELECT_AN_ACTIVITY_TO_CONTINUE")} module={activityCardData}/>
+        </React.Fragment>
+    )
+}
+
+export default ChooseActivity;
