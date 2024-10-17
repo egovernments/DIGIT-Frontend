0508e7bcc2a8deb21e2bfad395e207d0fa4f12db
Nipun Arora <aroranipun1@gmail.com>
2024-10-17 20:31:42 +0530

    updates to inbox, UICust (#1552)
    updates

--- health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
+++ health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
@@ -2,6 +2,7 @@ import _ from "lodash";
 import { useLocation, useHistory,Link,useParams } from "react-router-dom";
 import React from "react";
 import { Dropdown } from "@egovernments/digit-ui-components";
+import { DeleteIconv2, DownloadIcon, FileIcon, Button, Card, CardSubHeader,EditIcon,ArrowForward } from "@egovernments/digit-ui-react-components";
 //create functions here based on module name set in mdms(eg->SearchProjectConfig)
 //how to call these -> Digit?.Customizations?.[masterName]?.[moduleName]
 // these functions will act as middlewares
@@ -778,22 +779,130 @@ export const UICustomizations = {
       data.body.PlanConfigurationSearchCriteria.status = status?.status;
       cleanObject(data.body.PlanConfigurationSearchCriteria);
 
+      const dic = { '0': null, '1': ["DRAFT"], '2': ["EXECUTION_TO_BE_DONE"], '3': ["CENSUS_DATA_APPROVAL_IN_PROGRESS", "CENSUS_DATA_APPROVED", "RESOURCE_ESTIMATION_IN_PROGRESS"], "4": ["RESOURCE_ESTIMATIONS_APPROVED"] }
+      const url = Digit.Hooks.useQueryParams();
+
+      const tabId = url.tabId || '0'; // Default to '0' if tabId is undefined
+      data.body.PlanConfigurationSearchCriteria.status = dic[String(tabId)];
+      cleanObject(data.body.PlanConfigurationSearchCriteria)
       return data;
     },
     additionalCustomizations: (row, key, column, value, t, searchResult) => {
       if (key === "Actions") {
+        // `/${window.contextPath}/employee/microplan/setup-microplan?key=${9}&preview=${true}&action=${false}`
         return (
           <Dropdown
             option={[
               { code: "1", name: "Edit Setup" },
-              { code: "2", name: "Duplicate Setup" },
-              { code: "3", name: "View Summary" },
+              { code: "2", name: "View Summary" },
             ]}
-            select={(e) => { console.log(e, "event") }}
+            select={(e) => {
+              console.log(e, "event"); // e contains the selected option
+              if (e.code === "1") {
+                // Use window.location.href to navigate
+                window.location.href = `/${window.contextPath}/employee/microplan/setup-microplan?key=${1}&preview=${true}&action=${false}&microplanId=${row.id}&campaignId=${row.CampaignDetails.id}`;
+              }
+              if (e.code === "2") {
+                // Use window.location.href to navigate
+                window.location.href = `/${window.contextPath}/employee/microplan/setup-microplan?key=${9}&preview=${true}&action=${false}&microplanId=${row.id}&campaignId=${row.CampaignDetails.id}`;
+              }
+            }}
             optionKey={"name"}
             selected={{ code: "1", name: "Actions" }}
-          ></Dropdown>
+          />
           // <p>$${value}</p>
+
+        );
+      }
+
+      if (key === "Name of the Microplan") {
+        if (value && value !== "NA") {
+
+          return (
+            <div
+              style={{
+                maxWidth: '15rem', // Set the desired maximum width
+                wordWrap: 'break-word', // Allows breaking within words
+                whiteSpace: 'normal', // Ensures text wraps normally
+                overflowWrap: 'break-word' // Break long words at the edge
+              }}
+            >
+              <p>{value}</p>
+            </div>
+          )
+        } else {
+          return (
+            <div>
+              <p>NA</p>
+            </div>
+          )
+        }
+
+      }
+
+    },
+  },
+  MyMicroplanSearchConfig: {
+    preProcess: (data, additionalDetails) => {
+      const { name, status } = data?.state?.searchForm || {};
+
+      data.body.PlanConfigurationSearchCriteria = {};
+      data.body.PlanConfigurationSearchCriteria.limit = data?.state?.tableForm?.limit;
+      // data.body.PlanConfigurationSearchCriteria.limit = 10
+      data.body.PlanConfigurationSearchCriteria.offset = data?.state?.tableForm?.offset;
+      data.body.PlanConfigurationSearchCriteria.name = name;
+      data.body.PlanConfigurationSearchCriteria.tenantId = Digit.ULBService.getCurrentTenantId();
+      data.body.PlanConfigurationSearchCriteria.userUuid = Digit.UserService.getUser().info.uuid;
+      // delete data.body.PlanConfigurationSearchCriteria.pagination
+      data.body.PlanConfigurationSearchCriteria.status = status?.status;
+      cleanObject(data.body.PlanConfigurationSearchCriteria);
+
+      const dic = { '0': null, '1': ["DRAFT"], '2': ["EXECUTION_TO_BE_DONE"], '3': ["CENSUS_DATA_APPROVAL_IN_PROGRESS", "CENSUS_DATA_APPROVED", "RESOURCE_ESTIMATION_IN_PROGRESS"], "4": ["RESOURCE_ESTIMATIONS_APPROVED"] }
+      const url = Digit.Hooks.useQueryParams();
+
+      const tabId = url.tabId || '0'; // Default to '0' if tabId is undefined
+      data.body.PlanConfigurationSearchCriteria.status = dic[String(tabId)];
+      cleanObject(data.body.PlanConfigurationSearchCriteria)
+      return data;
+    },
+    additionalCustomizations: (row, key, column, value, t, searchResult) => {
+      if (key === "Actions") {
+        // `/${window.contextPath}/employee/microplan/setup-microplan?key=${9}&preview=${true}&action=${false}`
+        return (
+
+          row.status === "DRAFT" ? (<Button
+            label={t("WBH_EDIT")}
+            variation="secondary"
+            icon={<EditIcon styles={{ height: "1.25rem", width: "2.5rem" }} />}
+            type="button"
+            className="dm-workbench-download-template-btn dm-hover"
+            onButtonClick={(e) => {
+              
+            }}
+          />) : (row.status === "EXECUTION_TO_BE_DONE") ? (
+            <Button
+              label={t("START")}
+              variation="secondary"
+              icon={<ArrowForward styles={{ height: "1.25rem", width: "2.5rem" }} />}
+              type="button"
+              className="dm-workbench-download-template-btn dm-hover"
+              onButtonClick={(e) => {
+                
+              }}
+            />
+          ) : (row.status === "RESOURCE_ESTIMATIONS_APPROVED") ? (
+            <Button
+              label={t("WBH_DOWNLOAD")}
+              variation="secondary"
+              icon={<DownloadIcon styles={{ height: "1.25rem", width: "2.5rem" }} />}
+              type="button"
+              className="dm-workbench-download-template-btn dm-hover"
+              onButtonClick={(e) => {
+                
+              }}
+            />
+          ) : null
+
 
         );
       }
@@ -831,25 +940,26 @@ export const UICustomizations = {
     customValidationCheck: (data) => {
       const { phone } = data;
       const mobileRegex = /^[0-9]{10}$/;
-      
+
       // Allow empty mobile number
       if (!phone || phone.trim() === "") {
-        return false; 
+        return false;
       }
-    
+
       // Check if phone matches the regex
       if (!mobileRegex.test(phone)) {
         return { error: true, label: "INVALID_MOBILE_NUMBER" }; // Return an error message if invalid
       }
-    
+
       return false;
     },
-    preProcess: (data, additionalDetails) => {
 
+
+    preProcess: (data, additionalDetails) => {
       const { phone, name } = data?.state?.searchForm || {}
       const { sortOrder } = data?.state?.filterForm || {}
       let { roleschosen } = data?.state?.filterForm || []
-      
+
       if (!roleschosen) {
         roleschosen = {}
       }
@@ -865,19 +975,17 @@ export const UICustomizations = {
       if (roleschosen) {
         rolesString = Object.keys(roleschosen).filter(role => roleschosen[role] === true).join(',');
       }
-      
-      
+
 
       data.params.names = name;
 
       data.params.phone = phone;
-      
+
       data.params.roles = rolesString;
       data.params.tenantId = Digit.ULBService.getCurrentTenantId();
       cleanObject(data.params);
       delete data.params.roleschosen;
       delete data.params.name;
-      
 
       return data
     },
@@ -885,14 +993,13 @@ export const UICustomizations = {
     rolesForFilter: (props) => {
       const userInfo = Digit.UserService.getUser();
       const tenantId = Digit.ULBService.getCurrentTenantId();
-      
       return {
         params: {},
         url: '/mdms-v2/v2/_search', //mdms fetch from
 
         body: {
           MdmsCriteria: {
-            tenantId:  Digit.ULBService.getCurrentTenantId(),
+            tenantId: Digit.ULBService.getCurrentTenantId(),
             filters: {},
             schemaCode: "hcm-microplanning.rolesForMicroplan",
             limit: 10,
@@ -908,11 +1015,14 @@ export const UICustomizations = {
                 {
                   roleCode: item.data.roleCode,
                   i18nKey: Digit.Utils.locale.getTransformedLocale(`MP_ROLE_${item.data.roleCode}`)
+                  // orderNumber: item.data.orderNumber
+
+                  // roleCode:{labelKey:item.data.roleCode}
 
                 }
               )
             })
-            
+
             return roles
           },
         },
@@ -920,33 +1030,25 @@ export const UICustomizations = {
       }
     },
 
-
-
-
     additionalCustomizations: (row, key, column, value, t, searchResult) => {
-      
-      
       if (key === "Role") {
-        
-        if (value && value !== "NA") {
+        return (
+          <div>
+            {value.map((item, index) => (
+              <span key={index} className="dm-code">
+                {t(`MP_ROLE_${item.code}`)}
+                {index < value.length - 1 && ", "}
+              </span>
+            ))}
+          </div>
+        );
 
-          return (
-            <div>
-              {value.map((item, index) => (
-                <span key={index} className="dm-code">
-                  {Digit.Utils.locale.getTransformedLocale(`MP_ROLE_${item.code}`)}
-                  {index < value.length - 1 && ", "}
-                </span>
-              ))}
-            </div>
-          );
-        } 
 
-      }else{
-        return(
-          <div>NA</div>
-        )
       }
+
+
+
+
 
     },
 
--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/PopInbox.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/PopInbox.js
@@ -1,7 +1,8 @@
-import React,{Fragment} from 'react'
-import SearchJurisdiction from '../../components/SearchJurisdiction'
-import { boundaries } from '../../components/boundaries'
-
+import React, { Fragment, useState, useEffect } from "react";
+import SearchJurisdiction from "../../components/SearchJurisdiction";
+import { boundaries } from "../../components/boundaries";
+import PopInboxTable from "../../components/PopInboxTable";
+import { Card, Tab } from "@egovernments/digit-ui-components";
 const jurisdiction = {
   boundaryType: "District",
   boundaryCodes: [
@@ -9,21 +10,67 @@ const jurisdiction = {
     // "MICROPLAN_MO_05_04_GBLONEE",
     // "MICROPLAN_MO_04_06_KUNGBOR"
     // "MICROPLAN_MO"
-    "MICROPLAN_MO_05_06_JEADE"
+    "MICROPLAN_MO_05_06_JEADE",
   ],
 };
 
-const PopInbox = () => {
+const statusMap = {
+  PENDING_FOR_APPROVAL: 4,
+  PENDING_FOR_VALIDATION: 4,
+  APPROVED: 6,
+  VALIDATED: 3,
+};
 
-  
+const PopInbox = () => {
+  const [showTab, setShowTab] = useState(true);
+  const [activeLink,setActiveLink] = useState({
+    code: "ASSIGNED_TO_ME",
+    name: "ASSIGNED_TO_ME",
+  })
   const onSearch = (selectedBoundaries) => {
     console.log(selectedBoundaries);
+  };
+  const onFilter = (selectedStatus) => {
+    console.log(selectedStatus);
   }
-  return (
-    <>
-      <SearchJurisdiction boundaries={boundaries()} jurisdiction={jurisdiction} onSubmit={onSearch} />
-    </>
-  )
-}
+  // need to add table and filter card
 
-export default PopInbox
+  return (
+    <div className="pop-inbox-wrapper">
+      <SearchJurisdiction boundaries={boundaries()} jurisdiction={jurisdiction} onSubmit={onSearch} />
+
+      <div className={"pop-inbox-table-wrapper"}>
+        {showTab && (
+          <Tab
+            activeLink={activeLink.code}
+            configItemKey="code"
+            configNavItems={[
+              {
+                code: "ASSIGNED_TO_ME",
+                name: "ASSIGNED_TO_ME",
+              },
+              {
+                code: "ASSIGNED_TO_ALL",
+                name: "ASSIGNED_TO_ALL",
+              }
+            ]}
+            itemStyle={{}}
+            navStyles={{}}
+            onTabClick={(e)=>{
+              setActiveLink(e)
+              
+            }}
+            setActiveLink={setActiveLink}
+            showNav
+            style={{}}
+          />
+        )}
+        <Card>
+          <PopInboxTable />
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default PopInbox;
