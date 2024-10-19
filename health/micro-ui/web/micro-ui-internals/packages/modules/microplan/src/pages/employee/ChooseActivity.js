38bf6a0e88206f000d03db986004518019718786
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-10-19 11:27:44 +0530

    Connected population and facility to village screen

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/ChooseActivity.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/ChooseActivity.js
@@ -6,33 +6,33 @@ import ActivityHomeCard from "../../components/ActivityCard";
 
 const ChooseActivity = () => {
     const { t } = useTranslation();
+    const { campaignId, microplanId,  } = Digit.Hooks.useQueryParams();
 
     const disableCardBasedOnRole = (validRoles = []) => {
         const userInfo = Digit.UserService.getUser();
         const userRoles = userInfo?.info?.roles?.map((roleData) => roleData?.code);
-      //to see different flows should be removed later
+        //to see different flows should be removed later
         return false;
         const enabled = userRoles?.filter((role) => validRoles?.includes(role));
-      
         return !(validRoles?.length > 0 && enabled?.length > 0);
       };      
 
     const activityCardData = [
         { 
             name: t("VALIDATE_N_APPROVE_POPULATION_DATA"), 
-            link: "", 
+            link: `pop-inbox?campaignId=${campaignId}&microplanId=${microplanId}`, 
             icon: <SVG.Population/>,
             disable: disableCardBasedOnRole(["POPULATION_DATA_APPROVER", "ROOT_POPULATION_DATA_APPROVER"])
         },
         { 
             name: t("ASSIGN_FACILITIES_TO_VILLAGE"), 
-            link: "", 
+            link: `assign-facilities-to-villages?campaignId=${campaignId}&microplanId=${microplanId}`, 
             icon: <SVG.AssignmentTurnedIn/>,
             disable: disableCardBasedOnRole(["FACILITY_CATCHMENT_MAPPER", "ROOT_FACILITY_CATCHMENT_MAPPER"])
         },
         { 
             name: t("VALIDATE_N_APPROVE_MICROPLAN_ESTIMATIONS"), 
-            link: "", 
+            link: `pop-inbox?campaignId=${campaignId}&microplanId=${microplanId}`, 
             icon: <SVG.FactCheck/>,
             disable: disableCardBasedOnRole(["PLAN_ESTIMATION_APPROVER", "ROOT_PLAN_ESTIMATION_APPROVER"]),
         },
@@ -44,7 +44,7 @@ const ChooseActivity = () => {
         },
         { 
             name: t("VIEW_MICROPLAN_ESTIMATIONS"), 
-            link: null, 
+            link: `pop-inbox?campaignId=${campaignId}&microplanId=${microplanId}`, 
             icon: <SVG.Visibility/>,
             disable: disableCardBasedOnRole()
         }
