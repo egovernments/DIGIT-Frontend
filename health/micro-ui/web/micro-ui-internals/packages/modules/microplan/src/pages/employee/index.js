0de3e8c2fe9549845632b83e49df0552c0cc0a26
Abishek <abishek.t@egovernments.org>
2024-10-21 13:54:46 +0530

    index path for response

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/index.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/index.js
@@ -190,8 +190,12 @@ const App = ({ path, stateCode, userType, tenants, BOUNDARY_HIERARCHY_TYPE, hier
         <PrivateRoute path={`${path}/village-view`} component={() => <VillageView />} />
         <PrivateRoute path={`${path}/my-microplans`} component={() => <MyMicroplans/>} /> 
         <PrivateRoute path={`${path}/upload-user-success`} component={() => <Response/>} /> 
+        <PrivateRoute path={`${path}/setup-completed-response`} component={() => <Response/>} /> 
+
 
          <PrivateRoute path={`${path}/assign-facilities-to-villages`} component={() => <FacilityCatchmentMapping/>} />
+         
+        
 
 
       </AppContainer>
