926b625ef66a236916aab4a67220be2bbf2ab7a8
Bhargav-egov <bhargav.gowda@equidhi.org>
2024-10-21 20:13:33 +0530

    Formula Screen (#1599)
    * formula configuration
    
    * Apply suggestions from code review
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nipun Arora <aroranipun1@gmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FormulaConfigWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FormulaConfigWrapper.js
@@ -1,36 +1,437 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, createContext, useContext } from 'react';
 import FormulaConfiguration from './FormulaConfiguration';
+import { Stepper, TextBlock, ActionBar, Button, Card, Toast, Loader } from '@egovernments/digit-ui-components';
 import { useMyContext } from '../utils/context';
+import { useTranslation } from "react-i18next";
+ 
+const FormulaContext = createContext('formulaContext');
 
+export const useFormulaContext = () => {
+   return useContext(FormulaContext);
+};
 
 const FormulaConfigWrapper = ({onSelect, props:customProps})=>{
     
-    const [selectedCategory, setSelectedCategory] = useState("GENERAL_ESTIMATION"); 
-    const { state, dispatch } = useMyContext();
+   const { mutate: updateResources, ...rest } = Digit.Hooks.microplanv1.useCreateUpdatePlanProject();
+   const { t } = useTranslation();
+   const { state} = useMyContext();
+   const [formulaConfigValues, setFormulaConfigValues] = useState(customProps?.sessionData?.FORMULA_CONFIGURATION?.formulaConfiguration?.formulaConfigValues || [])
+   const assumptionsFormValues = customProps?.sessionData?.ASSUMPTIONS_FORM?.assumptionsForm //array with key value pair
+   const campaignType =  customProps?.sessionData?.CAMPAIGN_DETAILS?.campaignDetails?.campaignType?.code
+   const resourceDistributionStrategyCode= customProps?.sessionData?.CAMPAIGN_DETAILS?.campaignDetails?.distributionStrat?.resourceDistributionStrategyCode
+   const searchParams = new URLSearchParams(window.location.search);
+   const [formulaInternalKey, setFormulaInternalKey] = useState(() => {
+      const keyParam = searchParams.get("formulaInternalKey");
+      return keyParam ? parseInt(keyParam) : 1;
+  });
+  const [showToast, setShowToast] = useState(null);
+  const [deletedFormulas, setDeletedFormulas] = useState([]);
+  const [executionCount, setExecutionCount] = useState(0);
+  const [assumptions, setAssumptions] = useState([])
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+
+  const { campaignId, microplanId, key, ...queryParams } = Digit.Hooks.useQueryParams();
+  const { isLoading: isLoadingPlanObject, data: planObject, error: errorPlan, refetch: refetchPlan } = Digit.Hooks.microplanv1.useSearchPlanConfig(
+   {
+     PlanConfigurationSearchCriteria: {
+       tenantId,
+       id: microplanId,
+     },
+   },
+   {
+     enabled: microplanId ? true : false,
+   //   queryKey: currentKey,
+   }
+ );
+
+
    
+
+
+   const navigateBack = () => {
+      if(formulaInternalKey >1){
+         setFormulaInternalKey((prevKey) => prevKey - 1); 
+      }  
+   };
+   const isFormulaLastStep = () => {
+      Digit.Utils.microplanv1.updateUrlParams({ isFormulaLastVerticalStep:false }); 
+      Digit.Utils.microplanv1.updateUrlParams({ formulaInternalKey:formulaInternalKey }); 
+   }  
+
+   const updateUrlParams = (params) => {
+      const url = new URL(window.location.href);
+      Object.entries(params).forEach(([key, value]) => {
+          url.searchParams.set(key, value);
+      });
+      window.history.replaceState({}, "", url);
+  };
+
+  const handleFormulaChange = (output, type, value, category) => {
+   //const newValue = event.target.value;
+
+   setFormulaConfigValues((prevValues) => {
+       return prevValues.map((formula) => {
+           // If the key matches, update the value; otherwise, return the existing assumption
+           if (formula.output === output) {
+               return {
+                   ...formula,
+                   source:"MDMS",
+                   category,
+                   [type]:value.code, // Set to null if input is empty
+               };
+           }
+           return formula;
+       });
+   });
+};
+
+
+
+
+const filteredAutoFilledRuleConfigurations = state.AutoFilledRuleConfigurations?.filter((item) => {
+   const isHouseToHouseOrFixedPost =  resourceDistributionStrategyCode === "HOUSE_TO_HOUSE" || 
+                                       resourceDistributionStrategyCode === "FIXED_POST";
+  
+    if(isHouseToHouseOrFixedPost){
+        return (
+           campaignType === item.campaignType &&
+           resourceDistributionStrategyCode === item.resourceDistributionStrategyCode &&
+           assumptionsFormValues?.selectedRegistrationDistributionMode?.code === item.isRegistrationAndDistributionHappeningTogetherOrSeparately
+
+        ) 
+    }
+
+   return (
+            campaignType === item.campaignType &&
+            resourceDistributionStrategyCode === item.resourceDistributionStrategyCode &&
+            assumptionsFormValues?.selectedRegistrationProcess?.code===item?.RegistrationProcess && 
+            assumptionsFormValues?.selectedDistributionProcess?.code=== item.DistributionProcess 
+        )
+    });  
+    const [filteredFormulas, setFilteredFormulas] = useState([])
+    const ruleConfigurationCategories = filteredAutoFilledRuleConfigurations?.length > 0 ? filteredAutoFilledRuleConfigurations[0].ruleConfigurationCategories : []; 
+    //array of objects each with input, output, operatorName, assumptionValue
+   
+    const currentCategoryRuleConfigurations = ruleConfigurationCategories?.length > 0 ? (ruleConfigurationCategories[formulaInternalKey - 1]?.ruleConfigurations || []) : []; 
+    const currentCategory = ruleConfigurationCategories[formulaInternalKey - 1]?.category;
+    useEffect(()=>{
+      
+      setFilteredFormulas(currentCategoryRuleConfigurations) 
+    }, [currentCategoryRuleConfigurations])
+   
+
+
+
+
+
+const handleNext = () => {
+       //array of objects each with input, output, operatorName, assumptionValue
+   const currentRuleConfigurations = ruleConfigurationCategories[formulaInternalKey - 1]?.ruleConfigurations || [];
+   const existingFormulaOutputs = formulaConfigValues?.map(formula => formula.output);
+
+   //Filter current rule Configurations to only those that exist in formulaConfigValues and are not deleted
+   const visibleFormulas = currentRuleConfigurations.filter(item => 
+       existingFormulaOutputs?.includes(item.output) && !deletedFormulas?.includes(item.output)
+   );
+
+   //Validate: Check if any value is empty for visible Formulas
+   const hasEmptyFields = visibleFormulas.some(item => {
+       const formulaConfig = formulaConfigValues.find(formula => formula.output === item.output)
+       return !formulaConfig || !formulaConfig.input || !formulaConfig.operatorName || !formulaConfig.assumptionValue;// Check if any value is empty
+   });
+
+   
+
+   // Move to the next step if validation passes
+   if (formulaInternalKey < ruleConfigurationCategories?.length) {
+       setFormulaInternalKey((prevKey) => prevKey + 1); // Update key in URL
+   }
+
+
+};
+
+
+const handleBack = () => {
+   if (formulaInternalKey >1) {
+       setFormulaInternalKey((prevKey) => prevKey - 1); // Update key in URL
+   }else{
+       window.dispatchEvent(new Event("revertToPreviousScreenFromFormula"))
+   }
+};
+
+const handleStepClick = (step)=>{
+       
+   // Step is zero-based, so we adjust by adding 1
+   const currentStepIndex = formulaInternalKey - 1; // Current step index (zero-based)
+   
+   // // Check if the clicked step is the next step
+   if (step === currentStepIndex + 1) {
+       // Validate current step
+       const currentRuleConfigurations = ruleConfigurationCategories[currentStepIndex]?.ruleConfigurations || [];
+       const existingFormulaOutputs = formulaConfigValues?.map(formula => formula.output);
+       const visibleFormulas = currentRuleConfigurations?.filter(item => 
+         existingFormulaOutputs?.includes(item.output) && !deletedFormulas?.includes(item.output)
+     );
+
+       // Check for empty fields in visible formulas 
+   const hasEmptyFields = visibleFormulas.some(item => {
+      const formulaConfig = formulaConfigValues.find(formula => formula.output === item.output)
+      return !formulaConfig || !formulaConfig.input || !formulaConfig.operatorName || !formulaConfig.assumptionValue;// Check if any value is empty
+  });
+
+     
+       if (hasEmptyFields) {
+           setShowToast({
+               key: "error",
+               label: t("ERR_MANDATORY_FIELD"),
+               transitionTime: 3000,
+           });
+           return; 
+       }
+
+       // Move to the next step if validation passes
+       setFormulaInternalKey(step + 1); // Move to the next step
+   } 
+   // Allow going back to any previous step
+   else if (step < currentStepIndex) {
+       setFormulaInternalKey(step + 1); // Move back to the selected step
+   }
+   // Prevent jumping ahead to a later step if the user hasn't filled the required fields
+   else if (step > currentStepIndex + 1) {
+       setShowToast({
+           key: "error",
+           label: t("ERR_SKIP_STEP"),
+           transitionTime: 3000,
+       });
+   }
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+ useEffect(() => {
+   window.addEventListener("verticalStepper", navigateBack);
+   return () => {
+     window.removeEventListener("verticalStepper", navigateBack);
+   };
+ }, [formulaInternalKey]);
+
+ useEffect(() => {
+   useEffect(() => {
+     window.addEventListener("isFormulaLastStep", isFormulaLastStep);
+     return () => {
+       window.removeEventListener("isFormulaLastStep", isFormulaLastStep);
+     };
+   }, []);
+
+
+
+ useEffect(() => {
+   onSelect(customProps.name, { formulaConfigValues });
+}, [formulaConfigValues, formulaInternalKey]);
+useEffect(() => {
+   if (executionCount < 5) {
+       onSelect(customProps.name, {formulaConfigValues})
+       setExecutionCount((prevCount) => prevCount + 1);
+   }
+ }); 
+ useEffect(() => {
+   updateUrlParams({ formulaInternalKey,});
+}, [formulaInternalKey]);
+
+
+useEffect(() => {
+   const initialFormulas = filteredFormulas.map(item => ({
+       source:"MDMS",
+       category:currentCategory,
+       input:item.input,
+       output:item.output,
+       operatorName:item.operatorName,
+       assumptionValue:item.assumptionValue,
+       showOnEstimationDashboard:false,
+   }));
+
+   const existingOutputs = new Set(formulaConfigValues.map(formula => formula.output));
+   const newFormulas = initialFormulas?.filter(formula => 
+       !existingOutputs.has(formula.output) &&
+       !deletedFormulas.includes(formula.output)
+   );
+
+  
+   setFormulaConfigValues(prev => [...prev, ...newFormulas])
+}, [filteredFormulas]);
+
+
+
+
+
+//to get existing assumptions to provide options for assumptionValue dropdown
+
+useEffect(() => {
+    // Step 1: Filter assumptions based on current category
+    if(planObject?.assumptions?.length > 0 && !isLoadingPlanObject){
+      const filteredAssumptions = planObject?.assumptions.filter(
+         assumption => assumption.category === currentCategory
+     );
+    
+ 
+     // Step 2: Extract keys from filtered assumptions
+     const keys = filteredAssumptions?.map(assumption => assumption.key);
+     setAssumptions(keys);
+    }
+    
+    
+}, [currentCategory, planObject?.assumptions, formulaConfigValues, setFormulaConfigValues ]);
+
+
+
+   
+useEffect(() => {
+   if (formulaInternalKey === ruleConfigurationCategories?.length) {
+       Digit.Utils.microplanv1.updateUrlParams({ isFormulaLastVerticalStep:true });
+   }else{ // Assuming 1 is the first step
+       Digit.Utils.microplanv1.updateUrlParams({ isFormulaLastVerticalStep:false });
+   }
+}, [formulaInternalKey]);
+
+//array of objects each with operatorCode and operatorName
+const operators =  state.RuleConfigureOperators
+const filteredRuleConfigureInputs = state.RuleConfigureInputs.filter((item) => {
+   const isHouseToHouseOrFixedPost =  resourceDistributionStrategyCode === "HOUSE_TO_HOUSE" || 
+                                       resourceDistributionStrategyCode === "FIXED_POST";
+  
+    if(isHouseToHouseOrFixedPost){
+        return (
+           campaignType === item?.campaignType &&
+           resourceDistributionStrategyCode === item?.resourceDistributionStrategyCode &&
+           assumptionsFormValues?.selectedRegistrationDistributionMode?.code === item?.isRegistrationAndDistributionHappeningTogetherOrSeparately
+
+        ) 
+    }
+
+   return (
+            campaignType === item?.campaignType &&
+            resourceDistributionStrategyCode === item?.resourceDistributionStrategyCode &&
+            assumptionsFormValues?.selectedRegistrationProcess?.code===item?.RegistrationProcess && 
+            assumptionsFormValues?.selectedDistributionProcess?.code=== item?.DistributionProcess 
+        )
+    }); 
+
+   const inputCategories = filteredRuleConfigureInputs?.length > 0 ? filteredRuleConfigureInputs[0].inputCategories : []; 
+    //array of input labels
+    const filteredInputs = inputCategories?.length > 0 ? (inputCategories[formulaInternalKey - 1]?.inputs || []) : [];  
+
     
 
- customProps.ruleConfigurations = state.AutoFilledRuleConfigurations?.[0]?.ruleConfigurationCategories?.[0]?.ruleConfigurations
- customProps.ruleConfigureOperators  = state.RuleConfigureOperators
- customProps.ruleConfigureOutput    = state.RuleConfigureOutput[0].outputCategories[0].outputs
- 
+
+
+
+
 
  
+const filteredRuleConfigureOutputs = state.RuleConfigureOutput?.filter((item) => {
+const isHouseToHouseOrFixedPost =  resourceDistributionStrategyCode === "HOUSE_TO_HOUSE" || 
+                                    resourceDistributionStrategyCode === "FIXED_POST";
+
+   if(isHouseToHouseOrFixedPost){
+      return (
+         campaignType === item?.campaignType &&
+         resourceDistributionStrategyCode === item?.resourceDistributionStrategyCode &&
+         assumptionsFormValues?.selectedRegistrationDistributionMode?.code === item?.isRegistrationAndDistributionHappeningTogetherOrSeparately
+
+      ) 
+   }
+
+return (
+         campaignType === item?.campaignType &&
+         resourceDistributionStrategyCode === item?.resourceDistributionStrategyCode &&
+         assumptionsFormValues?.selectedRegistrationProcess?.code===item?.RegistrationProcess && 
+         assumptionsFormValues?.selectedDistributionProcess?.code=== item?.DistributionProcess 
+      )
+   });    
+     
+   const outputCategories = filteredRuleConfigureOutputs?.length > 0 ? filteredRuleConfigureOutputs[0].outputCategories : []; 
+   //array of ouput labels
+   const filteredOutputs = outputCategories?.length > 0 ? (outputCategories[formulaInternalKey - 1]?.outputs || []) : [];
+   
+   
+
+ customProps.filteredInputs = filteredInputs
+ customProps.filteredOutputs = filteredOutputs
+ customProps.operators = operators
+ customProps.assumptions = assumptions
+
+
+ if(isLoadingPlanObject){
+   return <Loader/>
+ }
  
- 
- 
+
   
 
-    return <>
-             <div>
-                 <FormulaConfiguration
-                    customProps={customProps}
-                    category={selectedCategory}
-                    onSelect={onSelect}
-                  />
-                  
-             </div>  
-          </>
+    return (
+      <Fragment>
+         <FormulaContext.Provider value={{formulaConfigValues, handleFormulaChange, setFormulaConfigValues, deletedFormulas, setDeletedFormulas, planObject}}>
+            <div style={{ display: "flex",gap:"2rem" }}>
+               <div className="card-container">
+                  <Card className="card-header-timeline">
+                     <TextBlock subHeader={t("FORMULA_CONFIGURATION")} subHeaderClasName={"stepper-subheader"} wrapperClassName={"stepper-wrapper"} />
+                  </Card>
+                  <Card className="stepper-card">
+                     <Stepper
+                        customSteps={ruleConfigurationCategories.map(category => category.category)}
+                        currentStep={formulaInternalKey}
+                        onStepClick={handleStepClick}
+                        direction={"vertical"}
+                     />
+                  </Card>
+               </div>
+
+
+               <div style={{width: '100%'                    
+                }}>
+                   <FormulaConfiguration
+                     category={ruleConfigurationCategories[formulaInternalKey - 1]?.category}
+                     formulas={filteredFormulas?.filter(item => !deletedFormulas?.includes(item.output))}
+                     onSelect={onSelect}
+                     customProps={customProps}
+                   
+                   />  
+
+                </div>
+            </div>
+
+            { (formulaInternalKey > 0 && formulaInternalKey < ruleConfigurationCategories?.length ) && (
+                        <ActionBar>
+                            <Button className="previous-button" variation="secondary" label={t("BACK")} onClick={handleBack} />
+                            <Button className="previous-button" variation="primary" label={t("NEXT")} onClick={handleNext} />
+                        </ActionBar>
+             )}
+         </FormulaContext.Provider>
+
+
+           {showToast && (
+                            <Toast
+                                type={showToast.key === "error" ? "error" : "success"} // Adjust based on your needs
+                                label={t(showToast.label)}
+                                transitionTime={showToast.transitionTime}
+                                onClose={() => {
+                                    setShowToast(false);
+                                }}
+                                isDleteBtn={true}
+                            />
+)}
+      </Fragment>
+    )
 
 
 
@@ -39,4 +440,3 @@ const FormulaConfigWrapper = ({onSelect, props:customProps})=>{
 
 
 export default FormulaConfigWrapper
-
--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/hooks/services/createUpdatePlanProject.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/hooks/services/createUpdatePlanProject.js
@@ -339,6 +339,52 @@ const createUpdatePlanProject = async (req) => {
 
         await updatePlan(upatedPlanObjSubHypothesis);
         return;
+
+      case "FORMULA_CONFIGURATION":
+            
+        //fetch current plan
+        const fetchedPlanForFormula = await searchPlanConfig({
+          PlanConfigurationSearchCriteria: {
+            tenantId,
+            id: microplanId,
+          },
+        })
+        
+        //here we can always invalidate prev assumptions
+        const prevFormulas = fetchedPlanForFormula?.operations?.map(row => {
+          const updatedRow = {
+            ...row,
+            active:false
+          }
+          return updatedRow
+        })
+        const formulasToUpdate = totalFormData?.FORMULA_CONFIGURATION?.formulaConfiguration?.formulaConfigValues?.filter(row => {
+          return (row.category && row.output && row.input && row.operatorName && row.assumptionValue) 
+        })
+        const updatedPlanObjFormula = {
+          ...fetchedPlanForFormula,
+          operations:[
+            ...prevFormulas,
+            ...formulasToUpdate
+          ]
+        }
+
+        const planResFormula = await updatePlan(updatedPlanObjFormula);
+        if(planResFormula?.PlanConfiguration?.[0]?.id){
+        
+          setCurrentKey((prev) => prev + 1);
+          setCurrentStep((prev) => prev + 1);
+          window.dispatchEvent(new Event("isFormulaLastStep"))
+          
+          return {
+            triggeredFrom,
+          };
+        }else {
+          setShowToast({ key: "error", label: "ERR_ASSUMPTIONS_FORM_UPDATE" });
+        }
+        
+        
+       
       case "UPLOADBOUNDARYDATA":
         const fetchedPlanForBoundary = await searchPlanConfig({
           PlanConfigurationSearchCriteria: {
