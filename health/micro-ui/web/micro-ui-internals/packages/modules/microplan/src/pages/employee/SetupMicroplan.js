96ccb9f48c72111001e0775f7ee7f3c23d3c2935
abishekTa-egov <abishek.t@egovernments.org>
2024-10-22 11:23:57 +0530

    Role access table (#1606)
    * removed iterator
    
    * changes
    
    * EOD checkopoint
    
    * validation for user role
    
    * Response and user-employee validation working
    
    * Pull from console
    
    * console.log and debuggers removed
    
    * index path for response
    
    * Making some changes
    
    * console removed
    
    * random
    
    * user access roles working
    
    * localizations added
    
    * Removed debuggera dn console
    
    * Localizations
    
    * Changes
    
    * response screen working
    
    * Delete package-lock.json

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/SetupMicroplan.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/SetupMicroplan.js
@@ -130,15 +130,21 @@ const SetupMicroplan = ({ hierarchyType, hierarchyData }) => {
   const handleUpdates = (propsForMutate) => {
     updateResources(propsForMutate, {
       onSuccess: (data) => {
+        // Check if there is a redirectTo property in the response
         if (data?.redirectTo) {
-          history.push(data?.redirectTo, data?.state)
-        } 
+          history.push(data?.redirectTo, data?.state); // Navigate to the specified route
+        }
       },
       onError: (error, variables) => {
-        setShowToast(({ key: "error", label: error?.message ? error.message : t("FAILED_TO_UPDATE_RESOURCE") }))
+        // Display error toast if update fails
+        setShowToast({
+          key: "error",
+          label: error?.message ? error.message : t("FAILED_TO_UPDATE_RESOURCE"),
+        });
       },
     });
   };
+  
 
   const onSubmit = (formData) => {
     // setIsSubmittting to true -> to run inline validations within the components
