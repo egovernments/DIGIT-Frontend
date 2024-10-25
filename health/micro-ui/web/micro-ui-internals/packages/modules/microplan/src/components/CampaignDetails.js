64b7e14e1970ec05e4dc3cb87e5f5f8890e42c9a
nabeelmd-eGov <nabeel.md@egovernments.org>
2024-10-25 11:24:48 +0530

    draft issue fix, table reload fix, table no data fix (#1664)
    * draft issue fix, table reload fix, table no data fix
    
    * key change to planConfiguration ID
    
    * change to plans
    
    ---------
    
    Co-authored-by: NabeelAyubee <nayubi7@gmail.com>

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/CampaignDetails.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/CampaignDetails.js
@@ -4,9 +4,10 @@ import { useTranslation } from "react-i18next";
 import { LabelFieldPair } from "@egovernments/digit-ui-react-components";
 import { Button, CardText, Dropdown, ErrorMessage, PopUp, MultiSelectDropdown } from "@egovernments/digit-ui-components";
 import { useMyContext } from "../utils/context";
-const CampaignDetails = ({onSelect,props:customProps,...props}) => {
-  const {campaignType:campaignTypeSession,disease:diseaseSession,distributionStrat:distributionStratSession} = customProps?.sessionData?.CAMPAIGN_DETAILS?.[customProps?.name] || {}
-  const { dispatch,state } = useMyContext();
+const CampaignDetails = ({ onSelect, props: customProps, ...props }) => {
+  const { campaignType: campaignTypeSession, disease: diseaseSession, distributionStrat: distributionStratSession } =
+    customProps?.sessionData?.CAMPAIGN_DETAILS?.[customProps?.name] || {};
+  const { dispatch, state } = useMyContext();
   const [executionCount, setExecutionCount] = useState(0);
   const { t } = useTranslation();
   const tenantId = Digit.ULBService.getStateId();
@@ -19,114 +20,124 @@ const CampaignDetails = ({onSelect,props:customProps,...props}) => {
         }
   );
   const [distributionStrat, setDistributionStrat] = useState(distributionStratSession);
+  useEffect(() => {
+    setCampaignType(campaignTypeSession);
+    setDisease(diseaseSession ? diseaseSession : { code: "MALARIA" });
+    setDistributionStrat(distributionStratSession);
+  }, [campaignTypeSession, diseaseSession, distributionStratSession]);
   const { campaignId, microplanId, key, ...queryParams } = Digit.Hooks.useQueryParams();
-  const [isFreezed,setIsFreezed] = useState(campaignId && microplanId ? true : false);
-  const { isLoading, data } = Digit.Hooks.useCustomMDMS(tenantId, "HCM-PROJECT-TYPES", [{ name: "projectTypes" }], {
-    select: (data) => {
-      let projectOptions = data?.["HCM-PROJECT-TYPES"]?.projectTypes;
-      projectOptions = Digit.Utils.microplanv1.filterUniqueByKey(projectOptions, "code").map((row) => {
+  const [isFreezed, setIsFreezed] = useState(campaignId && microplanId ? true : false);
+  const { isLoading, data } = Digit.Hooks.useCustomMDMS(
+    tenantId,
+    "HCM-PROJECT-TYPES",
+    [{ name: "projectTypes" }],
+    {
+      select: (data) => {
+        let projectOptions = data?.["HCM-PROJECT-TYPES"]?.projectTypes;
+        projectOptions = Digit.Utils.microplanv1.filterUniqueByKey(projectOptions, "code").map((row) => {
+          return {
+            ...row,
+            i18nKey: Digit.Utils.locale.getTransformedLocale(`CAMPAIGN_TYPE_${row.code}`),
+          };
+        });
         return {
-          ...row,
-          i18nKey: Digit.Utils.locale.getTransformedLocale(`CAMPAIGN_TYPE_${row.code}`),
+          campaignTypes: projectOptions,
+          diseases: [
+            {
+              code: "MALARIA",
+            },
+          ],
+          // distributionStrategies: data?.["HCM-PROJECT-TYPES"]?.projectTypes?.[0]?.distributionStrategy,
+          distributionStrategies: state?.ResourceDistributionStrategy,
         };
-      });
-      return {
-        campaignTypes: projectOptions,
-        diseases: [
-          {
-            code: "MALARIA",
-          },
-        ],
-        // distributionStrategies: data?.["HCM-PROJECT-TYPES"]?.projectTypes?.[0]?.distributionStrategy,
-        distributionStrategies:state?.ResourceDistributionStrategy
-      };
+      },
     },
-  },
-  {schemaCode:"ProjectType"}
-);
-  
+    { schemaCode: "ProjectType" }
+  );
 
   useEffect(() => {
-    onSelect(customProps.name,{
-      distributionStrat,disease,campaignType
-    })
-  }, [distributionStrat,disease,campaignType]);
+    onSelect(customProps.name, {
+      distributionStrat,
+      disease,
+      campaignType,
+    });
+  }, [distributionStrat, disease, campaignType]);
 
   useEffect(() => {
     if (executionCount < 5) {
       onSelect(customProps.name, {
-        distributionStrat,disease,campaignType
+        distributionStrat,
+        disease,
+        campaignType,
       });
       setExecutionCount((prevCount) => prevCount + 1);
     }
   });
 
-  if(isLoading){
-    return <Loader />
+  if (isLoading) {
+    return <Loader />;
   }
 
-
-
   return (
-    <Card >
-        <Header>{t(`HCM_CAMPAIGN_DETAILS_HEADER`)}</Header>
-        <p className="description-type">{t(`HCM_CAMPAIGN_DETAILS_DESC`)}</p>
-        <LabelFieldPair>
-          <div className="campaign-type" style={{ minWidth: "17rem" }}>
-            <span>{`${t("HCM_DISEASE_TYPE")}`}</span>
-            <span className="mandatory-span">*</span>
-          </div>
-          <Dropdown
-            style={{ width: "40rem", paddingBottom: "1rem" }}
-            // variant={error ? "error" : ""}
-            t={t}
-            option={data?.diseases}
-            optionKey={"code"}
-            selected={disease}
-            select={(value) => {
-              setDisease(value);
-            }}
-            disabled={isFreezed}
-          />
-        </LabelFieldPair>
-        <LabelFieldPair>
-          <div className="campaign-type" style={{ minWidth: "17rem" }}>
-            <span>{`${t("HCM_CAMPAIGN_TYPE_OF")}`}</span>
-            <span className="mandatory-span">*</span>
-          </div>
-          <Dropdown
-            style={{ width: "40rem", paddingBottom: "1rem" }}
-            // variant={error ? "error" : ""}
-            t={t}
-            option={data?.campaignTypes}
-            optionKey={"i18nKey"}
-            selected={campaignType}
-            select={(value) => {
-              setCampaignType(value);
-            }}
-            disabled={isFreezed}
-          />
-        </LabelFieldPair>
-        <LabelFieldPair>
-          <div className="campaign-type" style={{ minWidth: "17rem" }}>
-            <span>{`${t("HCM_CAMPAIGN_RESOURCE_DIST_STRAT")}`}</span>
-            <span className="mandatory-span">*</span>
-          </div>
-          <Dropdown
-            style={{ width: "40rem", paddingBottom: "1rem" }}
-            // variant={error ? "error" : ""}
-            t={t}
-            option={data?.distributionStrategies}
-            optionKey={"resourceDistributionStrategyCode"}
-            selected={distributionStrat}
-            select={(value) => {
-              setDistributionStrat(value);
-            }}
-            disabled={isFreezed}
-          />
-        </LabelFieldPair>
+    <Card>
+      <Header>{t(`HCM_CAMPAIGN_DETAILS_HEADER`)}</Header>
+      <p className="description-type">{t(`HCM_CAMPAIGN_DETAILS_DESC`)}</p>
+      <LabelFieldPair>
+        <div className="campaign-type" style={{ minWidth: "17rem" }}>
+          <span>{`${t("HCM_DISEASE_TYPE")}`}</span>
+          <span className="mandatory-span">*</span>
+        </div>
+        <Dropdown
+          style={{ width: "40rem", paddingBottom: "1rem" }}
+          // variant={error ? "error" : ""}
+          t={t}
+          option={data?.diseases}
+          optionKey={"code"}
+          selected={disease}
+          select={(value) => {
+            setDisease(value);
+          }}
+          disabled={isFreezed}
+        />
+      </LabelFieldPair>
+      <LabelFieldPair>
+        <div className="campaign-type" style={{ minWidth: "17rem" }}>
+          <span>{`${t("HCM_CAMPAIGN_TYPE_OF")}`}</span>
+          <span className="mandatory-span">*</span>
+        </div>
+        <Dropdown
+          style={{ width: "40rem", paddingBottom: "1rem" }}
+          // variant={error ? "error" : ""}
+          t={t}
+          option={data?.campaignTypes}
+          optionKey={"i18nKey"}
+          selected={campaignType}
+          select={(value) => {
+            setCampaignType(value);
+          }}
+          disabled={isFreezed}
+        />
+      </LabelFieldPair>
+      <LabelFieldPair>
+        <div className="campaign-type" style={{ minWidth: "17rem" }}>
+          <span>{`${t("HCM_CAMPAIGN_RESOURCE_DIST_STRAT")}`}</span>
+          <span className="mandatory-span">*</span>
+        </div>
+        <Dropdown
+          style={{ width: "40rem", paddingBottom: "1rem" }}
+          // variant={error ? "error" : ""}
+          t={t}
+          option={data?.distributionStrategies}
+          optionKey={"resourceDistributionStrategyCode"}
+          selected={distributionStrat}
+          select={(value) => {
+            setDistributionStrat(value);
+          }}
+          disabled={isFreezed}
+        />
+      </LabelFieldPair>
     </Card>
   );
-}
+};
 
-export default CampaignDetails
+export default CampaignDetails;
