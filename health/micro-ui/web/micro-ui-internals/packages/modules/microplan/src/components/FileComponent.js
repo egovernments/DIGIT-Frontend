c19d85337826f174f37d2bc05c0c16b6d2e061f3
ashish-egov <ashish.tiwari@egovernments.org>
2024-10-29 17:49:42 +0530

    Feat : changes in user download

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FileComponent.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FileComponent.js
@@ -1,108 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { EditIcon } from "@egovernments/digit-ui-react-components";
 import { DeleteIconv2, DownloadIcon, FileIcon, Card, CardSubHeader } from "@egovernments/digit-ui-react-components";
 import { Button, InfoButton } from "@egovernments/digit-ui-components";
+import { CustomSVG } from "@egovernments/digit-ui-components";
+
 
 const FileComponent = ({ title, fileName, status, auditDetails, editHandler, deleteHandler, downloadHandler }) => {
     const { t } = useTranslation();
-    const { lastmodTime } = auditDetails || {}; // Destructuring the audit details for easy access
+    const { XlsxFile } = CustomSVG;
+    const { lastmodTime } = auditDetails || {};
     return (
-        <div>
+        <Card
+            style={{ background: "#F3F3F3", border: "1px solid #D6D5D4" }}
+        >
+            <div className="dm-parent-container" style={{ background: "#F3F3F3", margin: "0" }}>
+                {/* Left side: File Icon */}
+                <div
+                    className="dm-uploaded-file-container-sub"
+                    style={{ marginLeft: "-1rem" }}
+                >
+                    {/* <FileIcon className="dm-icon" /> */}
+                    <XlsxFile styles={{ width: "6rem", height: "6rem" }} />
+                    <div style={{ marginLeft: "0.5rem", marginTop: "0.5rem" }}>{fileName}</div>
+                </div>
 
-            {/* First card */}
-            <div className="view-composer-header-section">
-                <CardSubHeader style={{ marginTop: 0, fontSize: "1.5rem", color: " #0B4B66", marginBottom: "0rem" }}>{title}</CardSubHeader>
-            </div>
+                {/* Right side: Edit, Delete, and Audit details */}
+                <div className="dm-actions-container">
 
-            <Card type={"secondary"} className="card-color">
-                <div className="dm-parent-container">
-                    {/* Left side: File Icon */}
-                    <div
-                        className="dm-uploaded-file-container-sub"
-                        onClick={() => setShowPreview(true)}
-                    >
-                        <FileIcon className="dm-icon" />
-                        <div>{fileName}</div>
-                    </div>
-
-                    {/* Right side: Edit, Delete, and Audit details */}
-                    <div className="dm-actions-container">
-
-                        {/* Display audit details (Uploaded by user and last modified time) */}
-                        {(lastmodTime) ? (
-                            <div className="dm-audit-info11">
-                                {/* Displaying the audit information */}
-                                {lastmodTime && <span style={{ color: "#C84C0E" }}>{lastmodTime}</span>}
-                            </div>) : null
-                        }
-                        {/* Edit Icon and Button */}
-                        <div className="dm-campaign-preview-edit-container" onClick={() => handleRedirect(1)}>
-                            {editHandler &&
-                                <Button
-                                    label={t("WBH_EDIT")}
-                                    variation="secondary"
-                                    icon={<EditIcon styles={{ height: "1.25rem", width: "2.5rem" }} />}
-                                    type="button"
-                                    className="dm-workbench-download-template-btn dm-hover"
-                                    onButtonClick={(e) => {
-                                        editHandler();
-                                    }}
-                                />
-                            }
-                        </div>
-
-                        {/* Delete Button */}
-                        {deleteHandler &&
+                    {/* Display audit details (Uploaded by user and last modified time) */}
+                    {(lastmodTime) ? (
+                        <div className="dm-audit-info11">
+                            {/* Displaying the audit information */}
+                            {lastmodTime && <span style={{ color: "#7a7674" }}>{lastmodTime}</span>}
+                        </div>) : null
+                    }
+                    {/* Edit Icon and Button */}
+                    <div className="dm-campaign-preview-edit-container" onClick={() => handleRedirect(1)}>
+                        {editHandler &&
                             <Button
-                                label={t("WBH_DELETE")}
+                                label={t("WBH_EDIT")}
                                 variation="secondary"
-                                icon={<DeleteIconv2 styles={{ height: "1.25rem", width: "2.5rem" }} />}
+                                icon={<EditIcon styles={{ height: "1.25rem", width: "2.5rem" }} />}
                                 type="button"
                                 className="dm-workbench-download-template-btn dm-hover"
                                 onButtonClick={(e) => {
-                                    deleteHandler();
+                                    editHandler();
                                 }}
                             />
                         }
-
-                        {/* Download Button */}
-                        {(downloadHandler && status === "completed") && (
-                            <Button
-                                label={t("WBH_DOWNLOAD")}
-                                variation="secondary"
-                                icon={"FileDownload"}
-                                type="button"
-                                className="dm-workbench-download-template-btn dm-hover"
-                                onClick={(e) => {
-                                    downloadHandler();
-                                }}
-                            />
-                        )}
-                        {(downloadHandler && status === "data-accepted") && (
-                            <Button
-                                label={t("WBH_INPROGRESS")}
-                                variation="secondary"
-                                icon={"Inprogress"}
-                                type="button"
-                                className="dm-workbench-download-template-btn"
-                            />
-                        )}
-
-                        {(downloadHandler && status !== "completed" && status !== "data-accepted") && (
-                            <InfoButton
-                                className="dm-workbench-download-template-btn"
-                                infobuttontype="error"
-                                label={t("WBH_FAILED")}
-                                isDisabled={true}
-                                style={{ background: "#b30505" ,opacity : 1}}
-                            />
-                        )}
                     </div>
-                </div>
-            </Card>
 
-        </div>
+                    {/* Delete Button */}
+                    {deleteHandler &&
+                        <Button
+                            label={t("WBH_DELETE")}
+                            variation="secondary"
+                            icon={<DeleteIconv2 styles={{ height: "1.25rem", width: "2.5rem" }} />}
+                            type="button"
+                            className="dm-workbench-download-template-btn dm-hover"
+                            onButtonClick={(e) => {
+                                deleteHandler();
+                            }}
+                            style={{ width: "11rem" }}
+                        />
+                    }
+
+                    {/* Download Button */}
+                    {(downloadHandler && status === "completed") && (
+                        <Button
+                            label={t("WBH_DOWNLOAD")}
+                            variation="secondary"
+                            icon={"FileDownload"}
+                            type="button"
+                            className="dm-workbench-download-template-btn dm-hover"
+                            onClick={(e) => {
+                                downloadHandler();
+                            }}
+                            style={{ width: "11rem" }}
+                        />
+                    )}
+                    {(downloadHandler && status === "data-accepted") && (
+                        <Button
+                            label={t("WBH_INPROGRESS")}
+                            variation="secondary"
+                            icon={"Info"}
+                            type="button"
+                            className="dm-workbench-download-template-btn"
+                            style={{ width: "11rem" }}
+                        />
+                    )}
+
+                    {(downloadHandler && status !== "completed" && status !== "data-accepted") && (
+                        <InfoButton
+                            className="dm-workbench-download-template-btn"
+                            infobuttontype="error"
+                            icon={"Info"}
+                            label={t("WBH_FAILED")}
+                            // isDisabled={true}
+                            style={{ background: "#b30505", opacity: 1 , width: "11rem" }}
+                        />
+                    )}
+                </div>
+            </div>
+        </Card>
     );
 };
 
--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/UserDownload.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/UserDownload.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import FileComponent from '../../components/FileComponent';
 import HeaderComp from '../../components/HeaderComp';
-import { TextBlock, Card } from '@egovernments/digit-ui-components';
-import {LoaderWithGap} from "@egovernments/digit-ui-react-components";
+import { TextBlock, Card, ActionBar, Button } from '@egovernments/digit-ui-components';
+import { LoaderWithGap } from "@egovernments/digit-ui-react-components";
+import { Link } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 
 
@@ -17,7 +18,7 @@ const UserDownload = () => {
     },
         {
             enabled: true,
-            select: data => { 
+            select: data => {
                 const currentUserUuid = Digit.UserService.getUser().info.uuid;
                 const ResourceDetails = data?.ResourceDetails || [];
                 const filteredData = ResourceDetails.filter(item => item?.auditDetails?.createdBy == currentUserUuid && item?.action == "create");
@@ -28,44 +29,31 @@ const UserDownload = () => {
 
     )
 
-
-
-
-
-
-    // Use useEffect to update Files only when data changes
     useEffect(() => {
         if (data && data["ResourceDetails"]) {
             // Create a new array of file IDs based on the data
             const newFiles = data["ResourceDetails"].map(ob => ob["processedFilestoreId"]);
             setFile(newFiles); // Update the state with the new file IDs
         }
-    }, [data]); // Only run this effect when `data` changes
-
-    
-
-
-
-
-
-
-
-
-
-
+    }, [data]); 
 
     return (
         <div>
             {isLoading && <LoaderWithGap text={t("CS_LOADING")} />}
-            <Card type="secondary" >
+            <Card type="secondary" style={{ margin: "1.5rem 0 0 0.5rem" }}>
                 <HeaderComp title="DOWNLOAD_USER_DATA" styles={{ color: "black" }} />
-                <TextBlock body={t("DOWNLOAD_DESC")} />
-                    {data?.ResourceDetails &&
-                        [...data?.ResourceDetails].reverse().map((item, index) => {
-                            let fileName = item?.additionalDetails?.fileName || `FileNo${item?.processedFilestoreId?.slice(0, 4) || ''}`;
-                            
+                <TextBlock body={
+                    <div style={{ color: "black" }}>
+                        <span>  {t("DOWNLOAD_DESC")}  </span>
+                    </div>
+                }
+                />
+                {data?.ResourceDetails &&
+                    [...data?.ResourceDetails].reverse().map((item, index) => {
+                        let fileName = item?.additionalDetails?.fileName || `FileNo${item?.processedFilestoreId?.slice(0, 4) || ''}`;
 
-                            return (
+
+                        return (
                             <FileComponent
                                 title=""
                                 fileName={fileName}
@@ -73,18 +61,28 @@ const UserDownload = () => {
                                     Digit.Utils.campaign.downloadExcelWithCustomName({
                                         fileStoreId: item?.processedFilestoreId,
                                         customName: String(fileName)
-
                                     });
                                 }} // Passing the download function
                                 status={item?.status}
-                                auditDetails={{userName:item?.username,lastmodTime:item?.auditDetails?.lastmodtime}}
+                                auditDetails={{ userName: item?.username, lastmodTime: item?.auditDetails?.lastmodtime }}
                             />
-                            )
-
-    })
-
-                    }
+                        )
+                    })
+                }
             </Card>
+            <ActionBar style={{ display: "flex", justifyContent: "space-between", alignItems: "center", width: "100%", zIndex: "1" }}>
+                <div style={{ marginLeft: "auto" }}>
+                    <Link to="/microplan-ui/employee/microplan/user-management" style={{ textDecoration: "none" }}>
+                        <Button
+                            style={{ minWidth: "12rem" }}
+                            className="previous-button"
+                            variation="secondary"
+                            label={t("BACK")}
+                            icon={"ArrowBack"}
+                        />
+                    </Link>
+                </div>
+            </ActionBar>
         </div>
     );
 }
