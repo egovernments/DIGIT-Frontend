cc32b1a0147adbed8db83afa8be3b282977e022a
Abishek <abishek.t@egovernments.org>
2024-10-29 17:17:39 +0530

    console removed

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/configs/UICustomizations.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/configs/UICustomizations.js
@@ -452,7 +452,6 @@ export const UICustomizations = {
         config: {
           enabled: true,
           select: (data) => {
-            console.log("dataaa",data)
             const roles = data?.mdms.map((item) => {
               return {
                 roleCode: item.data.roleCode,
