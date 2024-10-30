4eaa431ee735cad1118876eac9051bc87f793119
nabeelmd-eGov <nabeel.md@egovernments.org>
2024-10-30 16:56:43 +0530

    Update Response.js

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/Response.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/Response.js
@@ -5,57 +5,56 @@ import { ActionBar, SubmitBar, ArrowLeft, ArrowForward } from "@egovernments/dig
 import { Button } from "@egovernments/digit-ui-components";
 import { PanelCard } from "@egovernments/digit-ui-components";
 const Response = () => {
-    const { t } = useTranslation();
-    const history = useHistory();
-    const queryStrings = Digit.Hooks.useQueryParams();
-    const [campaignId, setCampaignId] = useState(queryStrings?.campaignId);
-    const [isResponseSuccess, setIsResponseSuccess] = useState(
-        queryStrings?.isSuccess === "true" ? true : queryStrings?.isSuccess === "false" ? false : true
-    );
-    const { state } = useLocation();
-    const back=(state?.back)?state?.back:"BACK";
-    const backlink=(state?.backlink)?(state.backlink):"employee";
-    const description=(state?.description)?state.description:"";
-    const message=(state?.message)?(state?.message):"";
-    const fileName=(state?.fileName)?(state.fileName):"";
-    return (
-        <>
-            <PanelCard
-                animationProps={{
-                    loop: false,
-                    noAutoplay: false
-                }}
-                cardClassName=""
-                cardStyles={{}}
-                className=""
-                customIcon=""
-                description={t(description)}
-                footerStyles={{}}
-                iconFill=""
-                info=""
-                maxFooterButtonsAllowed={5}
-                message={t(message)}
-                multipleResponses={[]}
-                props={{}}
-                response={t(state?.fileName)}
-                sortFooterButtons
-                style={{}}
-                type="success"
-            >
+  const { t } = useTranslation();
+  const history = useHistory();
+  const queryStrings = Digit.Hooks.useQueryParams();
+  const [campaignId, setCampaignId] = useState(queryStrings?.campaignId);
+  const [isResponseSuccess, setIsResponseSuccess] = useState(
+    queryStrings?.isSuccess === "true" ? true : queryStrings?.isSuccess === "false" ? false : true
+  );
+  const { state } = useLocation();
+  const back = state?.back ? state?.back : "BACK";
 
-            </PanelCard>
-            <ActionBar className="mc_back">
-                    <Button
-                        style={{ margin: "0.5rem", minWidth: "10rem", marginLeft: "6rem" }}
-                        className="ab-actions"
-                        variation="secondary"
-                        label={t(back)}
-                        icon={"ArrowBack"}
-                        onClick={()=>{history.push(`/${window.context}/microplan-ui/employee`)}}
-                    />
-            </ActionBar>
-        </>
-    );
+  return (
+    <>
+      <PanelCard
+        animationProps={{
+          loop: false,
+          noAutoplay: false,
+        }}
+        cardClassName=""
+        cardStyles={{}}
+        className=""
+        customIcon=""
+        description={t(state?.description)}
+        // footerChildren={[
+        //     <Button label="OK" onClick={function noRefCheck() { }} type="button" />
+        // ]}
+        footerStyles={{}}
+        iconFill=""
+        info={t(state?.info)}
+        maxFooterButtonsAllowed={5}
+        message={t(state?.message)}
+        multipleResponses={[]}
+        props={{}}
+        response={t(state?.fileName ? state?.fileName : state?.responseId ? state?.responseId : "")}
+        sortFooterButtons
+        style={{}}
+        type="success"
+      ></PanelCard>
+      <ActionBar className="mc_back">
+        <Link to={state?.backlink ? state?.backlink : `/${window.contextPath}/employee/`}>
+          <Button
+            style={{ margin: "0.5rem", marginLeft: "6rem" }}
+            className="microplan-response-button"
+            variation="secondary"
+            label={t(back)}
+            icon={"ArrowBack"}
+          />
+        </Link>
+      </ActionBar>
+    </>
+  );
 };
 
 export default Response;
