8b912e84ad7d95afb7c193984f3f962f01b7ae46
Swathi-eGov <swathi.chatrathi@egovernments.org>
2024-11-06 15:18:40 +0530

    pop-inbox and plan-inbox table audit fixes

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/PlanInbox.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/pages/employee/PlanInbox.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useState, useEffect, useMemo } from "react";
 import SearchJurisdiction from "../../components/SearchJurisdiction";
 import { useHistory } from "react-router-dom";
-import { Card, Tab, Button, SVG, Loader, ActionBar, Toast } from "@egovernments/digit-ui-components";
+import { Card, Tab, Button, SVG, Loader, ActionBar, Toast , ButtonsGroup} from "@egovernments/digit-ui-components";
 import { useTranslation } from "react-i18next";
 import InboxFilterWrapper from "../../components/InboxFilterWrapper";
 import DataTable from "react-data-table-component";
 import { CheckBox } from "@egovernments/digit-ui-components";
 import WorkflowCommentPopUp from "../../components/WorkflowCommentPopUp";
 import { tableCustomStyle } from "../../components/tableCustomStyle";
+import { CustomSVG } from "@egovernments/digit-ui-components";
 
 const PlanInbox = () => {
   const { t } = useTranslation();
@@ -450,6 +451,13 @@ const PlanInbox = () => {
     },
   ];
 
+  const actionIconMap = {
+    "VALIDATE": { isSuffix: false, icon: "CheckCircle" },
+    "EDIT_AND_SEND_FOR_APPROVAL": { isSuffix: false, icon: "Edit" },
+    "APPROVE": { isSuffix: false, icon: "CheckCircle" },
+    "SEND_BACK_FOR_CORRECTION": { isSuffix: true, icon: "ArrowForward" },
+  }
+
   if (isPlanEmpSearchLoading || isLoadingCampaignObject || isWorkflowLoading) {
     return <Loader />;
   }
@@ -483,7 +491,7 @@ const PlanInbox = () => {
             <Tab
               activeLink={activeLink?.code}
               configItemKey="code"
-              itemStyle={{ width: "unset !important" }}
+              itemStyle={{ width: "290px" }}
               configNavItems={[
                 {
                   code: "ASSIGNED_TO_ME",
@@ -510,20 +518,41 @@ const PlanInbox = () => {
                   <SVG.DoneAll width={"1.5rem"} height={"1.5rem"} fill={"#C84C0E"}></SVG.DoneAll>
                   <div className={"selected-state"}>{`${villagesSlected} ${t("MICROPLAN_VILLAGES_SELECTED")}`}</div>
                 </div>
-
                 <div className={`table-actions-wrapper`}>
-                  {actionsMain
-                    ?.filter((action) => !actionsToHide.includes(action.action))
-                    ?.map((action, index) => (
-                      <Button
-                        key={index}
-                        variation="secondary"
-                        label={t(action.action)}
-                        type="button"
-                        onClick={(curr) => handleActionClick(action?.action)}
-                        size={"large"}
-                      />
-                    ))}
+                  {actionsMain?.filter((action) => !actionsToHide.includes(action.action)).length > 1 ? (
+                    <ButtonsGroup
+                      buttonsArray={actionsMain
+                        ?.filter((action) => !actionsToHide.includes(action.action))
+                        ?.map((action, index) => (
+                          <Button
+                            key={index}
+                            variation="secondary"
+                            label={t(action.action)}
+                            type="button"
+                            onClick={(curr) => handleActionClick(action?.action)}
+                            size="large"
+                            icon={actionIconMap[action.action]?.icon}
+                            isSuffix={actionIconMap[action.action]?.isSuffix}
+                          />
+                        ))
+                      }
+                    />
+                  ) : (
+                    actionsMain
+                      ?.filter((action) => !actionsToHide.includes(action.action))
+                      ?.map((action, index) => (
+                        <Button
+                          key={index}
+                          variation="secondary"
+                          label={t(action.action)}
+                          type="button"
+                          onClick={(curr) => handleActionClick(action?.action)}
+                          size="large"
+                          icon={actionIconMap[action.action]?.icon}
+                          isSuffix={actionIconMap[action.action]?.isSuffix}
+                        />
+                      ))
+                  )}
                 </div>
 
                 {workFlowPopUp !== "" && (
@@ -566,6 +595,7 @@ const PlanInbox = () => {
               conditionalRowStyles={conditionalRowStyles}
               paginationPerPage={rowsPerPage}
               paginationRowsPerPageOptions={[10, 20, 50, 100]}
+              sortIcon={<CustomSVG.SortUp width={"16px"} height={"16px"} fill={"#0b4b66"} />}
             />
           </Card>
         </div>
