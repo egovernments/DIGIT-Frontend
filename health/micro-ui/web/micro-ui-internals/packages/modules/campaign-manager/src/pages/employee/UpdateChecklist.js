a3f3a5df48016031efa05eb3a7d035b1c79f1a51
suryansh-egov <suryansh.singh@egovernments.org>
2024-11-06 14:59:25 +0530

    z-index and camapaign-name in preview (#1753)

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/UpdateChecklist.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/UpdateChecklist.js
@@ -483,7 +483,7 @@ const UpdateChecklist = () => {
                             sortFooterChildren={true}
                         >
 
-                            <MobileChecklist questions={previewData} checklistRole={t(`${roleLocal}`)} typeOfChecklist={t(`${checklistTypeLocal}`)}></MobileChecklist>
+                            <MobileChecklist questions={previewData} campaignName={campaignName} checklistRole={t(`${roleLocal}`)} typeOfChecklist={t(`${checklistTypeLocal}`)}></MobileChecklist>
                         </PopUp>
                     )}
                     <Card type={"primary"} variant={"viewcard"} className={"example-view-card"}>
