006e8ae70da8d105f6ab72d01fb38868b528693f
ashish-egov <ashish.tiwari@egovernments.org>
2024-11-07 13:08:50 +0530

    Checking UisCustomisation

--- health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
+++ health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
@@ -7,7 +7,7 @@ import { Button as ButtonNew , Dropdown} from "@egovernments/digit-ui-components
 //create functions here based on module name set in mdms(eg->SearchProjectConfig)
 //how to call these -> Digit?.Customizations?.[masterName]?.[moduleName]
 // these functions will act as middlewares
-var Digit = window.Digit || {};
+// var Digit = window.Digit || {};
 
 const businessServiceMap = {
   "muster roll": "MR",
