4502bb6f41b570ffbc62502b0cf1ac74a64e8d16
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-11-11 13:01:35 +0530

    Update health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/hooks/useBoundaryHome.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/hooks/useBoundaryHome.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/hooks/useBoundaryHome.js
@@ -93,9 +93,7 @@ const useBoundaryHome = ({ screenType = "campaign", defaultHierarchyType = "", h
 
       const employeeDetails = (boundaryConfig?.department?.length > 0 && (await fetchEmployeeDetails(userName, tenantId))) || null;
       const hierarchyName=hierarchyType || boundaryConfig?.hierarchy;
-      // hierarchyName="test008";
       const defaultHierarchyName=defaultHierarchyType || final?.["default"]?.hierarchy;
-      // const defaultHierarchyName="DEFAULT-BOUNDARY-DEMO1";
       const boundaryData = await fetchBoundaryHierarchy(hierarchyName, tenantId);
       const defaultBoundaryData = await fetchBoundaryHierarchy(defaultHierarchyName, tenantId);
       boundaryData && generateFile(hierarchyType || boundaryConfig?.hierarchy, tenantId);
