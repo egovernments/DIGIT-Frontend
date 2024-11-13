b7491987039e73757aeda50e49126cd9c42ba051
rachana-egov <rachana.singh@egovernment.org>
2024-11-13 18:31:25 +0530

    updated assignee to assigner in timeline wrapper

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/timelinePopUpWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/timelinePopUpWrapper.js
@@ -36,8 +36,8 @@ const TimelinePopUpWrapper = ({ onClose, businessId, heading }) => {
                 label: t(instance?.action),
                 variant: 'completed',
                 subElements: [Digit.Utils.microplanv1.epochToDateTime(instance?.auditDetails?.lastModifiedTime),
-                instance?.assignes?.length > 0 &&
-                `${instance.assignes[0]?.name} - ${instance.assignes[0]?.roles?.[0]?.name || 'NA'}`,
+                instance?.assigner &&
+                `${instance.assigner?.name} - ${instance.assigner?.roles?.[0]?.name || 'NA'}`,
                 instance.comment && t(`${instance.comment}`)
                 ],
                 showConnector: true
