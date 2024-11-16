4276a66268408c7b7b4df3808ba04de25bc36aff
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2024-11-16 14:53:54 +0530

    updated the dss full module card component

--- micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/EmployeeModuleCard.js
+++ micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/EmployeeModuleCard.js
@@ -1,8 +1,10 @@
 
 
-import React from "react";
-import { useHistory } from "react-router-dom";
 import { Button, LandingPageCard,  } from "@egovernments/digit-ui-components";
+import React, { Fragment } from "react";
+import { ArrowRightInbox } from "./svgindex";
+import { useHistory, useLocation, Link } from "react-router-dom";
+
 
 /**
  * EmployeeModuleCard - A New reusable card component to display module information with KPIs, links, and actions.
@@ -84,4 +86,39 @@ const EmployeeModuleCard = ({
   return <LandingPageCard className={className} buttonSize={buttonSize} {...propsForModuleCard} />;
 };
 
-export default EmployeeModuleCard;
+
+
+const ModuleCardFullWidth = ({ moduleName,  links = [], isCitizen = false, className, styles, headerStyle, subHeader, subHeaderLink }) => {
+  return (
+    <div className={className ? className : "employeeCard card-home customEmployeeCard home-action-cards"} style={styles ? styles : {}}>
+      <div className="complaint-links-container" style={{ padding: "10px" }}>
+        <div className="header" style={isCitizen ? { padding: "0px" } : headerStyle}>
+          <span className="text removeHeight">{moduleName}</span>
+          <span className="link">
+            <a href={subHeaderLink}>
+              <span className={"inbox-total"} style={{ display: "flex", alignItems: "center", color: "#F47738", fontWeight: "bold" }} onClick={()=>history.push(`${link}`)}>
+                {subHeader || "-"}
+                <span style={{ marginLeft: "10px" }}>
+                  {" "}
+                  <ArrowRightInbox />
+                </span>
+              </span>
+            </a>
+          </span>
+        </div>
+        <div className="body" style={{ margin: "0px", padding: "0px" }}>
+          <div className="links-wrapper" style={{ width: "100%", display: "flex", flexWrap: "wrap" }}>
+            {links.map(({ count, label, link }, index) => (
+              <span className="link full-employee-card-link" key={index}>
+                {link ? (link?.includes(`${window?.contextPath}/`)?<Link to={link}>{label}</Link>:<a href={link}>{label}</a>) : null}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export { EmployeeModuleCard, ModuleCardFullWidth };
