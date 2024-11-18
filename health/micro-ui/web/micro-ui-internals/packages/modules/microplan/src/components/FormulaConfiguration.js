ade3ee023f8e97d3246ba2b5323e957f50fd4515
Abishek <abishek.t@egovernments.org>
2024-11-18 17:49:49 +0530

    resolved conflict

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FormulaConfiguration.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/FormulaConfiguration.js
@@ -271,12 +271,8 @@ const FormulaConfiguration = ({ onSelect, category, customProps, formulas: initi
   return (
     <>
       <Card className="middle-child">
-<<<<<<< HEAD
         <Header className="uploader-sub-heading">{t(category)}</Header>
         {(category==="FORMULA_CAMPAIGN_VEHICLES")?  <p className="mp-description">{t(`FORMULA_VEHICLE_DESCRIPTION`)}</p>:
-=======
-        <Header className="uploader-sub-heading">{t(`SUB_HEADING_${category}`)}</Header>
->>>>>>> console
         <p className="mp-description">{t(`FORMULA_CONFIGURATION_DESCRIPTION`)}</p>
         }
       </Card>
