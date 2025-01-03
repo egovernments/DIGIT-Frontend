8035876f99fb2d42cd30ce235e65e1ca007970ca
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2025-01-03 14:20:29 +0530

    code comment added why not used inboxsearch composer

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/components/custom_inbox_composer.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/components/custom_inbox_composer.js
@@ -9,6 +9,35 @@ import CustomFilter from "./custom_comp/filter_section";
 import CustomInboxTable from "./custom_comp/table_inbox";
 import { FilterCard, Toast, Card } from "@egovernments/digit-ui-components";
 
+/**
+* Business Flow Description:
+* 1. In the search section, a project select dropdown is provided.
+*    - When a project is selected from the dropdown, the Boundary Search Service is invoked to fetch the boundary hierarchy.
+* 2. On successful fetching of the boundary hierarchy from the service:
+*    - A dynamic list of boundary selection dropdowns is rendered in the filter section.
+* 3. When filters are applied:
+*    - The Attendance Register Search API is called with the applied filter criteria.
+*    - On receiving a successful response, the table data is rendered accordingly.
+* 4. Tab Functionality:
+*    - Tabs are implemented in the UI for additional functionality.
+*    - Based on the tab selection, the Attendance Register Search API is triggered with a custom payload specific to the selected tab.
+
+*/
+
+/**
+ * Reason for not using React Component - InboxComposer:
+ * 1. Restrictions in InboxComposer:
+ *    - The component requires showing "No Results" initially, which does not align with our requirement. 
+ *    - Search should only be triggered after filters are applied.
+ * 2. Dynamic Boundary Filters:
+ *    - The boundary filter options need to be dynamically determined based on the selected project.
+ * 3. Tab-Specific API Calls:
+ *    - On tab selection, the same Attendance Register Search API must be called with different status filters, 
+ *      which is not inherently supported by the InboxComposer component.
+
+ 
+ */
+
 const CustomInboxSearchComposer = () => {
   const { t } = useTranslation();
   const [showToast, setShowToast] = useState(null);
