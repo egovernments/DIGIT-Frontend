821bcb612a5949a6aec56242c8d97ebe28919c6a
rachana-egov <rachana.singh@egovernment.org>
2025-01-03 13:27:53 +0530

    removed logs

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/pages/employee/attendance_inbox.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/pages/employee/attendance_inbox.js
@@ -2,18 +2,12 @@ import React, { useMemo, useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Header, InboxSearchComposer, Loader } from "@egovernments/digit-ui-react-components";
 import { useLocation } from "react-router-dom";
-
-import { useMyContext } from "../../utils/context";
-import { AttendanceInboxConfig } from "../../configs/attendance_inbox_config";
 import CustomInboxSearchComposer from "../../components/custom_inbox_composer";
 
 const AttendanceInbox = () => {
- 
+
   const { t } = useTranslation();
   const location = useLocation();
-  // const moduleName = Digit?.Utils?.getConfigModuleName() || "commonSanitationUiConfig"
-  const tenant = Digit.ULBService.getStateId();
-  const configs = AttendanceInboxConfig();
 
   return (
     <React.Fragment>
@@ -22,8 +16,8 @@ const AttendanceInbox = () => {
         {location?.state?.count ? <span className="inbox-count">{location?.state?.count}</span> : null}
       </Header>
       <div className="inbox-search-wrapper">
-       {/* <InboxSearchComposer configs={configs}></InboxSearchComposer>*/}
-       <CustomInboxSearchComposer></CustomInboxSearchComposer>
+        {/* <InboxSearchComposer configs={configs}></InboxSearchComposer>*/}
+        <CustomInboxSearchComposer></CustomInboxSearchComposer>
       </div>
     </React.Fragment>
   );
--- health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/pages/employee/my_bills.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/pages/employee/my_bills.js
@@ -2,10 +2,6 @@ import React, { useMemo, useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Header, InboxSearchComposer, Loader } from "@egovernments/digit-ui-react-components";
 import { useLocation } from "react-router-dom";
-
-import { useMyContext } from "../../utils/context";
-import { AttendanceInboxConfig } from "../../configs/attendance_inbox_config";
-import CustomInboxSearchComposer from "../../components/custom_inbox_composer";
 import { Card } from "@egovernments/digit-ui-components";
 import MyBillsSearch from "../../components/MyBillsSearch";
 import MyBillsTable from "../../components/MyBillsTable";
@@ -14,9 +10,6 @@ const MyBills = () => {
 
     const { t } = useTranslation();
     const location = useLocation();
-    // const moduleName = Digit?.Utils?.getConfigModuleName() || "commonSanitationUiConfig"
-    const tenant = Digit.ULBService.getStateId();
-    const configs = AttendanceInboxConfig();
 
     return (
         <React.Fragment>
