056a3cc12dbb6053c5578e3565f2af36acc10615
Nipun Arora <aroranipun1@gmail.com>
2025-01-21 12:47:55 +0530

    stablize develop branch
    Users can run only the modules they want

--- micro-ui/web/micro-ui-internals/example/src/pgr.js
+++ micro-ui/web/micro-ui-internals/example/src/pgr.js
@@ -1,19 +1,8 @@
-import DocumentCard from "./components/DocumentCard";
-import EngagementCard from "./components/EngagementCard";
-import SelectName from "./components/SelectName";
-
-// import { config as complaintConfig } from "./complaintConfig";
-
 const pgrCustomizations = {
-  // complaintConfig,
-  getComplaintDetailsTableRows: ({ id, service, role, t }) => {
-    return {};
-  },
+ 
 };
 
 const overrideComponents = {
-  SelectName: SelectName,
-  DocumentCard,
-  EngagementCard
+
 };
 export { pgrCustomizations, overrideComponents };
