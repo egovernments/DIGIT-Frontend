2a15353714794c4e864f3d76c36779ce9282147a
Nipun Arora <aroranipun1@gmail.com>
2025-02-17 14:56:04 +0530

    updated individual create with Hrms create

--- micro-ui/web/micro-ui-internals/packages/modules/sample/src/pages/employee/IndividualCreate.js
+++ micro-ui/web/micro-ui-internals/packages/modules/sample/src/pages/employee/IndividualCreate.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React,{useState} from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-import { FormComposerV2, Header } from "@egovernments/digit-ui-react-components";
+import { FormComposerV2, Header,Toast } from "@egovernments/digit-ui-react-components";
 import { newConfig } from "../../configs/IndividualCreateConfig";
 import { transformCreateData } from "../../utils/createUtils";
 
@@ -9,14 +9,15 @@ import { transformCreateData } from "../../utils/createUtils";
 
 const IndividualCreate = () => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
+  const [showToast, setShowToast] = useState(null);
   const { t } = useTranslation();
   const history = useHistory();
   const reqCreate = {
-    url: `/individual/v1/_create`,
+    url: `/egov-hrms/employees/_create`,
     params: {},
     body: {},
     config: {
-      enable: false,
+      enable: true,
     },
   };
 
@@ -26,13 +27,20 @@ const IndividualCreate = () => {
     console.log(data, "data");
     await mutation.mutate(
       {
-        url: `/individual/v1/_create`,
+        url: `/egov-hrms/employees/_create`,
         params: { tenantId },
         body: transformCreateData(data),
         config: {
           enable: true,
         },
-      },
+      },{
+        onSuccess:(data)=>{
+          setShowToast({ key: "success", label: "Individual Created Successfully" });
+        },
+        onError:(error) => {
+          setShowToast({ key: "error", label: "Individual Creation Failed"});
+        }
+      }
     );
   };
   return (
@@ -52,7 +60,14 @@ const IndividualCreate = () => {
         onSubmit={(data,) => onSubmit(data, )}
         fieldStyle={{ marginRight: 0 }}
       />
-       
+       {showToast && (
+                <Toast style={{ zIndex: 10001 }}
+                    label={showToast.label}
+                    type={showToast.key}
+                    error={showToast.key === "error"}
+                    onClose={() => setShowToast(null)}
+                />
+            )}
     </div>
   );
 }
