00be31506794e20aab9ae069384b2d40b9743396
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-02-25 17:28:17 +0530

    Update JSONViewer.js

--- micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/JSONViewer.js
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/JSONViewer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { JsonEditor } from "json-edit-react";
 import _ from "lodash";
