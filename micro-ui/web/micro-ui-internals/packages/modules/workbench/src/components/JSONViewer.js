bbca6a6f16cd4cc4aaaecf71b1a055757e279497
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-02-25 17:25:04 +0530

    Update micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/JSONViewer.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/JSONViewer.js
+++ micro-ui/web/micro-ui-internals/packages/modules/workbench/src/components/JSONViewer.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect, Fragment } from "react";
 import { useTranslation } from "react-i18next";
 import { JsonEditor } from "json-edit-react";
 import _ from "lodash";
+import PropTypes from "prop-types";
+
+JSONViewer.propTypes = {
+  formData: PropTypes.object.isRequired,
+  screenType: PropTypes.oneOf(["view", "add", "edit"]).isRequired,
+  onDataUpdate: PropTypes.func
+};
 
 const JSONViewer = ({ formData, screenType, onDataUpdate }) => {
   const { t } = useTranslation();
