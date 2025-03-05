11b44921a020539dac40c1ac88533f81c7ab1716
NabeelAyubee <nayubi7@gmail.com>
2025-03-05 10:55:28 +0530

    adding fix

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationScreenParent/AppFieldScreenWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationScreenParent/AppFieldScreenWrapper.js
@@ -22,7 +22,7 @@ function AppFieldScreenWrapper({}) {
     type: FIELD_TYPE,
     item: () => {
       setIsDraggingField(true);
-      return field
+      return field;
     },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
@@ -62,121 +62,119 @@ function AppFieldScreenWrapper({}) {
   );
 
   return (
-    // <DndProvider backend={HTML5Backend}>
-      <React.Fragment>
-        {currentCard?.cards?.map(({ fields, description, header, headerFields }, index, card) => {
-          return (
-            <Card className="appConfigScreenCard">
-              {headerFields?.map(({ type, label, active, required, value }, indx, cx) => (
-                <AppFieldComposer
-                  type={type}
-                  label={label}
-                  active={active}
-                  required={required}
-                  value={useCustomT(`${projectType}_${currentCard.parent}_${currentCard.name}_${label}`)}
-                  headerFields={true}
-                  onChange={(event) => {
-                    dispatch({
-                      type: "UPDATE_HEADER_FIELD",
-                      payload: {
-                        currentField: card[index],
-                        currentScreen: currentCard,
-                        field: cx[indx],
-                        localisedCode: `${projectType}_${currentCard.parent}_${currentCard.name}_${label}`,
-                        value: event.target.value,
-                      },
-                    });
-                  }}
-                />
-              ))}
-              <Divider />
-              {fields?.map(
-                ({ type, label, active, required, Mandatory, helpText, infoText, innerLabel, dropDownOptions, deleteFlag, ...rest }, i, c) => (
-                  <div ref={ref} style={{ opacity: isDragging ? 0.5 : 1, display: "flex", alignItems: "center" }}>
-                    <span ref={dragRef} style={{ cursor: "grab", marginRight: "8px" }}>
-                      ☰
-                    </span>
-                    <AppFieldComposer
-                      type={type}
-                      label={useCustomT(label)}
-                      active={active}
-                      required={required}
-                      isDelete={deleteFlag === false ? false : true}
-                      dropDownOptions={dropDownOptions}
-                      onDelete={() => {
-                        dispatch({
-                          type: "DELETE_FIELD",
-                          payload: {
-                            currentScreen: currentCard,
-                            currentCard: card[index],
-                            currentField: c[i],
-                          },
-                        });
-                        // return;
-                      }}
-                      onSelectField={() => {
-                        dispatch({
-                          type: "SELECT_DRAWER_FIELD",
-                          payload: {
-                            currentScreen: currentCard,
-                            currentCard: card[index],
-                            drawerField: c[i],
-                          },
-                        });
-                        // return;
-                      }}
-                      config={c[i]}
-                      Mandatory={Mandatory}
-                      helpText={useCustomT(helpText)}
-                      infoText={useCustomT(infoText)}
-                      innerLabel={useCustomT(innerLabel)}
-                      rest={rest}
-                    />
-                  </div>
-                )
-              )}
-              {currentCard?.config?.enableFieldAddition && (
-                <Button
-                  className={"campaign-type-alert-button"}
-                  type={"button"}
-                  size={"large"}
-                  variation={"primary"}
-                  label={t("ADD_FIELD")}
-                  onClick={() => {
-                    dispatch({
-                      type: "ADD_FIELD",
-                      payload: {
-                        currentScreen: currentCard,
-                        currentCard: card[index],
-                      },
-                    });
-                    return;
-                  }}
-                />
-              )}
-            </Card>
-          );
-        })}
-        {currentCard?.config?.enableSectionAddition && (
-          <Button
-            className={"app-config-add-section"}
-            type={"button"}
-            size={"large"}
-            variation={"primary"}
-            label={t("ADD_SECTION")}
-            onClick={() => {
-              dispatch({
-                type: "ADD_SECTION",
-                payload: {
-                  currentScreen: currentCard,
-                },
-              });
-              return;
-            }}
-          />
-        )}
-      </React.Fragment>
-    {/* </DndProvider> */}
+    <React.Fragment>
+      {currentCard?.cards?.map(({ fields, description, header, headerFields }, index, card) => {
+        return (
+          <Card className="appConfigScreenCard">
+            {headerFields?.map(({ type, label, active, required, value }, indx, cx) => (
+              <AppFieldComposer
+                type={type}
+                label={label}
+                active={active}
+                required={required}
+                value={useCustomT(`${projectType}_${currentCard.parent}_${currentCard.name}_${label}`)}
+                headerFields={true}
+                onChange={(event) => {
+                  dispatch({
+                    type: "UPDATE_HEADER_FIELD",
+                    payload: {
+                      currentField: card[index],
+                      currentScreen: currentCard,
+                      field: cx[indx],
+                      localisedCode: `${projectType}_${currentCard.parent}_${currentCard.name}_${label}`,
+                      value: event.target.value,
+                    },
+                  });
+                }}
+              />
+            ))}
+            <Divider />
+            {fields?.map(
+              ({ type, label, active, required, Mandatory, helpText, infoText, innerLabel, dropDownOptions, deleteFlag, ...rest }, i, c) => (
+                <div ref={ref} style={{ opacity: isDragging ? 0.5 : 1, display: "flex", alignItems: "center" }}>
+                  <span ref={dragRef} style={{ cursor: "grab", marginRight: "8px" }}>
+                    ☰
+                  </span>
+                  <AppFieldComposer
+                    type={type}
+                    label={useCustomT(label)}
+                    active={active}
+                    required={required}
+                    isDelete={deleteFlag === false ? false : true}
+                    dropDownOptions={dropDownOptions}
+                    onDelete={() => {
+                      dispatch({
+                        type: "DELETE_FIELD",
+                        payload: {
+                          currentScreen: currentCard,
+                          currentCard: card[index],
+                          currentField: c[i],
+                        },
+                      });
+                      // return;
+                    }}
+                    onSelectField={() => {
+                      dispatch({
+                        type: "SELECT_DRAWER_FIELD",
+                        payload: {
+                          currentScreen: currentCard,
+                          currentCard: card[index],
+                          drawerField: c[i],
+                        },
+                      });
+                      // return;
+                    }}
+                    config={c[i]}
+                    Mandatory={Mandatory}
+                    helpText={useCustomT(helpText)}
+                    infoText={useCustomT(infoText)}
+                    innerLabel={useCustomT(innerLabel)}
+                    rest={rest}
+                  />
+                </div>
+              )
+            )}
+            {currentCard?.config?.enableFieldAddition && (
+              <Button
+                className={"campaign-type-alert-button"}
+                type={"button"}
+                size={"large"}
+                variation={"primary"}
+                label={t("ADD_FIELD")}
+                onClick={() => {
+                  dispatch({
+                    type: "ADD_FIELD",
+                    payload: {
+                      currentScreen: currentCard,
+                      currentCard: card[index],
+                    },
+                  });
+                  return;
+                }}
+              />
+            )}
+          </Card>
+        );
+      })}
+      {currentCard?.config?.enableSectionAddition && (
+        <Button
+          className={"app-config-add-section"}
+          type={"button"}
+          size={"large"}
+          variation={"primary"}
+          label={t("ADD_SECTION")}
+          onClick={() => {
+            dispatch({
+              type: "ADD_SECTION",
+              payload: {
+                currentScreen: currentCard,
+              },
+            });
+            return;
+          }}
+        />
+      )}
+    </React.Fragment>
   );
 }
 
