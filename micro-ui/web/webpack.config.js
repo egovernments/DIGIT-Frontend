37abbddff0de4ea6c6519280b61cff86ee5bfaef
Piyush Raj <piyushraj473@gmail.com>
2025-04-10 14:25:05 +0530

    trigger wf

--- micro-ui/web/webpack.config.js
+++ micro-ui/web/webpack.config.js
@@ -213,7 +213,7 @@ module.exports = {
 
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     publicPath: "/digit-ui/", // Use root for dev
   },
 
