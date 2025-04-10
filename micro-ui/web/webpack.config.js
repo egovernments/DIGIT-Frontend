4dffc2175c3410fc1d832c329d8c5a897c2d61d1
Piyush Raj <piyushraj473@gmail.com>
2025-04-10 14:46:57 +0530

    trigger wf

--- micro-ui/web/webpack.config.js
+++ micro-ui/web/webpack.config.js
@@ -205,18 +205,12 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
-  devtool: "eval-source-map", // Faster rebuilds, good for dev
-
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/digit-ui/", // Use root for dev
-  },
-
+  mode: "production",
+  entry: path.resolve(__dirname, 'src/index.js'),
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -225,36 +219,42 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-optional-chaining"],
-          },
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }]
+            ],
+            plugins: [
+              "@babel/plugin-proposal-optional-chaining"
+            ]
+          }
         },
       },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      },
+      }
     ],
   },
-
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/digit-ui/",
+  },
   plugins: [
     new webpack.ProvidePlugin({
       process: "process/browser",
     }),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-      inject: true,
-    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ inject: true, template: "public/index.html" }),
   ],
-
   resolve: {
-    modules: [path.resolve(__dirname, "src"), "node_modules"],
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    preferRelative: true,
     fallback: {
       process: require.resolve("process/browser"),
     },
   },
-
   devServer: {
     static: path.join(__dirname, "dist"),
     compress: true,
@@ -268,6 +268,7 @@ module.exports = {
           "/access/v1/actions/mdms",
           "/tenant-management",
           "/user-otp",
+          "/egov-mdms-service",
           "/mdms-v2",
           "/egov-idgen",
           "/egov-location",
@@ -276,6 +277,7 @@ module.exports = {
           "/pgr-services",
           "/filestore",
           "/egov-hrms",
+          "/user-otp",
           "/user",
           "/fsm",
           "/billing-service",
@@ -285,27 +287,64 @@ module.exports = {
           "/vehicle",
           "/vendor",
           "/property-services",
+          "/fsm-calculator/v1/billingSlab/_search",
+          "/pt-calculator-v2",
           "/dashboard-analytics",
-          "/egov-searcher",
-          "/egov-pdf",
-          "/egov-url-shortening",
-          "/inbox",
-          "/report",
-          "/loi-service",
-          "/project",
-          "/estimate-service",
-          "/muster-roll",
-          "/individual",
-          "/facility",
-          "/boundary-service",
+          "/echallan-services",
+          "/egov-searcher/bill-genie/mcollectbills/_get",
+          "/egov-searcher/bill-genie/billswithaddranduser/_get",
+          "/egov-searcher/bill-genie/waterbills/_get",
+          "/egov-searcher/bill-genie/seweragebills/_get",
+          "/egov-pdf/download/UC/mcollect-challan",
+          "/egov-hrms/employees/_count",
+          "/tl-services/v1/_create",
+          "/tl-services/v1/_search",
+          "/egov-url-shortening/shortener",
+          "/inbox/v1/_search",
+          "/inbox/v2/_search",
+          "/tl-services",
+          "/tl-calculator",
           "/org-services",
+          "/edcr",
+          "/hcm-moz-impl",
           "/bpa-services",
           "/noc-services",
+          "/egov-user-event",
+          "/egov-document-uploader",
+          "/egov-pdf",
+          "/egov-survey-services",
+          "/ws-services",
+          "/sw-services",
+          "/ws-calculator",
+          "/sw-calculator/",
+          "/audit-service/",
+          "/egov-searcher",
+          "/report",
+          "/inbox/v1/dss/_search",
+          "/loi-service",
+          "/project/v1/",
+          "/estimate-service",
+          "/loi-service",
+          "/works-inbox-service/v2/_search",
+          "/egov-pdf/download/WORKSESTIMATE/estimatepdf",
+          "/muster-roll",
+          "/individual",
+          "/mdms-v2",
+          "/facility/v1/_search",
+          "/project/staff/v1/_create",
+          "/product/v1/_create",
+          "/boundary-service",
+          "/project-factory",
+          "/project-factory/v1/data/_autoGenerateBoundaryCode",
+          "/billing-service/bill/v2/_fetchbill",
+          "/tenant-management",
+          "/default-data-handler",
+          "/facility/v1/_create"
         ],
         target: "https://unified-qa.digit.org",
         changeOrigin: true,
         secure: false,
       },
     ],
-  },
+  }
 };
