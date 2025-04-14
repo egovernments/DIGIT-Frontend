d1a105f8e14cdcb5b874fa8743f461cb06738c4c
Piyush Raj <piyushraj473@gmail.com>
2025-04-14 11:11:42 +0530

    removed svg, ui, and ui-react-comp from local and using npm vesrions for these packages

--- micro-ui/web/micro-ui-internals/packages/ui-components/src/constants/images/images.js
+++
@@ -1,9 +0,0 @@
-const IMAGES = {
-  DIGIT_DARK: "https://egov-dev-assets.s3.ap-south-1.amazonaws.com/digit.png",
-  DIGIT_LIGHT:
-    "https://egov-uat-assets.s3.ap-south-1.amazonaws.com/hcm/mseva-white-logo.png",
-  DIGIT_FOOTER_DARK:"https://unified-dev.digit.org/egov-dev-assets/digit-footer-bw.png",
-  DIGIT_FOOTER_LIGHT:"https://unified-dev.digit.org/egov-dev-assets/digit-footer.png"
-};
-
-export { IMAGES };
