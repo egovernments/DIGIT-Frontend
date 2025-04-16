8d20d25b24327dcc6fcae70e5a9b7b4c09f82d0c
Abishek <abishek.t@egovernments.org>
2025-04-16 12:11:27 +0530

    Validations

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/AddColumnsWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/AddColumnsWrapper.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, createContext, useContext } from "react";
 import AddColumns from "./AddColumns";
 import { useTranslation } from "react-i18next";
-import { PopUp ,CardText,Button} from "@egovernments/digit-ui-components";
+import { PopUp, CardText, Button, Toast } from "@egovernments/digit-ui-components";
 
 const AddColContext = createContext("addColContext");
 export const useAddColContext = () => useContext(AddColContext);
@@ -10,25 +10,25 @@ const AddColumnsWrapper = ({ formData, onSelect, props: customProps }) => {
     const { t } = useTranslation();
     const [colValues, setColValues] = useState(() => {
         const storedCols = Digit.SessionStorage.get("MICROPLAN_DATA")?.NEW_COLUMNS?.newColumns;
-      
+
         // If storedCols exists and is an array, map it to the desired format
         if (Array.isArray(storedCols)) {
-          return storedCols.map((val, index) => ({
-            key: `MP_COLUMN_${index}`,
-            value: val,
-          }));
+            return storedCols.map((val, index) => ({
+                key: `MP_COLUMN_${index}`,
+                value: val,
+            }));
         }
-      
+
         // Fallback to default columns
         return [
-          { key: "MP_COLUMN_0", value: "" },
-          { key: "MP_COLUMN_1", value: "" },
+            { key: "MP_COLUMN_0", value: "" },
+            { key: "MP_COLUMN_1", value: "" },
         ];
-      });
-      
+    });
+
     const [executionCount, setExecutionCount] = useState(0);
 
-    const [showToast, setShowToast] = useState({});
+    const [showToast, setShowToast] = useState(null);
     const [showDeletePopup, setShowDeletePopup] = useState(false);
     const [columnsToDelete, setColumnsToDelete] = useState(null);
 
@@ -44,17 +44,26 @@ const AddColumnsWrapper = ({ formData, onSelect, props: customProps }) => {
             setShowToast({ message: t("COLUMN_ALREADY_EXISTS"), type: "error" });
             return;
         }
+        if (newColName === null || !newColName || newColName.trim() === "") {
+            setShowToast({ message: t("COLUMN_NAME_CANNOT_BE_EMPTY"), type: "error" });
+            return;
+        }
+        // Check if newColName exceeds the maximum allowed length of 235 characters
+        if (newColName.length > 235) {
+            setShowToast({ message: t("COLUMN_NAME_TOO_LONG"), type: "error" });
+            return;
+        }
         setColValues((prev) => [...prev, { key: key, value: newColName }]); // Adds new column
     };
 
     useEffect(() => {
-        onSelect(customProps.name, colValues.map((item)=>(item.value)));
+        onSelect(customProps.name, colValues.map((item) => (item.value)));
     }, [colValues]);
 
 
     useEffect(() => {
         if (executionCount < 5) {
-            onSelect(customProps.name, colValues.map((item)=>(item.value)));
+            onSelect(customProps.name, colValues.map((item) => (item.value)));
             setExecutionCount((prevCount) => prevCount + 1);
         }
     });
@@ -97,23 +106,31 @@ const AddColumnsWrapper = ({ formData, onSelect, props: customProps }) => {
                             }}
                         />,
                         <Button
-                        type="button"
-                        size="large"
-                        variation="primary"
-                        label={t("NO")}
-                        title={t("NO")}
-                        onClick={() => {
-                          setShowDeletePopup(false);    // Hide the delete confirmation popup
-                          setColumnsToDelete(null);     // Clear the selected columns
-                        }}
-                      />
-                      
+                            type="button"
+                            size="large"
+                            variation="primary"
+                            label={t("NO")}
+                            title={t("NO")}
+                            onClick={() => {
+                                setShowDeletePopup(false);    // Hide the delete confirmation popup
+                                setColumnsToDelete(null);     // Clear the selected columns
+                            }}
+                        />
+
                     ]}
                     sortFooterChildren={true}
                     onClose={() => {
                         setShowDeletePopup(false);
                     }}
                 ></PopUp>
+            )}
+            {showToast && (
+                <Toast
+                    label={showToast?.message}
+                    type={showToast?.type}
+                    isDleteBtn={true}
+                    onClose={() => setShowToast(null)}
+                ></Toast>
             )}
 
         </div>
