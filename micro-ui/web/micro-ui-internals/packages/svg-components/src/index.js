6159665ae68043e4e46f12a397ef4231c67e2153
Bhavya-egov <bhavya.mangal@egovernments.org>
2025-04-30 14:36:05 +0530

    added 2 new icons outpatient_med and ad_units

--- micro-ui/web/micro-ui-internals/packages/svg-components/src/index.js
+++ micro-ui/web/micro-ui-internals/packages/svg-components/src/index.js
@@ -815,6 +815,8 @@ import { SetUpMicroplanSvg } from "./svg/SetUpMicroplanSvg";
 import { AddCircle } from "./svg/AddCircle";
 import { GlobeLocationPin } from "./svg/GlobeLocationPin";
 import { NewWindow } from "./svg/NewWindow";
+import { Outpatient_med } from "./svg/Outpatient_med";
+import { Ad_units } from "./svg/Ad_units";
 
 export {
   AddCircle,
@@ -1633,5 +1635,7 @@ export {
   HRIcon,
   MeasurementMuktaIcon,
   GlobeLocationPin,
-  NewWindow
+  NewWindow,
+  Outpatient_med,
+  Ad_units
 };
