15887a02013d62d817e18fe3bbbd0d742797c30b
NabeelAyubee <nayubi7@gmail.com>
2025-05-14 11:38:05 +0530

    BUGFIX/HCMPRE-2408: Fixes for AppConfigurationParentLayer and AppConfigurationWrapper

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
@@ -258,6 +258,7 @@ function AppConfigurationWrapper({ screenConfig }) {
   const [state, dispatch] = useReducer((state, action) => reducer(state, action, updateLocalization), initialState);
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const { t } = useTranslation();
+  const currentLocale = Digit?.SessionStorage.get("initData")?.selectedLanguage || "en_IN";
   const [showPopUp, setShowPopUp] = useState(false);
   const [popupData, setPopupData] = useState(null);
   const [addFieldData, setAddFieldData] = useState(null);
@@ -266,7 +267,7 @@ function AppConfigurationWrapper({ screenConfig }) {
   const localeModule = searchParams.get("localeModule");
   const module = localeModule ? `hcm-dummy-module-${localeModule}` : "hcm-dummy-module";
   const [showPreview, setShowPreview] = useState(null);
-  const { mutateAsync: localisationMutate } = Digit.Hooks.campaign.useUpsertLocalisation(tenantId, module, "en_IN");
+  const { mutateAsync: localisationMutate } = Digit.Hooks.campaign.useUpsertLocalisation(tenantId, module, currentLocale);
   const [showToast, setShowToast] = useState(null);
   const { isLoading: isLoadingAppConfigMdmsData, data: AppConfigMdmsData } = Digit.Hooks.useCustomMDMS(
     Digit.ULBService.getCurrentTenantId(),
@@ -324,10 +325,9 @@ function AppConfigurationWrapper({ screenConfig }) {
 
   function createLocaleArrays() {
     const result = {};
-
     // Dynamically determine locales
-    const locales = Object.keys(locState[0]).filter((key) => key.includes("_IN") && key !== "en_IN");
-    locales.unshift("en_IN");
+    const locales = Object.keys(locState[0]).filter((key) => key.includes(currentLocale.slice(currentLocale.indexOf("_"))) && key !== currentLocale);
+    locales.unshift(currentLocale);
     locales.forEach((locale) => {
       result[locale] = locState
         .map((item) => ({
@@ -380,7 +380,10 @@ function AppConfigurationWrapper({ screenConfig }) {
           title={t("NEXT")}
           icon="ArrowForward"
           isSuffix={true}
-          onClick={() => onSubmit(state)}
+          onClick={() => {
+            handleSubmit();
+            onSubmit(state);
+          }}
         />
       </div>
 
