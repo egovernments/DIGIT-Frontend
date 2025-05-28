70a81562b2fc26119df4e4098a31aa9482b22750
Bhavya-egov <bhavya.mangal@egovernments.org>
2025-05-28 09:55:03 +0530

    changes label of individual

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/NewCampaignCreate/CampaignDetails.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/NewCampaignCreate/CampaignDetails.js
@@ -193,7 +193,7 @@ const CampaignDetails = () => {
         <div style={{ display: "flex" }}>
           <Tag label={t(campaignData?.projectType)} showIcon={false} className={"campaign-view-tag"} type={"warning"} stroke={true}></Tag>
           <Tag
-            label={campaignData?.deliveryRules?.[0]?.cycles?.length > 1 ? t("HCM_MULTIROUND") : t("HCM_INDIVIDUAL")}
+            label={campaignData?.deliveryRules?.[0]?.cycles?.length > 1 ? t("HCM_MULTIROUND") : t("HCM_SINGLE_ROUND")}
             showIcon={false}
             className={"campaign-view-tag"}
             type={"monochrome"}
