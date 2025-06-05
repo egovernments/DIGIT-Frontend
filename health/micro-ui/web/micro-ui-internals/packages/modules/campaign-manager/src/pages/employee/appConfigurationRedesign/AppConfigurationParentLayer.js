7034ff466e6ed5da33499a30721294027b308440
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-06-05 16:20:57 +0530

    FEATURE/HCMPRE-2816 :: 2472 updated changes

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationParentLayer.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationParentLayer.js
@@ -227,7 +227,6 @@ const AppConfigurationParentRedesign = () => {
       setCurrentStep((prev) => prev - 1);
     }
   };
-  console.log(numberTabs, "numberTabs");
 
   return (
     <div>
@@ -273,7 +272,7 @@ const AppConfigurationParentRedesign = () => {
               <Tag
                 stroke={false}
                 showIcon={false}
-                label={`${t("CMN_SCREEN")} -  1.03`}
+                label={`${t("CMN_SCREEN")} -  1.1`}
                 labelStyle={{ color: "#787878" }}
                 //  style={{background: "#EFF8FF"}}  labelStyle={{color:"#0B4B66"}}
               />
