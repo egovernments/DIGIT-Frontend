d41cb04d8dc40de3129d22431f990544979a0509
nabeelmd-eGov <nabeel.md@egovernments.org>
2025-06-08 00:12:44 +0530

    FEATURE/HCMPRE-2428: App configuratio css FIXes (#2515)
    * FEATURE/HCMPRE-2428: Added new campaign creation page and redesigned app configuration screen
    
    * FEATURE/HCMPRE-2428: Added new component for App Configuration Parent Layer
    
    * FEATURE/HCMPRE-2428: Fixes for AppConfigurationRedesign
    
    * FEATURE/HCMPRE-2428: Fixes
    
    * fixes
    
    * FEATURE: HCMPRE-2428 - Fixes for App Configuration Redesign
    
    * FEATURE/HCMPRE-2428: Fix: Added missing translations for the new app configuration redesign
    
    * HCMPRE-2428: Refactor App Configuration Redesign
    
    * FEATURE/HCMPRE-2428: Fix added message for drawer field composer
    
    * HCMPRE-2428: Review comments fixes
    
    * FEATURE/HCMPRE-2428: Adding validations
    
    * HCMPRE-2428: Added defaultValue check
    
    * last config not updating fix
    
    * HCMPRE-2428: Adding version
    
    * Validation checks
    
    * Regex fixes
    
    * Added Properties, Tab Sorted. Validation mapping of regex, min, max. Redesgin Conditional Field
    
    * FEATURE/HCMPRE-2428: Fixes
    
    * fixes language change issue  in app configuration
    
    * BUGFIXES/HCMPRE-2428: Updated ui-component version and its related changes
    
    * BUGFIX/HCMPRE-2428: Update UI Component
    
    * fixes
    
    * added radio, selection, uiux fix, localisation per page fix, readnly, systemDate, remove unwanted mdms
    
    * Added selection Tag and fixes
    
    * page name fixes
    
    * updated response action code
    
    * checkbox, select , radio issue fix
    
    * css
    
    * css fix
    
    * CSS FIX
    
    ---------
    
    Co-authored-by: NabeelAyubee <nayubi7@gmail.com>

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/AppPreview.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/AppPreview.js
@@ -230,7 +230,6 @@ const renderField = (field, t) => {
           optionsKey={"name"}
           selected={[]}
           withContainer={true}
-          width="Fixed-Equal"
           populators={{
             t: field?.isMdms ? null : t,
           }}
@@ -362,7 +361,9 @@ const AppPreview = ({ data = dummydata, selectedField, t }) => {
                   description={field?.isMdms ? t(field?.helpText) : field?.helpText || null}
                   error={field?.isMdms ? t(field?.errorMessage) : field?.errorMessage || null}
                   infoMessage={field?.isMdms ? t(field?.tooltip) : field?.tooltip || null}
-                  label={getFieldType(field) === "checkbox" || getFieldType(field) === "button" ? null : field?.label}
+                  label={
+                    getFieldType(field) === "checkbox" || getFieldType(field) === "button" ? null : field?.isMdms ? t(field?.label) : field?.label
+                  }
                   onChange={function noRefCheck() {}}
                   placeholder={t(field?.innerLabel) || ""}
                   populators={{
