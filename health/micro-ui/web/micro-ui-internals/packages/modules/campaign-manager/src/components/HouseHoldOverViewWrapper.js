4b34071953444352eaf562a201fb43d57f8c089b
pitabash-eGov <151724478+pitabash-eGov@users.noreply.github.com>
2025-06-11 16:39:48 +0530

    Git 2535 (#2566)
    * 2535::Household Overview Screen Template Components
    
    * HCMPRE-2496: Develop carousel loginpage updated
    
    * modified components as per the config
    
    * components rendered based on the jsonPath
    
    ---------
    
    Co-authored-by: Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>

---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/components/HouseHoldOverViewWrapper.js
@@ -0,0 +1,371 @@
+import React from "react";
+//import { Button } from "@egovernments/digit-ui-components";
+import { getRegisteredComponent } from "../utils/template_components/RegistrationRegistry";
+import "../utils/template_components/RegistrationComponents";
+
+import {
+  Card,
+  CardText,
+  TextInput,
+  SelectionTag,
+  Dropdown,
+  CardHeader,
+  Button,
+  TooltipWrapper,
+  AlertCard,
+  FieldV1,
+  Loader,
+  CheckBox,
+} from "@egovernments/digit-ui-components";
+
+const config = [
+  {
+    type: "template",
+    label: "EDIT_HOUSEHOLD",
+    order: 1,
+    value: true,
+    format: "editHousehold",
+    hidden: false,
+    tooltip: "",
+    helpText: "",
+    infoText: "",
+    readOnly: false,
+    fieldName: "editHousehold",
+    deleteFlag: false,
+    innerLabel: "",
+    systemDate: false,
+    validations: [],
+    errorMessage: "",
+    isMultiSelect: false,
+  },
+  {
+    type: "template",
+    label: "EDIT_INDIVIDUAL",
+    order: 2,
+    value: "",
+    format: "editIndividual",
+    hidden: false,
+    tooltip: "",
+    helpText: "",
+    infoText: "",
+    readOnly: false,
+    fieldName: "editIndividual",
+    deleteFlag: false,
+    innerLabel: "",
+    systemDate: false,
+    validations: [],
+    errorMessage: "",
+    isMultiSelect: false,
+  },
+  {
+    type: "template",
+    label: "ADD_MEMBER",
+    order: 3,
+    value: [],
+    format: "addMember",
+    hidden: false,
+    tooltip: "",
+    helpText: "",
+    infoText: "",
+    readOnly: false,
+    fieldName: "addMember",
+    deleteFlag: false,
+    innerLabel: "",
+    systemDate: false,
+    validations: [],
+    errorMessage: "",
+    isMultiSelect: false,
+  },
+  {
+    type: "template",
+    label: "UNABLE_TO_DELIVER",
+    order: 4,
+    value: true,
+    format: "SMCDeliverySecondaryButton",
+    hidden: false,
+    tooltip: "",
+    helpText: "",
+    infoText: "",
+    readOnly: false,
+    fieldName: "SMCDeliverySecondaryButton",
+    deleteFlag: false,
+    innerLabel: "",
+    systemDate: false,
+    validations: [],
+    errorMessage: "",
+    isMultiSelect: false,
+  },
+  {
+    type: "template",
+    label: "DELIVERY_DETAILS_LABEL",
+    order: 5,
+    value: "",
+    format: "SMCDeliveryPrimaryButton",
+    hidden: false,
+    tooltip: "",
+    helpText: "",
+    infoText: "",
+    readOnly: false,
+    fieldName: "SMCDeliveryPrimaryButton",
+    deleteFlag: false,
+    innerLabel: "",
+    systemDate: false,
+    validations: [],
+    errorMessage: "",
+    isMultiSelect: false,
+  },
+];
+
+const prevconfig = [
+  {
+    label: "Edit HouseHold",
+    value: true,
+    defaultValue: true,
+    active: true,
+    jsonPath: "EditButton",
+    metaData: {},
+    hidden: false,
+    deleteFlag: false,
+    isLocalised: false,
+    innerLabel: "",
+    helpText: "",
+    errorMessage: "",
+    tooltip: "",
+    infoText: "",
+    order: 1,
+    readOnly: false,
+    systemDate: false,
+    RegexPattern: false,
+    MdmsDropdown: false,
+    isMdms: false,
+    isMultiSelect: false,
+    type: "button",
+    appType: "button",
+  },
+  {
+    label: "",
+    value: true,
+    defaultValue: true,
+    active: true,
+    jsonPath: "ContentDetails",
+    metaData: {},
+    hidden: true,
+    deleteFlag: false,
+    isLocalised: false,
+    innerLabel: "",
+    helpText: "",
+    errorMessage: "",
+    tooltip: "",
+    infoText: "",
+    order: 1,
+    readOnly: false,
+    systemDate: false,
+    RegexPattern: false,
+    MdmsDropdown: false,
+    isMdms: false,
+    isMultiSelect: false,
+    type: "button",
+    appType: "button",
+  },
+  {
+    label: "",
+    value: true,
+    defaultValue: true,
+    active: true,
+    jsonPath: "ProfileCard",
+    metaData: {},
+    hidden: true,
+    deleteFlag: false,
+    isLocalised: false,
+    innerLabel: "",
+    helpText: "",
+    errorMessage: "",
+    tooltip: "",
+    infoText: "",
+    order: 1,
+    readOnly: false,
+    systemDate: false,
+    RegexPattern: false,
+    MdmsDropdown: false,
+    isMdms: false,
+    isMultiSelect: false,
+    type: "button",
+    appType: "button",
+  },
+
+  {
+    label: "",
+    value: true,
+    defaultValue: true,
+    active: true,
+    jsonPath: "DeliverInterventionBtn",
+    metaData: {},
+    hidden: true,
+    deleteFlag: false,
+    isLocalised: false,
+    innerLabel: "",
+    helpText: "",
+    errorMessage: "",
+    tooltip: "",
+    infoText: "",
+    order: 1,
+    readOnly: false,
+    systemDate: false,
+    RegexPattern: false,
+    MdmsDropdown: false,
+    isMdms: false,
+    isMultiSelect: false,
+    type: "button",
+    appType: "button",
+  },
+];
+const HouseHoldOverViewWrapper = ({ components = config, metaMasterConfig, t, selectedField }) => {
+  // const renderComponent = (data) => {
+  //   const ComponentToRender = getRegisteredComponent("EditButton");
+  //   const ContentDetails = getRegisteredComponent("ContentDetails");
+  //   const ProfileCard = getRegisteredComponent("ProfileCard");
+  //   switch (data.jsonPath) {
+  //     case "EditButton":
+  //       return <ComponentToRender label={data.label} t={t} onClick={() => {}} hidden={data.hidden} />;
+  //     case "ContentDetails":
+  //       return <ContentDetails />;
+  //     case "ProfileCard":
+  //       return (
+  //         <ProfileCard
+  //           name="Joseph Sergio"
+  //           gender="Male"
+  //           age={40}
+  //           relation="H"
+  //           label={"Edit"}
+  //           t={t}
+  //           onClick={() => {}}
+  //           unableToDeliver={false}
+  //           ableToDeliver={false}
+  //         />
+  //       );
+
+  //     case "DeliverInterventionBtn":
+  //       return (
+  //         <div style={{ marginTop: "16px" }}>
+  //           <ComponentToRender alignment="center" label={"Add Member"} t={t} onClick={() => {}} />
+  //         </div>
+  //       );
+  //     case "Description":
+  //       return <CardHeader>{t(data.label)}</CardHeader>;
+
+  //     case "SMCDeliverySecondaryButton":
+  //       return (
+  //         <ProfileCard
+  //           name="Joseph Sergio"
+  //           gender="Male"
+  //           age={40}
+  //           relation="H"
+  //           label={"Edit"}
+  //           t={t}
+  //           onClick={() => {}}
+  //           unableToDeliver={data.hidden}
+  //           ableToDeliver={false}
+  //           smcDeliverLabel={""}
+  //           unableToDeliverLabel={data.label}
+  //         />
+  //       );
+  //     case "SMCDeliveryPrimaryButton":
+  //       return (
+  //         <ProfileCard
+  //           name="Joseph Sergio"
+  //           gender="Male"
+  //           age={40}
+  //           relation="H"
+  //           label={"Edit"}
+  //           t={t}
+  //           onClick={() => {}}
+  //           unableToDeliver={false}
+  //           ableToDeliver={data.hidden}
+  //           smcDeliverLabel={data.label}
+  //           unableToDeliverLabel={""}
+  //         />
+  //       );
+  //     default:
+  //       return <ComponentToRender label={data.label} t={t} onClick={() => {}} />;
+  //   }
+  // };
+
+  const renderComponents = (inputData) => {
+    const formatMap = {};
+    inputData.forEach((item) => {
+      formatMap[item.jsonPath] = item;
+    });
+    const ComponentToRender = getRegisteredComponent("EditButton");
+    const ContentDetails = getRegisteredComponent("ContentDetails");
+    const ProfileCard = getRegisteredComponent("ProfileCard");
+    const editHousehold = formatMap["editHousehold"];
+    const editIndividual = formatMap["editIndividual"];
+    const smcSecondaryBtn = formatMap["SMCDeliverySecondaryButton"];
+    const smcPrimaryBtn = formatMap["SMCDeliveryPrimaryButton"];
+    const addMember = formatMap["addMember"];
+
+    return (
+      <div>
+        {/* 1. Household Edit Button - outside ProfileCard */}
+        <ComponentToRender label={data.label} t={t} onClick={() => {}} hidden={data.hidden} />
+        <ContentDetails />
+        {/* 2. ProfileCard always rendered */}
+        <HousedoldOverViewMemberCard
+          name="Joseph Sergio"
+          relation="H"
+          gender="Male"
+          age={40}
+          editIndividual={editIndividual}
+          smcPrimaryBtn={smcPrimaryBtn}
+          smcSecondaryBtn={smcSecondaryBtn}
+        />
+
+        {addMember && <ComponentToRender hidden={addMember.hidden} label={data.label} t={t} onClick={() => {}} />}
+        <div style={{ marginTop: "16px" }}>
+          <ComponentToRender alignment="center" label={"Add Member"} t={t} onClick={() => {}} />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column", flexGrow: 1, height: "100%" }}>
+      {/* Render content fields */}
+      <div style={{ flexGrow: 1 }}>
+        {
+          //   components.map((data, index) => {
+
+          //   return (
+          //     <div
+          //       // className={"app-preview-field-pair app-preview-selected"}
+          //       key={index}
+          //       style={{ marginBottom: "16px", width: "100%" }}
+          //     >
+          //       {renderComponent(data)}
+
+          //     </div>
+          //   );
+          // })
+
+          renderComponents(components)
+        }
+      </div>
+      <div
+        style={{
+          marginTop: "auto",
+        }}
+      >
+        <Button
+          label={"Deliver Intervention"}
+          onClick={() => {}}
+          variation="primary"
+          style={{
+            minWidth: "100%",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HouseHoldOverViewWrapper;
--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/template_components/RegistrationComponents.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/template_components/RegistrationComponents.js
@@ -1,8 +1,9 @@
-import { Switch, FieldV1, RoundedLabel, CustomSVG } from "@egovernments/digit-ui-components";
+import { Switch, FieldV1, RoundedLabel, CustomSVG, SummaryCardFieldPair } from "@egovernments/digit-ui-components";
 import React from "react";
 import SearchBeneficiaryRegistrationWrapper from "../../components/SearchBeneficiaryRegistrationWrapper";
 import { registerComponent } from "./RegistrationRegistry";
 import AppPreviewResponse from "../../components/AppPreviewResponse";
+
 
 
 
@@ -46,9 +47,192 @@ const ProximitySearch = (props) => (
     />
 );
 
+
+
+// household
+
+const EditIcon = () => (
+  <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ minWidth: "16px", minHeight: "16px" }}>
+    <path
+      d="M9.126 5.12579L11.063 3.18879L14.81 6.93579L12.873 8.87379L9.126 5.12579ZM17.71 2.63079L15.37 0.290793C15.1826 0.104542 14.9292 0 14.665 0C14.4008 0 14.1474 0.104542 13.96 0.290793L12.13 2.12079L15.88 5.87079L17.71 4.00079C17.8844 3.81533 17.9815 3.57035 17.9815 3.31579C17.9815 3.06123 17.8844 2.81625 17.71 2.63079ZM5.63 8.63079L0 14.2508V18.0008H3.75L9.38 12.3808L12.873 8.87379L9.126 5.12579L5.63 8.63079Z"
+      fill="#F47738"
+    />
+  </svg>
+);
+
+const EditButton = (props) => {
+  const { label = "LABEL", onClick, alignment = "flex-end", hidden = false } = props;
+  if (hidden) return null;
+  return (
+    <div style={{ display: "flex", justifyContent: alignment, width: "100%" }}>
+      <div
+        style={{
+          display: "inline-flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "8px",
+          padding: "8px 16px",
+          background: "#FFFFFF",
+          color: "rgba(244, 119, 56, 1)",
+          borderRadius: "4px",
+          cursor: "pointer",
+          fontFamily: "Roboto, sans-serif",
+          fontWeight: 400,
+          fontSize: "16px",
+          lineHeight: "100%",
+          textTransform: "capitalize",
+          border: "none",
+        }}
+        className="digit-search-action"
+        onClick={onClick}
+      >
+        <EditIcon />
+        <span className="digit-search-text">{label}</span>
+      </div>
+    </div>
+  );
+};
+
+// household member card
+
+const ContentDetails = (props) => {
+  const householdDetails = [
+    { label: "HOUSEHOLD_HEAD", value: "Value" },
+    { label: "ADMINSTRATIVE_AREA", value: "value" },
+    { label: "MEMBER_COUNT", value: 5 },
+  ];
+  return (
+    <div>
+      {householdDetails.map((pair, index) => (
+        <div>
+          <SummaryCardFieldPair
+            key={index} // Provide a unique key for each item
+            className=""
+            inline
+            label={t(pair.label)} // Dynamically set the label
+            value={t(pair.value)} // Dynamically set the value
+            // style={{ fontSize: "16px", fontWeight: "bold" }} // Optional: customize styles
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const HousedoldOverViewMemberCard = (props) => {
+  // return (
+  //   <div style={styles.card}>
+  //     <div style={styles.header}>
+  //       <div style={styles.name}>
+  //         <strong>{props.name}</strong> <span style={styles.relation}>({props.relation})</span>
+  //       </div>
+
+  //       <div style={styles.editButton}>
+  //         <EditButton label={props.label} t={props.t} onClick={props.onClick} hidden={props.hidden} />
+  //       </div>
+  //     </div>
+  //     <div style={styles.details}>
+  //       <span>{props.gender}</span>
+  //       <div style={styles.separator} />
+  //       <span>{props.age} years</span>
+  //     </div>
+
+  //     <div>
+  //       <EditButton alignment="center" label={props.smcDeliverLabel} t={t} onClick={() => {}} hidden={props.hidden} />
+  //       <EditButton alignment="center" label={props.unableToDeliverLabel} t={props.t} onClick={props.onClick} hidden={props.hidden} />
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    <div style={styles.card}>
+      <div style={styles.header}>
+        <div style={styles.name}>
+          <strong>{name}</strong> <span style={styles.relation}>({relation})</span>
+        </div>
+        <EditButton label={editIndividual?.label} hidden={editIndividual?.hidden} onClick={() => console.log("Edit Individual")} />
+      </div>
+      <div style={styles.details}>
+        <span>{gender}</span>
+        <div style={styles.separator} />
+        <span>{age} years</span>
+      </div>
+
+      {/* Two Center Buttons */}
+      <EditButton alignment="center" label={smcPrimaryBtn?.label} hidden={smcPrimaryBtn?.hidden} onClick={() => console.log("SMC Delivery")} />
+      <EditButton
+        alignment="center"
+        label={smcSecondaryBtn?.label}
+        hidden={smcSecondaryBtn?.hidden}
+        onClick={() => console.log("Unable to Deliver")}
+      />
+    </div>
+  );
+};
+
+const styles = {
+  card: {
+    width: "100%",
+    boxSizing: "border-box",
+    border: "1px solid #ddd",
+    borderRadius: "4px",
+    padding: "12px 16px",
+    fontFamily: "sans-serif",
+    fontSize: "14px",
+    color: "#4a4a4a",
+    backgroundColor: "white",
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "8px",
+    width: "100%",
+  },
+  name: {
+    fontSize: "16px",
+    color: "#333",
+    flex: 1, // take available space
+  },
+  relation: {
+    color: "#d32f2f",
+    marginLeft: "4px",
+  },
+  editButton: {
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    color: "#f26a21",
+    background: "none",
+    border: "none",
+    padding: 0,
+    fontSize: "14px",
+  },
+  editText: {
+    marginLeft: "4px",
+    color: "#f26a21",
+    fontWeight: "500",
+  },
+  details: {
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
+    color: "#4a4a4a",
+  },
+  separator: {
+    width: "1px",
+    height: "16px",
+    backgroundColor: "#aaa",
+  },
+};
+
 // Register all components
 registerComponent("searchBar", SearchBar);
 registerComponent("filter", Filter);
 registerComponent("searchByProximity", ProximitySearch);
 registerComponent("SearchBeneficiary", SearchBeneficiaryRegistrationWrapper);
 registerComponent("HouseholdAcknowledgement", AppPreviewResponse);
+registerComponent("HouseholdOverview", HouseHoldOverViewWrapper);
+registerComponent("EditButton", EditButton);
+registerComponent("ContentDetails", ContentDetails);
+registerComponent("HousedoldOverViewMemberCard", HousedoldOverViewMemberCard);
