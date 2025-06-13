5addecd7414b678b6ace0680f4d1ee9e98a00e4b
NabeelAyubee <nayubi7@gmail.com>
2025-06-13 17:24:06 +0530

    adhoc fixes

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
@@ -397,7 +397,7 @@ function AppConfigurationWrapper({ screenConfig, localeModule }) {
       const fieldItem = headerFields[i];
       const value = locS?.find((i) => i?.code === fieldItem?.value)?.[cL] || null;
       if (!value || value.trim() === "") {
-        return { type: "error", value: `${t("HEADER_FIELD_EMPTY_ERROR")} ${useCustomT(fieldItem?.label)}` };
+        return { type: "error", value: `${t("HEADER_FIELD_EMPTY_ERROR")} ${(fieldItem?.label)}` };
       }
     }
     const validateValue = (value, validation, label, a, b) => {
@@ -411,7 +411,7 @@ function AppConfigurationWrapper({ screenConfig, localeModule }) {
           (typeof value === "string" && value.trim() === "") ||
           (Array.isArray(value) && value.length === 0)
         ) {
-          return validation.message || `${t(`${label || "FIELD"}_REQUIRED_FOR`)} ${useCustomT(a?.label)}`;
+          return validation.message || `${t(`${label || "FIELD"}_REQUIRED_FOR`)} ${(a?.label)}`;
         }
       }
 
