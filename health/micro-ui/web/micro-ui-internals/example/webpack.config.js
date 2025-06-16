d94f5249972571e3eba0eb8f141dea2cead2d004
sasidhar07 <jonna.sasi17@gmail.com>
2025-06-16 17:21:44 +0530

    with webpack target fixed

--- health/micro-ui/web/micro-ui-internals/example/webpack.config.js
+++ health/micro-ui/web/micro-ui-internals/example/webpack.config.js
@@ -160,7 +160,7 @@ module.exports = {
                     "/default-data-handler",
                     "/facility/v1/_create"
                 ],
-                target: "https://unified-qa.digit.org",
+                target: envFile.REACT_APP_PROXY_API,
                 changeOrigin: true,
                 secure: false,
             },
