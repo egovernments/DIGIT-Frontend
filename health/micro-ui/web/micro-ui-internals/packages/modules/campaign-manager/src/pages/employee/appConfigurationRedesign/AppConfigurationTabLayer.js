b20e0f8e384b69bfdbd1fdd3b0c7633a098859f3
Bhavya-egov <bhavya.mangal@egovernments.org>
2025-06-17 12:37:45 +0530

    BUGFIX/HCMPRE-2557 : Added flag for multiple module selection

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationTabLayer.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationTabLayer.js
@@ -115,6 +115,8 @@ const AppConfigurationTabLayer = () => {
 
   const { isLoading: isTabLoading, data: tabData } = Digit.Hooks.useCustomAPIHook(reqCriteriaTab);
 
+  console.log("tabs" , numberTabs , tabData );
+
   if (isTabLoading) return <Loader />;
   return (
     <div>
