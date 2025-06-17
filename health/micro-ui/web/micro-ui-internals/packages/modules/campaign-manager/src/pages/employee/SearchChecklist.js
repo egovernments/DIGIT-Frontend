90befa5352370ce106195c57c35528c0acc08bd2
Bhavya-egov <bhavya.mangal@egovernments.org>
2025-06-17 16:13:52 +0530

    FEATURE/HCMPRE-2557 : UI/UX Fixs

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/SearchChecklist.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/SearchChecklist.js
@@ -136,7 +136,7 @@ const SearchChecklist = () => {
           // className={"campaign-flow-stepper"}
         /> */}
          <TagComponent campaignName={campaignName} />  
-        <div style={{ fontSize: "2.5rem", fontWeight: "700", fontFamily: "Roboto Condensed" }}>{t("CONFIGURE_CHECKLIST")}</div>
+        <div style={{ fontSize: "2.5rem", fontWeight: "700", fontFamily: "Roboto Condensed" , marginTop: "1rem" }}>{t("CONFIGURE_CHECKLIST")}</div>
         <div style={{ display: "flex", justifyContent: "space-between", marginTop: "1rem" }}>
           {/* <Header styles={{ fontSize: "32px", marginBottom: "2rem", marginTop: "2rem" }}>{t("ACTION_LABEL_CONFIGURE_APP")}</Header> */}
           {/* <Button
