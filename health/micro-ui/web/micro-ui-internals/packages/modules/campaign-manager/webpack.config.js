54ee6e031ee2cd07868d3901b66295c557e9a78a
Piyush Raj <piyushraj473@gmail.com>
2025-06-30 16:51:51 +0530

    few webpack changes

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/webpack.config.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/webpack.config.js
@@ -7,33 +7,31 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    library: {
-      name: "@egovernments/digit-ui-module-campaign-manager",
-      type: "umd",
-    },
-    globalObject: 'this', // Ensures compatibility in Node and browser environments
+    library: "@egovernments/digit-ui-module-campaign-manager",
+    libraryTarget: "umd", // This replaces the object with name + type
+    globalObject: "this", // Ensures compatibility in Node and browser environments
   },
   resolve: {
     extensions: [".js"],
-    fallback: {
-      process: require.resolve("process/browser.js"), // Polyfill for 'process'
-    },
+    alias: {
+      process: "process/browser.js" // replace fallback with alias
+    }
   },
   externals: {
     react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "react",
+      root: "React"
     },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "react-dom",
+      root: "ReactDOM"
     },
-    'react-i18next': 'react-i18next',
-    'react-router-dom': 'react-router-dom',
+    "react-i18next": "react-i18next",
+    "react-router-dom": "react-router-dom",
     "@tanstack/react-query": "@tanstack/react-query"
   },
   module: {
@@ -43,15 +41,15 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      process: "process/browser.js",
-    }),
-  ],
+      process: "process/browser.js"
+    })
+  ]
 };
