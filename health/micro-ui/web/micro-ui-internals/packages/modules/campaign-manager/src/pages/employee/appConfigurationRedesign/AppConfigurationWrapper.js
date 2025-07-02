9b6e115c0bb168213e0c61912b9ac9cc78742ef6
NabeelAyubee <nayubi7@gmail.com>
2025-07-02 17:45:26 +0530

    fix

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/AppConfigurationWrapper.js
@@ -501,7 +501,7 @@ function AppConfigurationWrapper({ screenConfig, localeModule, pageTag }) {
     } catch (error) {
       setLoading(false);
       setShowToast({ key: "error", label: "CONFIG_SAVE_FAILED" });
-      console.error(`Error sending ${locale} localisation data:`, error);
+      console.error(`Error sending localisation data:`, error);
     }
     // for (const locale of Object.keys(localeArrays)) {
     //   if (localeArrays[locale].length > 0) {
@@ -547,7 +547,7 @@ function AppConfigurationWrapper({ screenConfig, localeModule, pageTag }) {
     } catch (error) {
       setLoading(false);
       setShowToast({ key: "error", label: "CONFIG_SAVE_FAILED" });
-      console.error(`Error sending ${locale} localisation data:`, error);
+      console.error(`Error sending localisation data:`, error);
     }
     setShowPopUp(false);
     setLoading(false);
