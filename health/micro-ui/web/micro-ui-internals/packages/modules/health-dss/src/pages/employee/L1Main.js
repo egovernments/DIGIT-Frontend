b94cab7ddea0e7275cce96e370e6c34216333473
Swathi-eGov <137176788+Swathi-eGov@users.noreply.github.com>
2025-07-03 15:10:26 +0530

    FEATURE/HCMPRE-2973 : Added Loader and build issue check (#2730)
    FEATURE/HCMPRE-2873 : Added loader, and build issue check

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/pages/employee/L1Main.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/pages/employee/L1Main.js
@@ -461,6 +461,7 @@ const L1Main = () => {
                 size={"medium"}
                 className={"digit-dss-options-header-option-button"}
                 isSearchable={false}
+                wrapperClassName={"digit-dss-options-header-options-button-wrapper"}
                 onOptionSelect={onActionSelect}
                 icon={"Share"}
                 iconFill={"#505a5f"}
