5b7484ee700a359febfecdc900da87d560ff7b10
Abishek <abishek.t@egovernments.org>
2025-07-03 11:58:27 +0530

    In customTable.js If bigger hierarchy is removed in filter fix

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/components/CustomTable.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/components/CustomTable.js
@@ -42,7 +42,7 @@ const calculateFSTPCapacityUtilization = (value, totalCapacity, numberOfDays = 1
 const CustomTable = ({ data = {}, onSearch = { searchQuery }, setChartData, setChartDenomination }) => {
   const { id } = data;
   const [chartKey, setChartKey] = useState(id);
-  const [filterStack, setFilterStack] = useState([{ id: chartKey }]);
+  const [filterStack, setFilterStack] = useState([{ id: chartKey, hierarchyLevel:2 }]);
   const { t } = useTranslation();
   const { value, setValue, ulbTenants, fstpMdmsData } = useContext(FilterContext);
   const tenantId = Digit?.ULBService?.getCurrentTenantId();
@@ -512,8 +512,8 @@ const CustomTable = ({ data = {}, onSearch = { searchQuery }, setChartData, setC
   };
 
   const removeFilter = (id) => {
-    setFilterStack(prev => prev.filter((_, idx) => idx !== id));
-    setChartKey(filterStack?.[id - 1]?.id);
+    setFilterStack(prev => prev.filter((_, idx) => idx < id));
+    setChartKey(filterStack?.[-1]?.id || data?.id);
   };
 
   
