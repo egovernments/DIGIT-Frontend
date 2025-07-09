1e577506afcbca55ce3d82028f20cd1fdd61a0b1
Swathi-eGov <137176788+Swathi-eGov@users.noreply.github.com>
2025-07-09 17:36:54 +0530

    FEATURE/HCMPRE-2881 : Updated breadcrumbs (#2749)

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/components/DSSCard.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/components/DSSCard.js
@@ -1,4 +1,4 @@
-import { EmployeeModuleCard, SVG } from "@egovernments/digit-ui-react-components";
+import { EmployeeModuleCard } from "@egovernments/digit-ui-react-components";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
