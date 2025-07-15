11e4190bdcad91b83a81220fd4faf90843f52093
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2025-07-15 10:33:06 +0530

    HCM v1.8- HRMS & PGR Tech Debts

---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-hrms/src/pages/employee/Inbox.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { InboxSearchComposer, HeaderComponent, Toast, Loader } from "@egovernments/digit-ui-components";
+import { useTranslation } from "react-i18next";
+import inboxSearchConfig from "../../components/config/inboxSearchConfig";
+
+const InboxSearch = () => {
+  const { t } = useTranslation();
+
+  const isMobile = window.Digit.Utils.browser.isMobile();
+  const tenantId = Digit.ULBService.getCurrentTenantId();
+
+  const { data: mdmsData, isLoading } = Digit.Hooks.useCommonMDMS(Digit.ULBService.getStateId(), "egov-hrms", ["InboxConfig"], {
+    select: (data) => {
+      return data?.["egov-hrms"]?.InboxConfig?.[0];
+    },
+    retry: false,
+    enable: false,
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  
+  // const config = mdmsData ? mdmsData : inboxSearchConfig(); [TODO: Use this once MDMS is updated with the new config]
+  const config = inboxSearchConfig();
+
+  return (
+    <div style={{ marginBottom: "80px" }}>
+      <div
+        style={
+          isMobile
+            ? { marginLeft: "-12px", fontFamily: "calibri", color: "#FF0000" }
+            : { marginLeft: "15px", fontFamily: "calibri", color: "#FF0000" }
+        }
+      >
+        {
+          <HeaderComponent className="digit-inbox-search-composer-header" styles={{ marginBottom: "1.5rem" }}>
+            {t("HR_HOME_SEARCH_RESULTS_HEADING")}
+          </HeaderComponent>
+        }
+      </div>
+      <div className="digit-inbox-search-wrapper">
+        <InboxSearchComposer
+          configs={config}
+          //   browserSession={SelectCampaignSession}
+          //   additionalConfig={{
+          //     resultsTable: {
+          //       onClickRow,
+          //     },
+          //   }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default InboxSearch;
---
+++ health/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/hooks/project/useProjectSearch.js
@@ -0,0 +1,10 @@
+import ProjectService from "../../services/project/ProjectService";
+import { useQuery } from "react-query";
+const useProjectSearch = ({data, params, config = {}}) => {
+  return useQuery(["SEARCH_PROJECT",data,config.queryKey], () => ProjectService.projectSearch({
+    body: data,
+    params,
+  }), { ...config });
+};
+
+export default useProjectSearch;
