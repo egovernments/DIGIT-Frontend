55a4891efda837e465fa37786d57b0aae85e2a6a
nabeelmd-eGov <nabeel.md@egovernments.org>
2025-07-21 18:47:15 +0530

    app config bug bash fixes

--- health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/OldCampaignCard.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/microplan/src/components/OldCampaignCard.js
@@ -20,18 +20,18 @@ const CampaignCard = () => {
   const microplanStatus =  "RESOURCE_ESTIMATIONS_APPROVED"
  
   let links = [
-
     {
-      label: t("ACTION_TEST_SETUP_CAMPAIGN"),
-      link: `/workbench-ui/employee/campaign/setup-campaign`,
-      roles: ROLES.CAMPAIGN_MANAGER_ONLY
-    },
-    {
-      label: t("ACTION_TEST_MY_CAMPAIGN"),
-      link: `/workbench-ui/employee/campaign/my-campaign`,
+      label: t("ACTION_TEST_CREATE_CAMPAIGN"),
+      link: `/workbench-ui/employee/campaign/campaign-home`,
       roles: ROLES.CAMPAIGN_MANAGER,
       // count: isLoading?"-":data
-    },  
+    },
+    {
+      label: t("ACTION_TEST_MY_CAMPAIGN_NEW"),
+      link: `/${window?.contextPath}/employee/campaign/my-campaign-new`,
+      roles: ROLES.CAMPAIGN_MANAGER,
+      // count: isLoading?"-":data
+    },
     { 
       label: t("ACTION_TEST_SETUP_CAMPAIGN_FROM_MICROPLAN"),
       link: `/${window?.contextPath}/employee/campaign/setup-from-microplan?userId=${userId}&status=${microplanStatus}`,
@@ -54,13 +54,7 @@ const CampaignCard = () => {
       link: `/workbench-ui/employee/campaign/boundary/home`,
       roles: ROLES.BOUNDARY_MANAGER,
       // count: isLoading?"-":data
-    },
-    {
-      label: t("ACTION_TEST_APP_CONFIGURATION"),
-      link: `/workbench-ui/employee/campaign/app-configuration`,
-      roles: ROLES.CAMPAIGN_MANAGER,
-      // count: isLoading?"-":data
-    },  
+    }
   ];
 
   links = links.filter((link) => (link?.roles && link?.roles?.length > 0 ? Digit.Utils.didEmployeeHasAtleastOneRole(link?.roles) : true));
