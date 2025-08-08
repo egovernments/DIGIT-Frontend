3f516d6fac7d33e39cd83cd24c168085f83368b6
Swathi-eGov <137176788+Swathi-eGov@users.noreply.github.com>
2025-08-08 16:01:52 +0530

    FEATURE/HCMPRE-2958 : Added dependency field changes (#2980)
    * Added template screen for referral flow
    
    * FEATURE/HCMPRE-2958 : Added dependency field wrapper

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/ImpelComponentWrapper.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/appConfigurationRedesign/ImpelComponentWrapper.js
@@ -6,7 +6,7 @@ import { AppLocalisationWrapper, Loader, useCustomT } from "@egovernments/digit-
 //development mode
 import AppPreview from "../../../components/AppPreview";
 
-function ImpelComponentWrapper({ variant, screenConfig, submit, back, showBack, parentDispatch, localeModule, pageTag, ...props }) {
+function ImpelComponentWrapper({ variant, screenConfig, submit, back, showBack, parentDispatch, localeModule, pageTag, parentState,...props }) {
   const MODULE_CONSTANTS = "HCM-ADMIN-CONSOLE";
   const searchParams = new URLSearchParams(location.search);
   const fieldMasterName = searchParams.get("fieldType");
@@ -186,6 +186,7 @@ function ImpelComponentWrapper({ variant, screenConfig, submit, back, showBack, 
   //       parentDispatch={parentDispatch}
   //       localeModule={localeModule}
   //       pageTag={pageTag}
+  //       parentState={parentState}
   //     />
   //   );
   // }
@@ -199,6 +200,7 @@ function ImpelComponentWrapper({ variant, screenConfig, submit, back, showBack, 
       parentDispatch={parentDispatch}
       localeModule={localeModule}
       pageTag={pageTag}
+      parentState={parentState}
     >
       <AppPreview />
     </AppLocalisationWrapper>
--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/template_components/RegistrationComponents.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/utils/template_components/RegistrationComponents.js
@@ -297,6 +297,10 @@ const styles = {
 
 export const getTemplateRenderer = (templateName) => {
 
+  if (templateName?.includes('Acknowledgement')) {
+    return responsePanelComponent;
+  }
+
   switch (templateName) {
     case "BeneficiaryAcknowledgement":
     case "HouseholdAcknowledgement":
