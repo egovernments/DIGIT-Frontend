bd1558d88416822ac35fed25f2e47224d2bd3025
Abishek <abishek.t@egovernments.org>
2025-08-21 14:28:15 +0530

    BUGFIX/HCMPRE-11112: context-path change for health-project sevice

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/services/ProjectService.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/services/ProjectService.js
@@ -2,7 +2,7 @@ export const ProjectService = {
   staffSearch: async ({ body, params }) => {
     try {
       const response = await Digit.CustomService.getResponse({
-        url: `${window.globalConfigs?.getConfig("PROJECT_SERVICE")}/staff/v1/_search` || "/project/staff/v1/_search",
+        url: `${window?.globalConfigs?.getConfig("PROJECT_SERVICE_PATH")}/staff/v1/_search` || "/project/staff/v1/_search",
         useCache: false,
         method: "POST",
         userService: true,
@@ -20,7 +20,7 @@ export const ProjectService = {
   projectSearch: async ({ body, params }) => {
     try {
       const response = await Digit.CustomService.getResponse({
-        url: `${window.globalConfigs?.getConfig("PROJECT_SERVICE")}/v1/_search` || "/project/v1/_search",
+        url: `${window?.globalConfigs?.getConfig("PROJECT_SERVICE_PATH")}/v1/_search` || "/project/v1/_search",
         useCache: false,
         method: "POST",
         userService: true,
