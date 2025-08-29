6dd713a22b155e3e376f55cdbe4f90d2c2bc2a70
piyushraj-egov <111127118+piyushraj-egov@users.noreply.github.com>
2025-08-29 11:30:10 +0530

    reverted back to react-components for setupcampaign

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/SetupCampaign.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/src/pages/employee/SetupCampaign.js
@@ -1,4 +1,4 @@
-import { FormComposerV2, LoaderWithGap } from "@egovernments/digit-ui-components";
+import { FormComposerV2, LoaderWithGap } from "@egovernments/digit-ui-react-components";
 import React, { useState, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
