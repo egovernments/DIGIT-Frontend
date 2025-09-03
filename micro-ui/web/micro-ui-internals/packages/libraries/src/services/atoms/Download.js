8265698b7d453521670a3495cf5852fc0a182c3f
Swathi-eGov <137176788+Swathi-eGov@users.noreply.github.com>
2025-09-03 13:19:26 +0530

    BUGFIX/HCMPRE-1111 : Testing with new PDF Download Logic (#3160)
    BUGFIX/HCMPRE-1111 : TESTING WITH NEW PDF DOWNLOAD LOGIC

--- micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Download.js
+++ micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Download.js
@@ -164,7 +164,40 @@ const Download = {
     });
     */
   },
+  // PDF IS BEING DOWNLOADED AS IMAGE : TESTING WITH NEW PDF DOWNLOAD LOGIC
+  PDFMAIN: (node, fileName, share, resolve = null) => {
+    changeClasses("dss-white-pre-line", "dss-white-pre-temp");
+    applyCss();
 
+    const element = ReactDOM.findDOMNode(node.current);
+
+    return domtoimage
+      .toJpeg(element, {
+        quality: 1,
+        bgcolor: "white",
+        filter: (node) => !node?.className?.includes?.("divToBeHidden"),
+        style: { margin: "25px" },
+      })
+      .then((dataUrl) => {
+        // create PDF
+        const pdf = new jsPDF("l", "pt", [element.offsetWidth, element.offsetHeight]);
+        pdf.setFontSize?.(16);
+        pdf.text?.(40, 30, "Certificate");
+        pdf.addImage(dataUrl, "JPEG", 25, 50, element.offsetWidth, element.offsetHeight);
+
+        changeClasses("dss-white-pre-temp", "dss-white-pre-line");
+        revertCss();
+
+        if (share) {
+          // return PDF as File
+          const pdfBlob = pdf.output("blob");
+          return resolve(new File([pdfBlob], `${fileName}.pdf`, { type: "application/pdf" }));
+        } else {
+          // trigger download
+          pdf.save(`${fileName}.pdf`);
+        }
+      });
+  },
   IndividualChartImage: (node, fileName, share, resolve = null) => {
     const saveAs = (uri, filename) => {
       if(window.mSewaApp && window.mSewaApp.isMsewaApp()){
