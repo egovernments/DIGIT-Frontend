2d841453fffcf78d05ffda0874cdef3dd5a9428e
Piyush Raj <piyushraj473@gmail.com>
2025-09-08 09:23:16 +0530

    changes for reload crash

--- health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/webpack.config.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/campaign-manager/webpack.config.js
@@ -44,20 +44,26 @@ module.exports = {
 
   externals: {
     // Core React ecosystem
-    react: "React",
-    "react-dom": "ReactDOM",
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
     "react-router-dom": "react-router-dom",
     "react-i18next": "react-i18next",
     "@tanstack/react-query": "@tanstack/react-query",
-    // Redux ecosystem
     "react-redux": "react-redux",
     redux: "redux",
     "redux-thunk": "redux-thunk",
     // DIGIT UI cross-dependencies
-    "@egovernments/digit-ui-components": "@egovernments/digit-ui-components",
-    "@egovernments/digit-ui-react-components": "@egovernments/digit-ui-react-components",
-    "@egovernments/digit-ui-libraries": "@egovernments/digit-ui-libraries",
-    "@egovernments/digit-ui-svg-components": "@egovernments/digit-ui-svg-components",
+    "@egovernments/digit-ui-svg-components": "@egovernments/digit-ui-svg-components"
   },
 
   module: {
