1958b7ca6448cede33289e2463ab6fec7ae734e1
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2025-10-15 09:40:05 +0530

    Update install-deps.sh

--- micro-ui/web/install-deps.sh
+++ micro-ui/web/install-deps.sh
@@ -4,11 +4,20 @@ BRANCH="$(git branch --show-current)"
 
 echo "Main Branch: $BRANCH"
 
-INTERNALS="micro-ui-internals"
+INTERNALS="frontend/web/micro-ui-internals"
+
+git clone -b STUDIO-UI-4 https://github.com/egovernments/DIGIT-Studio.git $INTERNALS
+
 
 cp $INTERNALS/example/src/UICustomizations.js src/Customisations
+cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && yarn && yarn build && find . -name "node_modules" -type d -prune -print -exec rm -rf '{}' \;
+cd ..
 
-cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && echo "installing packages"
+rm -rf node_modules
+rm yarn.lock
+
+fi
+# cd $INTERNALS && echo "Branch: $(git branch --show-current)" && echo "$(git log -1 --pretty=%B)" && echo "installing packages"
 
 
 # yarn install
