dbc4b354c716dabd4d6dd8645bb04944cfdf1d45
Abishek <abishek.t@egovernments.org>
2025-10-15 21:04:03 +0530

    FEATURE/HCMPRE-1012: UIcustomizations Update

--- health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
+++ health/micro-ui/web/micro-ui-internals/example/src/UICustomizations.js
@@ -1603,6 +1603,7 @@ export const UICustomizations = {
     preProcess: (data) => {
       data.body.inbox.tenantId = Digit.ULBService.getCurrentTenantId();
       data.body.inbox.processSearchCriteria.tenantId = Digit.ULBService.getCurrentTenantId();
+      data.body.inbox.limit= data?.state?.tableForm?.limit;
 
       const requestDate = data?.body?.inbox?.moduleSearchCriteria?.range?.requestDate;
 
@@ -1612,23 +1613,30 @@ export const UICustomizations = {
 
         data.body.inbox.moduleSearchCriteria.fromDate = fromDate;
         data.body.inbox.moduleSearchCriteria.toDate = toDate;
-      }
-      else{
+      } else {
         delete data.body.inbox.moduleSearchCriteria.fromDate;
         delete data.body.inbox.moduleSearchCriteria.toDate;
       }
 
+      if (data?.state?.tableForm?.sortOrder) {
+        delete data.state.tableForm.sortOrder;
+      }
+      
       // Always delete the full range object if it exists
       delete data.body.inbox.moduleSearchCriteria.range;
-    
+
       const assignee = _.clone(data.body.inbox.moduleSearchCriteria.assignedToMe);
       delete data.body.inbox.moduleSearchCriteria.assignedToMe;
       delete data.body.inbox.moduleSearchCriteria.assignee;
-    
+
       if (assignee?.code === "ASSIGNED_TO_ME" || data?.state?.filterForm?.assignedToMe?.code === "ASSIGNED_TO_ME") {
         data.body.inbox.moduleSearchCriteria.assignedToMe = Digit.UserService.getUser().info.uuid;
       }
-    
+
+      if(data?.state?.filterForm){
+        window.Digit.SessionStorage.set("filtersForInbox",data?.state?.filterForm); 
+      }
+
       // --- Handle serviceCode ---
       let serviceCodes = _.clone(data.body.inbox.moduleSearchCriteria.serviceCode || null);
       serviceCodes = serviceCodes?.serviceCode;
@@ -1649,54 +1657,56 @@ export const UICustomizations = {
           localityArray = [rawLocality.code];
         }
       }
-      
+
       if (localityArray.length > 0) {
         delete data.body.inbox.moduleSearchCriteria.locality;
         data.body.inbox.moduleSearchCriteria.area = localityArray;
       } else {
         delete data.body.inbox.moduleSearchCriteria.area;
       }
-    
+
       // --- Handle status from state.filterForm ---
       const rawStatuses = _.clone(data?.state?.filterForm?.status || {});
       const statuses = Object.keys(rawStatuses).filter((key) => rawStatuses[key] === true);
-    
+
       if (statuses.length > 0) {
         data.body.inbox.moduleSearchCriteria.status = statuses;
       } else {
         delete data.body.inbox.moduleSearchCriteria.status;
       }
-    
+
       return data;
     },
     additionalCustomizations: (row, key, column, value, t, searchResult) => {
       switch (key) {
         case "CS_COMMON_COMPLAINT_NO":
           return (
-            <div style={{display:"grid"}}>
-            <span className="link" style={{display:"grid"}}>
-            <Link
-              to={ `/${window.contextPath}/employee/pgr/complaint-details/${value}`}
-            >
-              {String(value ? (column.translate ? t(column.prefix ? `${column.prefix}${value}` : value) : value) : t("ES_COMMON_NA"))}
-            </Link>
-          </span>
-          <span>{t(`SERVICEDEFS.${row?.businessObject?.service?.serviceCode.toUpperCase()}`)}</span>
-          </div>
+            <div style={{ display: "grid" }}>
+              <span className="link" style={{ display: "grid" }}>
+                <Link to={`/${window.contextPath}/employee/pgr/complaint-details/${value}`}>
+                  {String(value ? (column.translate ? t(column.prefix ? `${column.prefix}${value}` : value) : value) : t("ES_COMMON_NA"))}
+                </Link>
+              </span>
+              <span>{t(`SERVICEDEFS.${row?.businessObject?.service?.serviceCode.toUpperCase()}`)}</span>
+            </div>
           );
 
-          case "WF_INBOX_HEADER_LOCALITY":
+        case "WF_INBOX_HEADER_LOCALITY":
           return value ? <span>{t(`${value}`)}</span> : <span>{t("NA")}</span>;
 
         case "CS_COMPLAINT_DETAILS_CURRENT_STATUS":
-          return <span>{t(`CS_COMMON_${value}`)}</span>;
+          return value && value?.length>0
+            ? <span>{t(`WF_INBOX_${value}`)}</span>: <span>{t("NA")}</span>;
 
         case "WF_INBOX_HEADER_CURRENT_OWNER":
           return value ? <span>{value?.[0]?.name}</span> : <span>{t("NA")}</span>;
 
-        case "WF_INBOX_HEADER_SLA_DAYS_REMAINING":
-          return value > 0 ? <Tag label={value} showIcon={false} type="success" /> : <Tag label={value} showIcon={false} type="error" />;
-
+        case "WF_INBOX_HEADER_CREATED_DATE":
+          return value > 0 ? (
+            <Tag label={new Date(value).toLocaleDateString()} showIcon={false} type="success" />
+          ) : (
+            <Tag label={new Date(value).toLocaleDateString()} showIcon={false} type="error" />
+          );
         default:
           return t("ES_COMMON_NA");
       }
