97c2ea16aeb95833072d3844488551a4aa574ae1
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2025-10-17 13:40:14 +0530

    console issue fixed

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/Module.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/Module.js
@@ -12,22 +12,23 @@ export const DSSModule = ({ stateCode, userType, tenants }) => {
 
   const { path, url } = useRouteMatch();
   const hierarchyType = window?.globalConfigs?.getConfig("HIERARCHY_TYPE") || "HIERARCHYTEST";
-  const moduleCode = ["hcm-campaignmanager","hcm-dss", `hcm-boundary-${hierarchyType}`,"rainmaker-hcm-dss"];
+  const moduleCode = ["hcm-campaignmanager", "hcm-dss", `hcm-boundary-${hierarchyType}`, "rainmaker-hcm-dss"];
   const modulePrefix = "";
   const language = Digit.StoreData.getCurrentLanguage();
   const currentTenant = Digit.ULBService.getCurrentTenantId();
   const { isLoading, data: store } = Digit.Services.useStore({
     currentTenant,
-   // stateCode,
+    // stateCode,
     moduleCode,
     language,
     modulePrefix,
   });
 
-  console.log(data)
-  console.log(store)
+  console.log(stateCode)
+  console.log(currentTenant)
+
   if (isLoading) {
-    return <Loader className={"digit-center-loader"}/>;
+    return <Loader className={"digit-center-loader"} />;
   } else {
     return (
       <ProviderContext>
@@ -45,7 +46,7 @@ const componentsToRegister = {
 };
 
 export const initDSSComponents = () => {
-  
+
   overrideHooks();
   updateCustomConfigs();
   Object.entries(componentsToRegister).forEach(([key, value]) => {
