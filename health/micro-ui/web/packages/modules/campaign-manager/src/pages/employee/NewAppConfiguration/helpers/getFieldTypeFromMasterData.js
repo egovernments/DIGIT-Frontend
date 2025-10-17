8a6948c3822ac27af2ea1ede3396775048b1e7b9
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2025-10-17 08:54:35 +0530

    Dependent Field Chnages

--- health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/helpers/getFieldTypeFromMasterData.js
+++ health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/helpers/getFieldTypeFromMasterData.js
@@ -1,6 +1,19 @@
 export const getFieldTypeFromMasterData = (field, fieldTypeMasterData) => {
+  console.log("getFieldTypeFromMasterData", { field, fieldTypeMasterData });
   if (!fieldTypeMasterData || !Array.isArray(fieldTypeMasterData)) {
     return "text";
+  }
+
+    // Special check for template type fields
+  // Match by metadata.type === field.type AND fieldType === field.fieldName
+  if (field.type === "template" && field.fieldName) {
+    const templateMatch = fieldTypeMasterData.find(
+      (item) => item?.metadata?.type === field.type && item?.metadata?.format === field.format && item?.fieldType === field.fieldName
+    );
+    if (templateMatch?.fieldType) {
+      console.log("Template match found:", templateMatch.fieldType);
+      return templateMatch.fieldType;
+    }
   }
 
   // Find matching field type based on type and format
