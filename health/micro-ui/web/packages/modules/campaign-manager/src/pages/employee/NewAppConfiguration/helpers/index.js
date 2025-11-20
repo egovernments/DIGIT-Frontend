92800f514889e2931037a8d591345cda56ba4d63
Ramkrishna-egov <85437265+Ramkrishna-egov@users.noreply.github.com>
2025-11-20 14:46:37 +0530

    GIT-3369:: Demo Fixes (#3389)

--- health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/helpers/index.js
+++ health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/helpers/index.js
@@ -1,3 +1,4 @@
 export { getFieldTypeFromMasterData } from "./getFieldTypeFromMasterData";
 export { getFieldValueByPath } from "./getFieldValueByPath";
 export { getComponentFromMasterData } from "./getComponentFromMasterData";
+export {getFieldTypeFromMasterData2} from "./getFieldTypeFromMasterData";
