1fd62f5ad7fcc323d3ee1527f539d7d966adb0b0
Abishek <abishek.t@egovernments.org>
2025-11-25 12:30:21 +0530

    FEATURE/HCMPRE-1031: Pgr workflow model new popup

--- health/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRWorkflowModal.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRWorkflowModal.js
@@ -1,40 +1,20 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { FormComposerV2,} from '@egovernments/digit-ui-components';
+import { FormComposerV2, PopUp, Button } from '@egovernments/digit-ui-components';
 import PropTypes from 'prop-types';
-import {  Modal, CloseSvg, Close } from "@egovernments/digit-ui-react-components";
 
-
-const CloseBtn = (props) => {
-  return (
-    <div onClick={props?.onClick} style={props?.isMobileView ? { padding: 5 } : null}>
-      {props?.isMobileView ? (
-        <CloseSvg />
-      ) : (
-        <div className={"icon-bg-secondary"} style={{ backgroundColor: "#FFFFFF" }}>
-          <Close />
-        </div>
-      )}
-    </div>
-  );
-};
-
-const ModalHeading = (props) => {
-  return <h1 className="heading-m">{props.label}</h1>;
-};
-
-const PGRWorkflowModal = ({ 
+const PGRWorkflowModal = ({
   selectedAction,
   config,
-  onSubmit, 
+  onSubmit,
   closeModal,
   sessionFormData,
   setSessionFormData,
   clearSessionFormData,
   popupModuleActionBarStyles,
-  popupModuleMianStyles 
+  popupModuleMianStyles
 }) => {
-    
+
   const { t } = useTranslation();
 
   const onFormValueChange = (setValue, formData, formState, reset, setError, clearErrors, trigger, getValues) => {
@@ -46,27 +26,48 @@ const PGRWorkflowModal = ({
   if (!config || !selectedAction) return null;
 
   return (
-    <Modal
-     popupStyles={{ width: "48.438rem", borderRadius: "0.25rem", height: "fit-content" }}
-      headerBarMainStyle={{ padding: 0, margin: 0 }}
-      headerBarMain={<ModalHeading style={{ fontSize: "1.5rem" }} label={t(config?.label?.heading)} />}
-      actionCancelLabel={t(config.label.cancel)}     
-      actionCancelOnSubmit={closeModal}
-      actionSaveLabel={t(config.label.submit)}
-      actionSaveOnSubmit={()=> onSubmit(sessionFormData)}
-      headerBarEnd={<CloseBtn onClick={closeModal} />}
-      formId="modal-action"
-    >
-      <FormComposerV2
-        config={config.form}
-        noBoxShadow
-        inline
-        childrenAtTheBottom
-        onFormValueChange={onFormValueChange}
-        defaultValues={sessionFormData}
-        formId="modal-action"
-      />
-    </Modal>
+    <PopUp
+      style={{ width: "47.5rem", borderRadius: "0.25rem", height: "fit-content" }}
+      onClose={closeModal}
+      heading={t(config?.label?.heading)}
+      onOverlayClick={closeModal}
+      equalWidthButtons={true}
+      children={[
+        <div key="form-composer">
+          <FormComposerV2
+            config={config.form}
+            noBoxShadow
+            inline
+            childrenAtTheBottom
+            onFormValueChange={onFormValueChange}
+            defaultValues={sessionFormData}
+            formId="modal-action"
+          />
+        </div>
+      ]}
+      footerChildren={[
+        <Button
+          key="cancel-button"
+          className="campaign-type-alert-button"
+          type="button"
+          size="large"
+          variation="secondary"
+          label={t(config.label.cancel)}
+          title={t(config.label.cancel)}
+          onClick={closeModal}
+        />,
+        <Button
+          key="submit-button"
+          className="campaign-type-alert-button"
+          type="button"
+          size="large"
+          variation="primary"
+          label={t(config.label.submit)}
+          title={t(config.label.submit)}
+          onClick={() => onSubmit(sessionFormData)}
+        />
+      ]}
+    />
   );
 };
 
