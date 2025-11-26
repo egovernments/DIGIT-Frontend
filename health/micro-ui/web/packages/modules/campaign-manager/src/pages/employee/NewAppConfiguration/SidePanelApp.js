db908e2495ea92aacfa1550202bd577be84a16de
Swathi-eGov <swathi.chatrathi@egovernments.org>
2025-11-26 16:52:08 +0530

    FEATURE/HCMPRE-3399 : Added tag component in app preview for template and forms

--- health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/SidePanelApp.js
+++ health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/SidePanelApp.js
@@ -1,22 +1,38 @@
 import React from "react";
-import { Button, SidePanel } from "@egovernments/digit-ui-components";
+import { Button, SidePanel, Tag } from "@egovernments/digit-ui-components";
 import NewDrawerFieldComposer from "./NewDrawerFieldComposer";
 import NewAppFieldScreenWrapper from "./NewAppFieldScreenWrapper";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { useTranslation } from "react-i18next";
 import { deselectField, setShowPopupPreview } from "./redux/remoteConfigSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const SidePanelApp = ({ showPanelProperties }) => {
   const { t } = useTranslation();
   const dispatch = useDispatch();
+  const { currentData } = useSelector((state) => state.remoteConfig);
+
   const handleBackClick = () => {
     // Close popup preview if open
     dispatch(setShowPopupPreview(false));
     // Deselect field
     dispatch(deselectField());
   };
+
+  // Determine page type tag based on data.type
+  const pageTypeTag = currentData?.type === "template"
+    ? { label: t("TEMPLATE_SCREEN"), type: "warning" }
+    : { label: t("FORM_SCREEN"), type: "default" };
+
+  const labelStyles = {
+    fontFamily: "Roboto",
+    fontWeight: 600,
+    fontStyle: "SemiBold",
+    fontSize: "12px",
+    textAlign: "right"
+  };
+  
   return (
     <SidePanel
       bgActive
@@ -29,9 +45,25 @@ const SidePanelApp = ({ showPanelProperties }) => {
       defaultClosedWidth=""
       footer={[]}
       header={[
-        <div className="typography heading-m" style={{ color: "#0B4B66" }}>
-          {showPanelProperties ? t("CONFIGURE_APPCONFIG_PROPERTIES") : t("FIELD_CONFIGURATION")}
-        </div>,
+        <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+          <div className="typography heading-m" style={{ color: "#0B4B66", marginLeft: "0px", paddingTop: "0px" }}>
+            {showPanelProperties ? t("CONFIGURE_APPCONFIG_PROPERTIES") : t("FIELD_CONFIGURATION")}
+          </div>
+          {/* PAGE TYPE TAG below header */}
+          <Tag
+            label={pageTypeTag.label}
+            showIcon={false}
+            type={pageTypeTag.type}
+            stroke={false}
+            labelStyle={currentData?.type === "template" ? {
+              color: "#5C450A",
+              ...labelStyles
+            } : {
+              color: "#1C00BD", ...labelStyles
+            }}
+            style={currentData?.type === "template" ? { backgroundColor: "#FFF7D6", borderRadius: "8px" } : { backgroundColor: "#EBECFE", borderRadius: "8px" }}
+          />
+        </div>
       ]}
       hideScrollIcon
       isDraggable={false}
