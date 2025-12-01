44958f0afe4209f606d1b8f71bf01e4eecd9f06b
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2025-12-01 15:00:43 +0530

    Added STOCK REPORTS template components

--- health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/components/InfoCardTemplate.js
+++ health/micro-ui/web/packages/modules/campaign-manager/src/pages/employee/NewAppConfiguration/components/InfoCardTemplate.js
@@ -1,26 +1,23 @@
 import React from "react";
-// import { InfoCard } from "@egovernments/digit-ui-components";
+import { AlertCard } from "@egovernments/digit-ui-components";
 const InfoCardTemplate = ({ field, t, fieldTypeMasterData }) => {
   const infoConfig = fieldTypeMasterData?.find((item) => item?.metadata?.type === "template" && item?.metadata?.format === "infoCard");
   const infoTypes = infoConfig?.properties?.find((p) => p.code === "infoCardType")?.options || ["info"];
   const infoType = field?.properties?.infoCardType || infoTypes[0];
   const variantMap = {
-    info: "default",
+    info: "info",
     success: "success",
     error: "error",
     warning: "warning",
   };
   return (
-    <div />
-    // <InfoCard
-    //   populators={{
-    //     name: field?.fieldName || "infocard",
-    //   }}
-    //   variant={variantMap[infoType] || "default"}
-    //   text={field?.value || "Information message"}
-    //   label={field?.label ? t(field.label) : undefined}
-    //   style={{ marginBottom: "8px" }}
-    // />
+    <AlertCard
+      populators={{ name: "infocard" }}
+      variant="default"
+      className="cmn-help-info-card"
+      label={t(field.label) || "Info Card"}
+      text={t(field?.description) || "This is an info card template"}
+    />
   );
 };
 
