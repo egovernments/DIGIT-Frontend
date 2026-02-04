dca812fbd1a24bbb70a67005790ed905c3e72ab4
Piyush Raj <piyushraj473@gmail.com>
2026-02-04 12:22:38 +0530

    deleted packages

--- micro-ui/web/packages/ui-components/postcss.config.js
+++
@@ -1,8 +0,0 @@
-// module.exports = {
-//     plugins: [
-//       require('postcss-flexbugs-fixes'),
-//       require('autoprefixer'),
-//       // Add more plugins as needed
-//     ],
-//   };
-  
