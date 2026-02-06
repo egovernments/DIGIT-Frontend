ff0cd3c35ea156832f8ecc7dd66114826d331f59
Swathi-eGov <swathi.chatrathi@egovernments.org>
2026-02-06 16:25:02 +0530

    BUGFIX/HCMPRE-0000 : Updated data table styles

--- health/micro-ui-react19/web/packages/modules/campaign-manager/src/components/LocalisationEditorPopup.js
+++ health/micro-ui-react19/web/packages/modules/campaign-manager/src/components/LocalisationEditorPopup.js
@@ -103,6 +103,81 @@ const LocalisationEditorPopup = forwardRef(({ locales, languages, currentLocale,
     },
   ];
 
+  const tableCustomStyle = {
+    header: {
+      style: {
+        minHeight: "56px",
+      },
+    },
+    rows: {
+      style: {
+        backgroundColor: "#FFFFFF",
+        borderBottomStyle: "solid",
+        borderBottomWidth: "1px",
+        borderBottomColor: "#D6D5D4",
+        "&:hover": {
+          backgroundColor: "#FBEEE8",
+        },
+      },
+    },
+    headRow: {
+      style: {
+        borderTopStyle: "solid",
+        borderTopWidth: "1px",
+        borderTopColor: "#D6D5D4",
+        borderBottomStyle: "solid",
+        borderBottomWidth: "1px",
+        borderBottomColor: "#D6D5D4",
+        backgroundColor: "#EEEEEE",
+      },
+    },
+    headCells: {
+      style: {
+        "&:first-of-type": {
+          borderLeftStyle: "solid",
+          borderLeftWidth: "1px",
+          borderLeftColor: "#D6D5D4",
+          borderTopLeftRadius: "0.25rem",
+        },
+        "&:last-of-type": {
+          borderTopRightRadius: "0.25rem",
+        },
+        borderRightStyle: "solid",
+        borderRightWidth: "1px",
+        borderRightColor: "#D6D5D4",
+        fontFamily: "Roboto",
+        fontWeight: "700",
+        fontStyle: "normal",
+        fontSize: "16px",
+        color: "#0B4B66",
+        padding: "16px",
+        lineHeight: "1.14rem",
+        zIndex: 10,
+      },
+    },
+    cells: {
+      style: {
+        color: "#363636",
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: 400,
+        lineHeight: "1.37rem",
+        textAlign: "left",
+        fontSize: "16px",
+        padding: "16px",
+      },
+    },
+    pagination: {
+      style: {
+        borderStyle: "solid",
+        borderWidth: "1px",
+        borderColor: "#D6D5D4",
+        borderTopWidth: "0px",
+      },
+    },
+  };
+
+
   if (initialLoading) {
     return (
       <div className="localisation-editor-content">
@@ -137,6 +212,8 @@ const LocalisationEditorPopup = forwardRef(({ locales, languages, currentLocale,
           highlightOnHover
           noHeader
           persistTableHead
+          className={`data-table`}
+          customStyles={tableCustomStyle}
         />
       )}
     </div>
