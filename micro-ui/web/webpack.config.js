06b069a6982f4ba1d1e9e5013fec8313a69064b3
Hari-egov <hariprasad@egovernments.org>
2026-02-10 12:39:00 +0530

    webpack fixes

--- micro-ui/web/webpack.config.js
+++ micro-ui/web/webpack.config.js
@@ -20,9 +20,10 @@ module.exports = {
           }
         },
       },
-      {
+      {           
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      
       }
     ],
   },
