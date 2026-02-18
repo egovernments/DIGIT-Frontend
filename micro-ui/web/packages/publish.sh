0b1e175a009d4fd429e4dcaf72f84b844cc4d3bc
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2026-02-18 12:32:22 +0530

    updated the publish file to solve publish issue

--- micro-ui/web/packages/publish.sh
+++ micro-ui/web/packages/publish.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "$0")" && pwd)"
+ROOTDIR="$(cd "$BASEDIR/../../.." && pwd)"
 
 msg() {
   echo -e "\n\n\033[32;32m$1\033[0m"
 }
 
-
-# msg "Pre-building all packages"
-# yarn build
-# sleep 5
+########################################
+msg "Installing workspace dependencies"
+cd "$ROOTDIR" && yarn install --frozen-lockfile
+########################################
 
 msg "Building and publishing css"
-cd "$BASEDIR/css" && rm -rf dist && yarn && yarn build && npm publish --tag console-0.5
-
+cd "$BASEDIR/css" && rm -rf dist && yarn build && npm publish --tag console-0.5
 
 msg "Building and publishing core"
-cd "$BASEDIR/modules/core" &&   rm -rf dist && yarn&& yarn build &&   npm publish --tag console-0.5
+cd "$BASEDIR/modules/core" && rm -rf dist && yarn build && npm publish --tag console-0.5
 
 msg "Building and publishing workbench"
-cd "$BASEDIR/modules/workbench" &&   rm -rf dist && yarn&& yarn build && npm publish --tag console-0.5
-
+cd "$BASEDIR/modules/workbench" && rm -rf dist && yarn build && npm publish --tag console-0.5
