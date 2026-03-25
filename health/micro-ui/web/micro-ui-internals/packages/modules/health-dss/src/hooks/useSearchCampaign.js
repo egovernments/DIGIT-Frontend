488f4a4ad9eae1ea4b5d627956631d1006300a61
Swathi-eGov <swathi.chatrathi@egovernments.org>
2026-03-25 17:07:50 +0530

    fixed import issue

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/hooks/useSearchCampaign.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-dss/src/hooks/useSearchCampaign.js
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 const searchCampaignService = async ({ tenantId, filter, pagination }) => {
   const response = await Digit.CustomService.getResponse({
     url: "/project-factory/v1/project-type/search",
