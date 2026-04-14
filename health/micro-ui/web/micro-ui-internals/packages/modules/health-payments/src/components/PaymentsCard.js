706ff7ada60ca87a0b7230522cc9c847da878317
ishaan-egov <ishaan.damadia@egovernments.org>
2026-04-14 11:31:59 +0530

    commented unused links

--- health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/components/PaymentsCard.js
+++ health/micro-ui/web/micro-ui-internals/packages/modules/health-payments/src/components/PaymentsCard.js
@@ -40,8 +40,8 @@ const PaymentsCard = () => {
     generateLink("ATTENDANCE_REGISTERS", "project-selection"),
     generateLink("CS_COMMON_INBOX", "project-and-aggregation-selection", ROLES.BILLS),
     generateLink("CS_TITLE_MY_BILLS", "my-bills", ROLES.BILLS),
-    generateLink("CS_TITLE_VERIFY_AND_GENERATE_PAYMENTS", "verify-bills", ROLES.BILLS),
-    generateLink("CS_TITLE_EDIT_BILLS", "edit-bills", ROLES.EDIT_BILLS),
+    // generateLink("CS_TITLE_VERIFY_AND_GENERATE_PAYMENTS", "verify-bills", ROLES.BILLS), //todo
+    // generateLink("CS_TITLE_EDIT_BILLS", "edit-bills", ROLES.EDIT_BILLS),
   ];
   const hasRequiredRoles = (link) => {
     if (!link?.roles?.length) return true;
