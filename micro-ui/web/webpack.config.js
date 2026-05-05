97089c7d7ebf5101df7e61623d81e8ef149ab4db
Hari-egov <168806306+Hari-egov@users.noreply.github.com>
2026-05-05 15:36:06 +0530

    build fixes

--- micro-ui/web/webpack.config.js
+++ micro-ui/web/webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        include: /node_modules\/axios\//,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: { ie: 11 } }]]
+          }
+        }
+      },
+      {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: {
