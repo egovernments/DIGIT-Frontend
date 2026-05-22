f76957fd3f54cd2605bfbb5123ccf6101b0bb8f9
Ramkrishna-egov <ramkrishna.sahoo@egovernments.org>
2026-05-22 18:03:44 +0530

    Fix process.env.NODE_ENV not being defined in DefinePlugin causing dev jsx-runtime to load in production

--- health/micro-ui-react19/web/webpack.common.js
+++ health/micro-ui-react19/web/webpack.common.js
@@ -58,6 +58,7 @@ module.exports = {
       "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
     }),
     new webpack.ProvidePlugin({
+      process: "process/browser",
       React: "react",
       ReactDOM: "react-dom",
     }),
