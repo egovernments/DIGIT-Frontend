35abf4c2e169e87dcb43b8e22afa6152218b02b8
Swathi-eGov <swathi.chatrathi@egovernments.org>
2026-05-28 18:24:06 +0530

    BUGFIX/HCMPRE-9876 : Moved mobile validation as toast

--- health/micro-ui-react19/web/packages/modules/health-hrms/src/components/pageComponents/DeactivatePopUp.js
+++ health/micro-ui-react19/web/packages/modules/health-hrms/src/components/pageComponents/DeactivatePopUp.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { PopUp, Button, Toast, Dropdown, FileUpload, FieldV1 } from "@egovernments/digit-ui-components";
+import {
+  PopUp,
+  Button,
+  Toast,
+  Dropdown,
+  FileUpload,
+  FieldV1,
+} from "@egovernments/digit-ui-components";
 import { convertEpochToDate } from "../../utils/utlis";
 
-const DeactivatePopUp = ({bussnessBtnLabel, label, onClose, onSubmit,reasonMsg }) => {
+const DeactivatePopUp = ({
+  bussnessBtnLabel,
+  label,
+  onClose,
+  onSubmit,
+  reasonMsg,
+}) => {
   const { t } = useTranslation();
   const tenantId = Digit.ULBService.getCurrentTenantId();
 
-  const { isLoading, isError, errors, data, ...rest } = Digit.Hooks.hrms.useHrmsMDMS(tenantId, "egov-hrms", "DeactivationReason");
+  const {
+    isLoading,
+    isError,
+    errors,
+    data,
+    ...rest
+  } = Digit.Hooks.hrms.useHrmsMDMS(tenantId, "egov-hrms", "DeactivationReason");
   // state variables
   const [comment, setComment] = useState("");
   const [showToast, setShowToast] = useState(null);
@@ -39,68 +58,6 @@ const DeactivatePopUp = ({bussnessBtnLabel, label, onClose, onSubmit,reasonMsg }
           onClose();
         }}
         heading={t(label ? label : `HR_COMMON_DEACTIVATED_EMPLOYEE_HEADER`)}
-        children={[
-          <div className="comment-section">
-            <div className="comment-label">
-              {t(reasonMsg?`HR_ACTIVATION_REASON`:`HR_DEACTIVATION_REASON`)}
-              <span className="required"> *</span>
-            </div>
-
-            <Dropdown
-              additionalWrapperClass=""
-              defaultValue="FEMALE"
-              description=""
-              error=""
-              errorStyle={null}
-              inputRef={null}
-              label={t(`HRMS_SELECT_OPTION`)}
-              name="genders"
-              onChange={(e) => {}}
-              option={data?.["egov-hrms"]?.DeactivationReason.map((ele) => {
-                ele["i18key"] = "EGOV_HRMS_DEACTIVATIONREASON_" + ele.code;
-                return ele;
-              })}
-              optionKey="code"
-              optionsCustomStyle={{}}
-              select={(e) => {
-                setReason(e.code);
-              }}
-              t={t}
-              type="dropdown"
-            />
-          </div>,
-
-          <FieldV1
-            label={t("HR_ENTER_ORDER_NO")}
-            onChange={(e) => {
-              setComment(e.target.value);
-            }}
-            type="text"
-            value={comment || ""}
-          />,
-
-          <FieldV1
-            label={t("HR_EFFECTIVE_DATE")}
-            type="date"
-            populators={{ name: "date", disableTextField: true }}
-            key="effective"
-            onChange={(e) => {
-              setDate(e);
-            }}
-            disable={true}
-            value={date}
-          />,
-
-          <FileUpload label={t("TL_APPROVAL_UPLOAD_SUBHEAD")} showLabel uploadedFiles={[]} variant="uploadField" />,
-          <FieldV1
-            label={t("HR_ENTER_REMARKS")}
-            onChange={(e) => {
-              setOrder(e.target.value);
-            }}
-            type="text"
-            value={order || ""}
-          />,
-        ]}
         onOverlayClick={onClose}
         equalWidthButtons={true}
         footerChildren={[
@@ -123,15 +80,91 @@ const DeactivatePopUp = ({bussnessBtnLabel, label, onClose, onSubmit,reasonMsg }
             size="large"
             variation="primary"
             style={{ minWidth: "270px" }}
-            label={t( bussnessBtnLabel)}
+            label={t(bussnessBtnLabel)}
             title={t(bussnessBtnLabel)}
             onClick={() => {
               handleSave();
             }}
           />,
         ]}
-      />
-      {showToast && <Toast style={{ zIndex: 10001 }} label={showToast.label} type={showToast.key} onClose={() => setShowToast(null)} />}
+      >
+        <div className="comment-section">
+          <div className="comment-label">
+            {t(reasonMsg ? `HR_ACTIVATION_REASON` : `HR_DEACTIVATION_REASON`)}
+            <span className="required"> *</span>
+          </div>
+          <Dropdown
+            additionalWrapperClass=""
+            defaultValue="FEMALE"
+            description=""
+            error=""
+            errorStyle={null}
+            inputRef={null}
+            label={t(`HRMS_SELECT_OPTION`)}
+            name="genders"
+            onChange={(e) => {}}
+            option={data?.["egov-hrms"]?.DeactivationReason.map((ele) => {
+              ele["i18key"] = "EGOV_HRMS_DEACTIVATIONREASON_" + ele.code;
+              return ele;
+            })}
+            optionKey="code"
+            optionsCustomStyle={{}}
+            select={(e) => {
+              setReason(e.code);
+            }}
+            t={t}
+            type="dropdown"
+          />
+        </div>
+        <FieldV1
+          label={t("HR_ENTER_ORDER_NO")}
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+          type="text"
+          value={comment || ""}
+          populators={{ alignFieldPairVerically: true }}
+        />
+        <FieldV1
+          label={t("HR_EFFECTIVE_DATE")}
+          type="date"
+          populators={{
+            name: "date",
+            disableTextField: true,
+            alignFieldPairVerically: true,
+          }}
+          key="effective"
+          onChange={(e) => {
+            setDate(e);
+          }}
+          disable={true}
+          value={date}
+        />
+        <FileUpload
+          label={t("TL_APPROVAL_UPLOAD_SUBHEAD")}
+          showLabel
+          uploadedFiles={[]}
+          variant="uploadField"
+          populators={{ alignFieldPairVerically: true }}
+        />
+        <FieldV1
+          label={t("HR_ENTER_REMARKS")}
+          onChange={(e) => {
+            setOrder(e.target.value);
+          }}
+          type="text"
+          value={order || ""}
+          populators={{ alignFieldPairVerically: true }}
+        />
+      </PopUp>
+      {showToast && (
+        <Toast
+          style={{ zIndex: 10001 }}
+          label={showToast.label}
+          type={showToast.key}
+          onClose={() => setShowToast(null)}
+        />
+      )}
     </React.Fragment>
   );
 };
