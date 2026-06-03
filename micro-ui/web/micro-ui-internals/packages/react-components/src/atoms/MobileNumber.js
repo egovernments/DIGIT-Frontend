151549b284aa769aa2ff91da08962d1c575bc0f5
ishaan-egov <ishaan.damadia@egovernments.org>
2026-06-03 23:36:11 +0530

    11 digit

--- micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/MobileNumber.js
+++ micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/MobileNumber.js
@@ -6,7 +6,7 @@ const MobileNumber = (props) => {
 
   const onChange = (e) => {
     let val = e.target.value;
-    if (isNaN(val) || [" ", "e", "E"].some((e) => val.includes(e)) || val.length > (props.maxLength || props.maxlength || 10)) {
+    if (isNaN(val) || [" ", "e", "E"].some((e) => val.includes(e)) || val.length > (props.maxLength || props.maxlength || 11)) {
       val = val.slice(0, -1);
     }
     props?.onChange?.(val);
