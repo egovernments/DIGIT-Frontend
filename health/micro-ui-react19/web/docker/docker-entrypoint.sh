281cc32268d94499459b0decc406359ea2a55e2b
Holash Chand <132540997+holashchand@users.noreply.github.com>
2026-07-21 16:54:35 +0530

    Update docker-entrypoint.sh for dynamic base path
    Refactor docker-entrypoint.sh to compute base path dynamically and update nginx config generation.

--- health/micro-ui-react19/web/docker/docker-entrypoint.sh
+++ health/micro-ui-react19/web/docker/docker-entrypoint.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 set -e
 
-# Inject <base> tag into index.html so relative asset paths resolve correctly on any route
-sed -i "s|<head>|<head><base href=\"/${COUNTRY_PREFIX}/${BUILD_VARIANT}/\">|" /var/web/app/index.html
+# Compute the base path
+if [ -n "$COUNTRY_PREFIX" ]; then
+    BASE_PATH="/$COUNTRY_PREFIX/$BUILD_VARIANT"
+else
+    BASE_PATH="/$BUILD_VARIANT"
+fi
 
-# Substitute environment variables in nginx config template at runtime
-envsubst '${BUILD_VARIANT} ${COUNTRY_PREFIX}' < /tmp/nginx.conf.template > /etc/nginx/conf.d/default.conf
+export BASE_PATH
+
+# Inject <base> tag
+sed -i "s|<head>|<head><base href=\"${BASE_PATH}/\">|" /var/web/app/index.html
+
+# Generate nginx config
+envsubst '${BASE_PATH}' \
+    < /tmp/nginx.conf.template \
+    > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
