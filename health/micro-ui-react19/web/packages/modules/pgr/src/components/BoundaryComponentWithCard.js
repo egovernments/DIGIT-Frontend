ee85c405e4d79dad43a2b099a30bdc85e5f654c7
Swathi-eGov <swathi.chatrathi@egovernments.org>
2026-08-20 19:14:02 +0530

    FEATURE/HCMPRE-0964 : Boundary component labels fix, css version update

--- health/micro-ui-react19/web/packages/modules/pgr/src/components/BoundaryComponentWithCard.js
+++ health/micro-ui-react19/web/packages/modules/pgr/src/components/BoundaryComponentWithCard.js
@@ -124,7 +124,7 @@ const BoundaryDropdown = ({ label, data, onChange, selected, isMandatory }) => {
 
   return (
     <div className="boundary-dropdown-wrapper" style={{display:"flex",flexDirection:"row",gap:"1rem"}}>
-      <div className="comment-label-without-card" style={{width:"33%"}}>
+      <div className="digit-header-content label comment-label-without-card" style={{width:"33%"}}>
         {label}
         {isMandatory && <span style={{ color: "#d4351c" }}> *</span>}
       </div>
