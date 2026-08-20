9eace8548322e91dbb504ae2018f70b3b9425363
bhanu <bhanu.prakash@egovernments.org>
2026-08-20 15:55:06 +0530

    updating mobilenumber validation to pick from mdms

--- health/micro-ui-react19/web/packages/modules/health-hrms/src/pages/employee/createEmployee.js
+++ health/micro-ui-react19/web/packages/modules/health-hrms/src/pages/employee/createEmployee.js
@@ -60,13 +60,23 @@ const CreateEmployee = ({ editUser = false }) => {
       return data?.["egov-hrms"]?.CreateEmployeeConfig?.[0];
     },
     retry: false,
-    enable: false,
+    enable: true,
   });
 
-
-  const validatePhoneNumber = (value) => {
+  // Validate phone number based on config
+  const validatePhoneNumber = (value, config) => {
+    const { minLength, maxLength, min, max } = config?.populators?.validation || {};
     const stringValue = String(value || "");
-    return !!stringValue.match(getPattern("MobileNo"));
+
+    if (
+      (minLength && stringValue.length < minLength) ||
+      (maxLength && stringValue.length > maxLength) ||
+      (min && Number(value) < min) ||
+      (max && Number(value) > max)
+    ) {
+      return false;
+    }
+    return true;
   };
 
   const onFormValueChange = (setValue, formData) => {
