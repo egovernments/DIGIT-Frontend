8945c5f918bc77a7b5eaaf0e24ea5aa9c8cc3e3a
Surya Chetty <surya.chetty@egov.global>
2026-09-18 23:22:15 +0530

    Multi-heirarchy Fix

--- health/micro-ui-react19/web/packages/modules/campaign-manager/src/components/CreateCampaignComponents/NewUploadData.js
+++ health/micro-ui-react19/web/packages/modules/campaign-manager/src/components/CreateCampaignComponents/NewUploadData.js
@@ -1432,7 +1432,11 @@ const NewUploadData = ({ formData, onSelect, ...props }) => {
         props?.props?.campaignData?.additionalDetails?.localityCode ||
         props?.props?.campaignData?.additionalDetails?.boundaryCode ||
         props?.props?.campaignData?.boundaryCode;
-      const hierarchyType = "ADMIN";
+      const hierarchyType =
+        searchParams.get("hierarchyType") ||
+        params?.hierarchyType ||
+        props?.props?.campaignData?.hierarchyType ||
+        "ADMIN";
 
       const requestBulkTemplate = async () => {
         const response = await Digit.CustomService.getResponse({
